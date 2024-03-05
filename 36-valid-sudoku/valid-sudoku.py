c9ab745015d5df2a97d2b711b78fe7f2f17ab6ad
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-05 15:49:56 -0800

    Time: 59 ms (80.51%) | Memory: 11.7 MB (80.87%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -4,44 +4,49 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: bool
         """
-        for i in board:
+        # check the rows
+
+        for i in range(9): # row
             s = set()
-            for j in i:
-                if j in s:
+            for j in range(9): # cols
+                cellValue = board[i][j]
+                if cellValue in s:
                     return False
                 else:
-                    if j!=".":
-                        s.add(j)
-        print("rows ok")
-
-        for i in range(9):
+                    if cellValue !='.':
+                        s.add(cellValue)
+        print("rows are cool")      
+        #  check the cols
+        for i in range(9): #row
             s = set()
-            for j in range(9):
-                cell = board[j][i]
-                if cell in s:
+            for j in range(9): #cols
+                cellValue = board[j][i]
+                if cellValue in s:
                     return False
                 else:
-                    if cell!=".":
-                        s.add(cell)
+                    if cellValue !='.':
+                        s.add(cellValue)
 
-        print("cols ok")
-
+        print("cols are cool")
+        # check the 3x3 grids each
         anchors = [(0,0), (0,3), (0,6),
                     (3,0), (3,3), (3,6),
                     (6,0), (6,3), (6,6)]
 
-        for grid in range(9):
+
+        for ij in range(9):
             s = set()
-            i = anchors[grid][0]
-            j = anchors[grid][1]
-            for ii in range(i, i+3):
-                for jj in range(j, j+3):
-                    # print(">>>", ii, jj)
-                    cell = board[ii][jj]
-                    if cell in s:
+            x = anchors[ij][0]
+            y = anchors[ij][1]
+            for i in range(x, x+3):
+                for j in range(y, y+3):
+                    # print(">> ",i,j , " vs ", anchors[ij])
+                    cellValue = board[j][i]
+                    if cellValue in s:
                         return False
                     else:
-                        if cell!='.':
-                            s.add(cell)
-            print("=======")
+                        if cellValue != '.':
+                            s.add(cellValue)
+
         return True
+                    
