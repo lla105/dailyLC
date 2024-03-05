2d41bc2c6816eb5df0c698201fe5135de33e8e99
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-04 16:44:09 -0800

    Time: 81 ms (27.74%) | Memory: 15.7 MB (77.28%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -4,17 +4,14 @@ class Solution(object):
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        anagram_map = defaultdict(list)
-        count = 1
-        print( "0) ", anagram_map)
+        dict = defaultdict(list)
 
         for word in strs:
-            sorted_word = ''.join(sorted(word))
-            anagram_map[sorted_word].append(word)
-            # sorted_word = ''.join(sorted(word))
-            # print(">>>> ", sorted_word)
-            # anagram_map[sorted_word].append(word)
-            # print(count, ") ", anagram_map)
-            # count +=1
+            sorted_key = sorted(word)
+            sorted_key = ''.join(sorted_key)
+            # print(sorted_key)
+            dict[sorted_key].append(word)
 
-        return list(anagram_map.values())
+        print(list(dict.values()))
+        return list(dict.values())
+        # print(list(dict.values()))
