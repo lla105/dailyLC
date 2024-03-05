115457b943c13c600e13975a4f8461170ebbdcf1
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-04 20:26:08 -0800

    Time: 75 ms (44.30%) | Memory: 15.7 MB (71.01%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -4,14 +4,13 @@ class Solution(object):
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        dict = defaultdict(list)
+        d = defaultdict(list)
 
         for word in strs:
-            sorted_key = sorted(word)
-            sorted_key = ''.join(sorted_key)
-            # print(sorted_key)
-            dict[sorted_key].append(word)
+            sortedWord = sorted(word)
+            sortedWord = ''.join(sortedWord)
+            d[sortedWord].append(word)
 
-        print(list(dict.values()))
-        return list(dict.values())
-        # print(list(dict.values()))
+
+        print(list(d.values()))
+        return list(d.values())
