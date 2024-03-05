bcef39fd8081456426bba39f9a23df5f93f78e3e
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-04 20:43:20 -0800

    Time: 90 ms (17.84%) | Memory: 15.1 MB (72.14%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -5,21 +5,24 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        # nums = [1,1,1,2,2,3,3,3,3]
+        # nums = [4,4,4,5,5,6]
         # nums = [2,3,4,1,4,0,4,-1,-2,-1]
-
         d = Counter(nums)
         print(d)
         heap = []
+        for each in d:
+            # print(each)
+            heapq.heappush(heap, (-d.get(each) , each ))
 
-        for num,freq in d.items():
-            heapq.heappush(heap, (-freq, num))
-
-        print(heap)
-        res = []
+        print("heap : ", heap)
+        answer = []
         print()
+        # for i in range(k):
+        #     print(heap[i][1])
+        #     answer.append(heap[i][1])
+
         for _ in range(k):
             _, num = heapq.heappop(heap)
-            print(_, num)
-            res.append(num)
-        return res
+            answer.append(num)
+
+        return answer
