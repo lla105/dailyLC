b37240739ad8cf8c241b1ca31c7f287cac698e36
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-05 22:35:03 -0800

    Time: 96 ms (36.67%) | Memory: 16.7 MB (40.56%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,15 +1,15 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        
         for i in range(9):
             s = set()
             for j in range(9):
                 cell = board[i][j]
                 if cell in s:
                     return False
-                else :
-                    if cell != '.':
+                else:
+                    if cell!='.':
                         s.add(cell)
+        print("row is good")
 
         for i in range(9):
             s = set()
@@ -17,18 +17,21 @@ class Solution:
                 cell = board[j][i]
                 if cell in s:
                     return False
-                else :
-                    if cell != '.':
+                else:
+                    if cell!='.':
                         s.add(cell)
 
-        anchor = [(0,0), (0,3), (0,6),
+        print("col is good too")
+
+        anchors = [ (0,0), (0,3), (0,6),
                     (3,0), (3,3), (3,6),
-                    (6,0), (6,3), (6,6)]
-        z = anchor[0]
-        x = anchor[0][0]
-        y = anchor[0][1]
-        for each in range(9):
-            z = anchor[each]
+                    (6,0), (6,3), (6,6) ]
+        
+        # traverse 3x3s
+        x=0
+        y=0
+        for eachgrid in range(9):
+            z = anchors[eachgrid]
             s = set()
             for i in range(z[0], z[0]+3, 1):
                 for j in range(z[1], z[1]+3, 1):
@@ -36,7 +39,8 @@ class Solution:
                     if cell in s:
                         return False
                     else:
-                        if cell !='.':
+                        if cell!='.':
                             s.add(cell)
+            #         print(i,j)
+            # print("===")
         return True
-
