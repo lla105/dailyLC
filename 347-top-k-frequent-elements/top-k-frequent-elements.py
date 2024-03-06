8f7352a1650fb1ca50b268106b42a3e8d90915e2
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-05 23:27:03 -0800

    Time: 170 ms (8.26%) | Memory: 21.4 MB (5.57%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -5,26 +5,26 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
+        # if len(nums) == 1:
+        #     return 
+        # nums = [11,11,11,22,22,33,33]
+        dic1 = Counter(nums)
+        print("dic1: ",dic1)
 
-        dic = Counter(nums)
-
-        dic2 = {}
-        for i in range(len(nums) + 1):
+        freqdic = {}
+        for i in range( 0, len(nums)+1, 1):
             # print(i)
-            dic2[i] = []
+            freqdic[i] = []
+        print("freq: ", freqdic)
 
+        for v,i in dic1.items():
+            if i in freqdic:
+                freqdic[i].append(v)
+        print("2freq: ", freqdic)
 
-        for v,i in dic.items():
-            # print(v,i)
-            dic2[i].append(v)
-
-        res = []
-
-        for i in range(len(dic2)-1, 0, -1):
-            for n in dic2[i]:
-                res.append(n)
-                if len(res)==k:
-                    return res
-
-            
-        
+        answer = []
+        for i in range(len(freqdic)-1, 0, -1) :
+            for jj in freqdic[i]:
+                answer.append(jj)
+                if len(answer) == k:
+                    return answer
