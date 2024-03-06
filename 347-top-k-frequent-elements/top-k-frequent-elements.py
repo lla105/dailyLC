75538f934fcee74a5c0aeb30026429ad81380390
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-05 23:45:30 -0800

    Time: 81 ms (32.42%) | Memory: 21 MB (5.57%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -10,24 +10,19 @@ class Solution(object):
 
         # nums = [11,11,11,22,22,33,33]
         dic = Counter(nums)
-        print("dic: ", dic)
 
         fredic = {}
         for freq in range(len(nums)+1):
             fredic[freq] = []
-        print("fredic : ", fredic)
 
         for v,i in dic.items():
             # print(v,i)
             if i in fredic:
                 fredic[i].append(v)
-        print(fredic)
 
         answer = []
         for i in range(len(fredic)-1, -1, -1):
-            print("i:", i)
             for number in fredic[i]:
-                print(">>>", number)
                 answer.append(number)
                 k-=1
                 if k==0:
