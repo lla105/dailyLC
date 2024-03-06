6d66ce3770baeadcd9b86e869020ca617889c74e
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-05 22:03:09 -0800

    Time: 94 ms (47.74%) | Memory: 16.6 MB (72.84%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,52 +1,44 @@
-class Solution(object):
-    def isValidSudoku(self, board):
-        """
-        :type board: List[List[str]]
-        :rtype: bool
-        """
-        # check the rows
-
-        for i in range(9): # row
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        for i in range(9):
             s = set()
-            for j in range(9): # cols
-                cellValue = board[i][j]
-                if cellValue in s:
+            for j in range(9):
+                cell = board[i][j]
+                if cell in s:
                     return False
-                else:
-                    if cellValue !='.':
-                        s.add(cellValue)
-        print("rows are cool")      
-        #  check the cols
-        for i in range(9): #row
-            s = set()
-            for j in range(9): #cols
-                cellValue = board[j][i]
-                if cellValue in s:
-                    return False
-                else:
-                    if cellValue !='.':
-                        s.add(cellValue)
+                else :
+                    if cell != '.':
+                        s.add(cell)
+        print("row is good")
 
-        print("cols are cool")
-        # check the 3x3 grids each
-        anchors = [(0,0), (0,3), (0,6),
+        for i in range(9):
+            s = set()
+            for j in range(9):
+                cell = board[j][i]
+                if cell in s:
+                    return False
+                else :
+                    if cell != '.':
+                        s.add(cell)
+        print("col is good ")
+
+        anchor = [(0,0), (0,3), (0,6),
                     (3,0), (3,3), (3,6),
                     (6,0), (6,3), (6,6)]
-
-
-        for ij in range(9):
+        z = anchor[0]
+        x = anchor[0][0]
+        y = anchor[0][1]
+        for each in range(9):
+            z = anchor[each]
             s = set()
-            x = anchors[ij][0]
-            y = anchors[ij][1]
-            for i in range(x, x+3):
-                for j in range(y, y+3):
-                    # print(">> ",i,j , " vs ", anchors[ij])
-                    cellValue = board[j][i]
-                    if cellValue in s:
+            for i in range(z[0], z[0]+3, 1):
+                for j in range(z[1], z[1]+3, 1):
+                    cell = board[i][j]
+                    if cell in s:
                         return False
                     else:
-                        if cellValue != '.':
-                            s.add(cellValue)
-
+                        if cell !='.':
+                            s.add(cell)
         return True
-                    
+
