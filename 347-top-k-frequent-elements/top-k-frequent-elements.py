c71a264fb539aaf3b7a3d66450ff9510d6bb341e
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-05 23:02:30 -0800

    Time: 148 ms (9.21%) | Memory: 21.3 MB (5.57%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,21 +1,33 @@
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # nums = [5,3,1,1,1,3,73,1]
-        counter = Counter(nums)
-        heap = [(v,k) for (k,v) in counter.items()]
-        # heap = []
-        # for k,v in counter.items():
-        #     heap.append( (v,k))
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
 
-        heapq.heapify(heap)
-        while len(heap) > k:
-            heapq.heappop(heap)
+        dic = Counter(nums)
+        print(dic)
 
-        # return [num for (freq, num) in heap]
+        dic2 = {}
+        for i in range(len(nums) + 1):
+            # print(i)
+            dic2[i] = []
 
+        print(dic2)
 
-        answer = []
-        for freq,num in heap :
-            answer.append(num)
+        for v,i in dic.items():
+            # print(v,i)
+            dic2[i].append(v)
+        print("dic2: ", dic2)
 
-        return answer
+        res = []
+
+        for i in range(len(dic2)-1, 0, -1):
+            for n in dic2[i]:
+                res.append(n)
+                if len(res)==k:
+                    return res
+
+            
+        
