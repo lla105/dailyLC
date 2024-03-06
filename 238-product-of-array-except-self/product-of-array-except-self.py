34aeceb8f6c9fe66bf9c10848843d7f2d5f38d04
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-05 21:40:44 -0800

    Time: 181 ms (14.39%) | Memory: 25 MB (14.61%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,21 +1,20 @@
-class Solution(object):
-    def productExceptSelf(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
         length = len(nums)
-        left = [1] * length
         right = [1] * length
+        left = [1] * length
 
         for i in range(1, length, 1):
             left[i] = nums[i-1] * left[i-1]
+        print(left)
 
-        for i in range(length-2, -1, -1):
-            right[i] = nums[i+1] * right[i+1]
-        answer = [1] * length
-        for i in range(0, length,1):
-            print(i)
-            answer[i] = left[i] * right[i]
+        for i in range(length-2, -1, -1) :
+            # print(i)
+            right[i] = right[i+1] * nums[i+1]
+        print(right)
+        answer = []
+        for i in range(length):
+            answer.append(right[i] * left[i])
 
         return answer
+
