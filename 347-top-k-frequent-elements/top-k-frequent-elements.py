440faac3da4264e03e02f39ec38014d2e9cd07f0
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-05 23:45:13 -0800

    Time: 208 ms (7.09%) | Memory: 21.3 MB (5.57%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -5,26 +5,32 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        # if len(nums) == 1:
-        #     return 
+        if len(nums)==1:
+            return [1]
+
         # nums = [11,11,11,22,22,33,33]
-        dic1 = Counter(nums)
-        print("dic1: ",dic1)
+        dic = Counter(nums)
+        print("dic: ", dic)
 
-        freqdic = {}
-        for i in range( 0, len(nums)+1, 1):
-            # print(i)
-            freqdic[i] = []
-        print("freq: ", freqdic)
+        fredic = {}
+        for freq in range(len(nums)+1):
+            fredic[freq] = []
+        print("fredic : ", fredic)
 
-        for v,i in dic1.items():
-            if i in freqdic:
-                freqdic[i].append(v)
-        print("2freq: ", freqdic)
+        for v,i in dic.items():
+            # print(v,i)
+            if i in fredic:
+                fredic[i].append(v)
+        print(fredic)
 
         answer = []
-        for i in range(len(freqdic)-1, 0, -1) :
-            for jj in freqdic[i]:
-                answer.append(jj)
-                if len(answer) == k:
+        for i in range(len(fredic)-1, -1, -1):
+            print("i:", i)
+            for number in fredic[i]:
+                print(">>>", number)
+                answer.append(number)
+                k-=1
+                if k==0:
                     return answer
+
+        
