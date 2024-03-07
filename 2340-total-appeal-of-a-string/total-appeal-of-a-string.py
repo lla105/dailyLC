e3bf366b9940a9ca01e0f491e6965a7b2c7fe995
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-07 13:32:59 -0800

    Time: 311 ms (9.46%) | Memory: 17.9 MB (30.18%) - LeetSync

--- 2340-total-appeal-of-a-string/total-appeal-of-a-string.py
+++ 2340-total-appeal-of-a-string/total-appeal-of-a-string.py
@@ -1,39 +1,39 @@
 class Solution:
     def appealSum(self, s: str) -> int:
         lastseendic = {}
-        # lastseendic format = { char : last seen index }
-
+        # dic format = { char : last seen index }
+        ptotal=0
         ctotal = 0
-        ptotal = 0
         total = 0
-
         for i in range(len(s)):
+            print(i)
             char = s[i]
-            if char in lastseendic:
-                ctotal = ptotal + (i - lastseendic[char])
-            else:
+            if char in lastseendic: # we've seen this char before
+                ctotal = (i- lastseendic[char]) + ptotal
+            else: # new char. never before seen
                 ctotal = i + 1 + ptotal
             lastseendic[char] = i
             total += ctotal
-            ptotal = ctotal 
-        return total 
+            ptotal = ctotal
+        return total
 
 
 
-# s: "abc"
-# 0: a:     a>1 = 1
-# 1: ab:    ab>2, b>1 = 3
-# 2: abc:   abc>3, bc>2, c>1 = 6
-# 3: abcd:  abcd>4, bcd>3, cd>2, d>1 = 10
-# 3: abcc:  abcc>3, bcc>2, cc>1, c>1 = 7
-# 3: abcb:  abcb>3, bcb>2, cb>2, b>1 = 8
-# 3: abca:  abca>3, bca>3, ca>2, a>1 = 9
+# s = abcd
+# i
+# 0:a      a>1 =1
+# 1:ab     ab>2, b>1 =3
+# 2:abc    abc>3, bc>2, c>1 = 6
+# 3:abcc.  abcc>3, bcc>2, cc>1, c>1 =7
+# 3:abcb.  abcb>3, bcb>2, cb>2, b>1 =8
+# 3:abca.  abca>3, bca>3, ca>2, a>1 =9
+# 3:abcd.  abcd>4, bcd>3, cd>2, d>1 =10 <-- all new letters
 
-# # 4: abcde: abcde>5, bcde>4, cde>3, de>2, e>1. =15 
 
-# if char is NOT seen before: 
-#     current total = (current index) + 1 + previous total
-#     all total = previous total + current total 
 
-# if char is seen before:
-#     current total = previous total + (how far previous index is)
+# if new char is not seen before:
+#     (current index)+1 +(previous total)
+# if new char is seen before:
+#     (current index - previous seen index) +previous total
+
+# update dictionary 
