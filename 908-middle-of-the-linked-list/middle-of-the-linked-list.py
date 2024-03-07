fea118dcf34cdd9f42ac0c08c50f906a67830735
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-06 21:54:21 -0800

    Time: 39 ms (27.18%) | Memory: 16.4 MB (89.17%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -5,21 +5,15 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # dic = {}
-        # dic format is { index : point to current node }
-        # templist = []
         current = head
         length=0
         while current:
             length+=1
-            print(f'current.val->{current.val}')
-            previous = current 
-            # dic[length] = previous
             current = current.next
-        print(f'length:{length}')
-        # print(f'dic: {dic}')
-        print(f'5//2={5//2}')
-        print(f'6//2={6//2}')
+        # print(f'length:{length}')
+        # # print(f'dic: {dic}')
+        # print(f'5//2={5//2}')
+        # print(f'6//2={6//2}')
         current = head
         for i in range(length//2):
             current = current.next
