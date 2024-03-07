7211745fe5ef7a583800f83eec39f846d3191ffa
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-06 21:54:04 -0800

    Time: 36 ms (50.55%) | Memory: 16.5 MB (46.37%) - LeetSync

---
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # dic = {}
+        # dic format is { index : point to current node }
+        # templist = []
+        current = head
+        length=0
+        while current:
+            length+=1
+            print(f'current.val->{current.val}')
+            previous = current 
+            # dic[length] = previous
+            current = current.next
+        print(f'length:{length}')
+        # print(f'dic: {dic}')
+        print(f'5//2={5//2}')
+        print(f'6//2={6//2}')
+        current = head
+        for i in range(length//2):
+            current = current.next
+        return current
+
+
