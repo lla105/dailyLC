67071ce1ffe717b9f3e53af5509b72b0ce68c392
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-09 13:46:22 -0800

    Time: 38 ms (40.02%) | Memory: 16.5 MB (79.86%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,26 +1,32 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        bracketsdic = {'(': ')',
-                        '{': '}',
-                        '[': ']'
-                        }
         stack = []
-        for b in s:
-            if b in bracketsdic:
-                # open bracket
-                stack.append(b)
+        length = len(s)
+
+        bdic = { ")":"(" , "]":"[", "}":"{"}
+        
+        for i in range(length):
+            char = s[i]
+            if char not in bdic:
+                # is open bracket
+                stack.append(char)
             else:
-                # close bracket
-                if len(stack) == 0:
+                #is closed bracket
+                if len(stack)==0:
+                    # stack empty, is false
                     return False
                 else:
-                    prevbracket = stack.pop()
-                    print(f'{b} vs {bracketsdic[prevbracket]}')
-                    if b != bracketsdic[prevbracket]:
+                    #stack not empty, check last item
+                    if stack[-1]!=bdic[char]:
                         return False
-                        
+                    else:
+
+                        stack.pop()
+            # print(f'{stack}')
+                
         if len(stack)==0:
             return True
         else:
             return False
+
             
