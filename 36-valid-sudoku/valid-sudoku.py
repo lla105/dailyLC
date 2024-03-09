9b25dd66aec7bad10aace680c8f3bad159e229cb
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-08 23:53:12 -0800

    Time: 84 ms (92.47%) | Memory: 16.4 MB (96.04%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,46 +1,49 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # check the rows
+        s = set()
+
         for i in range(9):
             s = set()
             for j in range(9):
                 cell = board[i][j]
                 if cell in s:
+                    # print(">>>",cell, s)
                     return False
-                else:
-                    if cell!='.':
+                else :
+                    if cell !='.':
                         s.add(cell)
-        print("row is good")
+        print(f' rows are cool')
 
+        # check the cols
         for i in range(9):
             s = set()
             for j in range(9):
                 cell = board[j][i]
                 if cell in s:
+                    # print(">>>",cell, s)
                     return False
-                else:
-                    if cell!='.':
+                else :
+                    if cell !='.':
                         s.add(cell)
+        print(f' cols are cool')
 
-        print("col is good too")
+        # check each mini 3x3 grid
 
         anchors = [ (0,0), (0,3), (0,6),
                     (3,0), (3,3), (3,6),
-                    (6,0), (6,3), (6,6) ]
-        
-        # traverse 3x3s
-        x=0
-        y=0
-        for eachgrid in range(9):
-            z = anchors[eachgrid]
+                    (6,0), (6,3), (6,6)
+                    ]
+        for a in anchors:
             s = set()
-            for i in range(z[0], z[0]+3, 1):
-                for j in range(z[1], z[1]+3, 1):
+            for i in range(a[0], a[0]+3):
+                for j in range(a[1], a[1]+3):
+                    # print(f'({i},{j})')
                     cell = board[i][j]
                     if cell in s:
                         return False
                     else:
-                        if cell!='.':
+                        if cell !='.':
                             s.add(cell)
-            #         print(i,j)
-            # print("===")
+                # print("---------")
         return True
