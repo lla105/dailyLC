d2ca0991ffb4b11b9c6b618aa3423920128164ae
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-09 13:22:01 -0800

    Time: 922 ms (27.29%) | Memory: 33 MB (8.93%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -9,9 +9,9 @@ class Solution:
 
         for curday, curtemp in enumerate(t):
             # print(f'{curday} : {curtemp}')
-            if curday == 0:
-                # s.append({curday:curtemp})
-                s.append([curday, curtemp])
+            # if curday == 0:
+            #     # s.append({curday:curtemp})
+            #     s.append([curday, curtemp])
             while len(s)!=0 and curtemp>s[-1][1]:
                 daydiff = abs(s[-1][0] - curday)
                 result[s[-1][0]] = daydiff
