09764bf3fa4fb7c80f5136a068a246f2a6fcc959
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-08 23:23:19 -0800

    Time: 101 ms (12.67%) | Memory: 27.2 MB (6.29%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,25 +1,38 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
         # nums = [11,11,11,22,22,22,33]
-       # dictionary format : { value : frequency }
 
-        if len(nums)==1:
-            return [1]
+        length = len(nums)
+        if length == 1:
+            return [nums[0]]
 
-        # nums = [11,11,11,22,22,33,33]
-        dic = Counter(nums)
+        # d format = { num : frequency }
+        d1 = Counter(nums)
+        print(f'd1 : {d1}')
+        # d2 format = {freq : [num, num]}
+        # lets say nums = 4
+        # d2 = { 4:[], 3:[], 2:[11,22], 1:[33] }
+        # eg nums = [ 11,11,22,22]
+        # { 2 : [11,22] }
 
-        fredic = {}
-        for freq in range(len(nums)+1):
-            fredic[freq] = []
+        d2 = {}
+        for i in range(length, 0, -1):
+            d2[i] = []
+        print(f'd2 : {d2}')
 
-        for v,i in dic.items():
-            fredic[i].append(v)
+        for i,v in d1.items():
+            if v in d2:
+                d2[v].append(i)
+        print(f'd2 now: {d2}')
 
-        answer = []
-        for i in range(len(fredic)-1, -1, -1):
-            for number in fredic[i]:
-                answer.append(number)
-                k-=1
-                if k==0:
-                    return answer
+        result = []
+        kcount = 0
+        for i in range(length, 0,-1):
+            # print(i)
+            for j in d2[i]:
+                result.append(j)
+                kcount += 1
+                if kcount == k:
+                    return result
+        return result
