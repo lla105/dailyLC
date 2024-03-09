403369f73fb32d736c7527346f899b9434406949
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-09 01:41:08 -0800

    Time: 934 ms (18.90%) | Memory: 32.8 MB (10.81%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,32 +1,25 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # monotonic stack
+        # where tempa must be > tempb
+        # and tempa has structure : [ curtemp, curday/i ]
         t = temperatures
         length = len(t)
-        print(t)
-        s = [] # format [ [temp,day] , [temp,day] ]
-        result = [0]*length
-        # { index : amount of days}
-        # t.pop(0) <-- pops from the head
-
-        # monotonic stack = [ tempa, tempb, tempc ...]
-        # where tempa, tempb, tempc is of format: { temp : i }
-        #for loop from front to back
+        # structure: s = [ tempa, tempb , ...]
+        s = [] 
+        result = [0] * length
+        # for curtemp in t
         for curday, curtemp in enumerate(t):
             # print(f'{curday}, {curtemp}')
-            # print(f'curday) : {curday}')
-            while len(s)>0 and curtemp>s[-1][0] :
-                prevday = s[-1][1]
-                diffday = curday - prevday
-                result[prevday] = diffday 
+            # while .....
+            while len(s)!=0 and curtemp>s[-1][0] :
+                previousday = s[-1][1]
+                difference = curday - previousday
+                result[previousday] = difference
                 s.pop(-1)
-            s.append([curtemp, curday])
-
+            s.append( [curtemp, curday])
         return result
-        # in mono stack, if i'm adding tempd, 
-        # tempd must be SMALLER than everything preceeding
-        # if tempd is BIGGER than previous, 
-        # pop previous while tempd>previous
-        # if tempd is SMALLER than previous,
-        # simply stack on top of mono stack
+        # if curtemp > s[-1]
+        # pop tempb, and update dayb with answer by: curday - dayb
 
-        # the remaining ones in the stack are all 0s ?
+        # if stack is empty, means constantly getting hotter
