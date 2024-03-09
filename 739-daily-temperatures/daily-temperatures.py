171716aa6aabf0055f6f0b5630f2744060b87199
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-09 13:21:27 -0800

    Time: 1010 ms (6.14%) | Memory: 33.3 MB (5.80%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,25 +1,22 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # monotonic stack
-        # where tempa must be > tempb
-        # and tempa has structure : [ curtemp, curday/i ]
         t = temperatures
         length = len(t)
-        # structure: s = [ tempa, tempb , ...]
-        s = [] 
-        result = [0] * length
-        # for curtemp in t
-        for curday, curtemp in enumerate(t):
-            # print(f'{curday}, {curtemp}')
-            # while .....
-            while len(s)!=0 and curtemp>s[-1][0] :
-                previousday = s[-1][1]
-                difference = curday - previousday
-                result[previousday] = difference
-                s.pop(-1)
-            s.append( [curtemp, curday])
-        return result
-        # if curtemp > s[-1]
-        # pop tempb, and update dayb with answer by: curday - dayb
 
-        # if stack is empty, means constantly getting hotter
+        result = [0]*length
+        #monotonic stack structure : [ {tempa:daya} , {tempb:dayb} , ....]
+        s = [ ]
+
+        for curday, curtemp in enumerate(t):
+            # print(f'{curday} : {curtemp}')
+            if curday == 0:
+                # s.append({curday:curtemp})
+                s.append([curday, curtemp])
+            while len(s)!=0 and curtemp>s[-1][1]:
+                daydiff = abs(s[-1][0] - curday)
+                result[s[-1][0]] = daydiff
+                s.pop()
+            s.append( [curday, curtemp])
+
+        print(f'{s} and {result}')
+        return result
