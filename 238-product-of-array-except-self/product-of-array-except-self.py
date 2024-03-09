6a667ee92273e2feeb42a3d8145f92628ce07245
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-08 23:41:33 -0800

    Time: 179 ms (17.53%) | Memory: 25 MB (21.40%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,20 +1,83 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # go through nums left to right
         length = len(nums)
-        right = [1] * length
-        left = [1] * length
-
-        for i in range(1, length, 1):
-            left[i] = nums[i-1] * left[i-1]
-        print(left)
-
-        for i in range(length-2, -1, -1) :
-            # print(i)
-            right[i] = right[i+1] * nums[i+1]
-        print(right)
-        answer = []
+        right = [1]*length
+        left = [1]*length
         for i in range(length):
-            answer.append(right[i] * left[i])
+            if i!=0 :
+                left[i] = left[i-1] * nums[i-1]
+        print(f'left : {left}')
+        # go through nums backwards 
+        for i in range(length-2, -1, -1):
+            right[i] = right[i+1] * nums[i+1]
+        print(f'right: {right}')
 
-        return answer
+        # result = []
+        result = [1]*length
+        for i in range(length):
+            result[i] = right[i] * left[i]
+        return result
+
+
+# nums = 1,2,3,4
+# [0] = a*2*3*4
+# [1] = 1*b*3*4
+# [2] = 1*2*c*4
+# [3] = 1*2*3*d
+
+# right = [a,b,c,d] = [24, 12, 4, 1]
+# left = [a,b,c,d]. = [1 ,  1, 2, 6]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # length = len(nums)
+        # right = [1] * length
+        # left = [1] * length
+
+        # for i in range(1, length, 1):
+        #     left[i] = nums[i-1] * left[i-1]
+
+        # for i in range(length-2, -1, -1) :
+        #     right[i] = right[i+1] * nums[i+1]
+        # answer = []
+        # for i in range(length):
+        #     answer.append(right[i] * left[i])
+
+        # return answer
 
