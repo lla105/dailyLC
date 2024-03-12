63947f19a8ad3fc2d8043ab807f8d7ad2666ea01
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-11 19:23:43 -0700

    Time: 760 ms (23.51%) | Memory: 42.5 MB (6.37%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -1,22 +1,18 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        # pair = sorted(pair(position, speed))
-        length = len(position)
-        pair = []
-        for i in range(length):
-            pair.append( (position[i], speed[i]) )
-        pair = sorted(pair, reverse=True)
-        # print(f'>> {pair}')
-
-        stack = []
-        for i in range(len(pair)):
-            p = pair[i][0]
-            s = pair[i][1]
-            stack.append( (target-p) / s)
-            # print(f'>>>>> {p}, {s} : {stack}')
-
-            if len(stack)>=2 and stack[-1]<=stack[-2]: #merge
-                # print(f'pop : {stack[-1]} >= {stack[-2]}')
+        pairs = []
+        for i in range(len(position)):
+            pairs.append( (position[i], speed[i]) )
+        pairs = sorted(pairs, reverse=True)
+        print(f'{pairs}')
+        stack=[]
+        for i in range(len(pairs)):
+            p = pairs[i][0]
+            s = pairs[i][1]
+            # print(p,s)
+            t = (target-p)/s
+            stack.append( t )
+            if len(stack)>1 and stack[-1]<=stack[-2]:
                 stack.pop()
+
         return len(stack)
-        
