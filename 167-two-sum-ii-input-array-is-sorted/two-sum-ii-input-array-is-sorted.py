2b7da87b572fd9a6990a1aaba18fd9315676c4e1
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-13 14:24:47 -0700

    Time: 105 ms (60.25%) | Memory: 17.4 MB (62.26%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,14 +1,14 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        print(f'numbers: {numbers}, target: {target}')
+        # print(f'numbers: {numbers}, target: {target}')
         d = {} #format : {value:index}
         for i in range(len(numbers)):
             num = numbers[i]
             diff = target-num
-            print(f"diff : {diff}")
+            # print(f"diff : {diff}")
             if diff in d:
                 return [d[diff]+1, i+1]
             d[num]=i
-            print(f'>> {d}')
+            # print(f'>> {d}')
  
         
