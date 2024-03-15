a835644cd8cda947915b89b2944a232a863e4352
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-14 22:49:33 -0700

    Time: 544 ms (19.97%) | Memory: 29.7 MB (28.77%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        print(f'{height}')
+        maxArea = 0
+        left = 0
+        right = len(height)-1
+
+        while left<right:
+            #pick the shorter bar
+            h = min(height[left], height[right])
+            currArea = h * (right-left)
+            maxArea = max(maxArea, currArea)
+
+            #remember to append left or right
+            if height[left] > height[right]:
+                right = right -1
+            else:
+                left = left + 1
+        return maxArea
