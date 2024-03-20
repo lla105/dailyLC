f56c1abd091ca8c304763fdb3e869d6d6122a35e
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-20 15:21:53 -0700

    Time: 42 ms (79.04%) | Memory: 17 MB (78.94%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -4,17 +4,14 @@ def bs(array, target):
 
     while left<=right:
         mid = left + (right-left) // 2
-        print(f'mid:{mid} . arr[mid]:{array[mid]}')
+        # print(f'mid:{mid} . arr[mid]:{array[mid]}')
         if array[mid] == target:
-            print("??")
+            # print("??")
             return True
         elif array[mid] > target:
             right = mid - 1
         elif array[mid] < target:
             left = mid + 1
-        # # else:
-        # elif array[mid] == target:
-        #     return True
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
