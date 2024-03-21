06dafe3ad9005ab9d905b1868f55491301c75b1e
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-20 22:05:22 -0700

    Time: 707 ms (83.77%) | Memory: 27.3 MB (72.69%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -3,10 +3,8 @@ class Solution:
         buyprice = prices[0]
         bestprofit = 0
         for i in range(1, len(prices)):
-            buyprice = min(buyprice, prices[i])
-            bestprofit = max(bestprofit, prices[i]-buyprice)
-            # if prices[i] < buyprice:
-            #     buyprice = prices[i]
-            # elif bestprofit < prices[i] - buyprice:
-            #     bestprofit = prices[i] - buyprice
+            if prices[i] < buyprice:
+                buyprice = prices[i]
+            elif bestprofit < prices[i] - buyprice:
+                bestprofit = prices[i] - buyprice
         return bestprofit
