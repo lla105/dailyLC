443e9461bcb90cbb6f212cdf9a1061abce92e0ae
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-22 16:35:09 -0700

    Time: 31 ms (89.58%) | Memory: 16.5 MB (38.91%) - LeetSync

---
+++ 202-happy-number/happy-number.py
@@ -0,0 +1,34 @@
+def strToList(n):
+    lst = []
+    for i in range(len(n)):
+        lst.append(n[i])
+    return lst
+
+def squareDaList(lst):
+    for i in range(len(lst)):
+        num = int(lst[i])
+        lst[i] = int(num*num)
+    return lst
+ 
+def sumList(lst):
+    summ = 0
+    for i in range(len(lst)):
+        summ += lst[i]
+        # print(summ)
+    return summ
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        s = set()
+        summ = 0
+        # lst = str(n)
+        lst = strToList(str(n))
+        while summ!=1:
+            lst = squareDaList(lst)
+            summ = sumList(lst)
+            if summ in s:
+                return False
+            s.add(summ)
+            lst = strToList(str(summ))
+            print(">>>", summ)
+        return True
