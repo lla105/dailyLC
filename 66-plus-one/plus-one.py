f205e2fa9fd3e16ec9bdaa93b4c53a938ebdacef
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-21 21:53:27 -0700

    Time: 42 ms (19.29%) | Memory: 16.8 MB (10.35%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -4,7 +4,7 @@ class Solution:
         for i in range(len(digits)-1, -1, -1):
             print(i)
             if digits[i]==10 and i==0:
-                print("a", digits[i])
+                # print("a", digits[i])
 
                 digits.insert(0,1)
                 digits[1] = 0
@@ -12,7 +12,7 @@ class Solution:
 
             elif digits[i]==10:
                 digits[i] = 0
-                print("b: ", digits[i])
+                # print("b: ", digits[i])
                 digits[i-1] = digits[i-1] + 1
         return digits
         
