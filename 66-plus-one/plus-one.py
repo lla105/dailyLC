25868fc06e46b567a57042c85cb6a59624df919d
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-21 21:53:38 -0700

    Time: 26 ms (98.07%) | Memory: 16.5 MB (40.19%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -4,26 +4,10 @@ class Solution:
         for i in range(len(digits)-1, -1, -1):
             print(i)
             if digits[i]==10 and i==0:
-                # print("a", digits[i])
-
                 digits.insert(0,1)
                 digits[1] = 0
-                # print("a", digits[1])
-
             elif digits[i]==10:
                 digits[i] = 0
-                # print("b: ", digits[i])
                 digits[i-1] = digits[i-1] + 1
         return digits
         
-        # [6,7,8,9]
-        # [6,7,9,0]
-
-
-        # [9,9,9]
-        # [0,0,0]
-        # [1,0,0,0]
-
-        # templist = [1,2,3]
-        # templist.insert(0,7)
-        # print(templist)
