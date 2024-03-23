5ee9aedbcb8e1946042abd56e20f6f2eb895d5fa
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-23 00:53:09 -0700

    Time: 38 ms (46.39%) | Memory: 16.7 MB (12.06%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,11 +1,11 @@
 class Solution:
     def helper(self, nums):
-        print(">>>", nums)
+        # print(">>>", nums)
         dp = [0] * (len(nums)+2)
         # print(dp)
         for i in range(len(nums)-1, -1, -1):
             dp[i] = max(nums[i]+dp[i+2] , dp[i+1])
-            print(dp)
+            # print(dp)
         return max(dp)
     def rob(self, nums: List[int]) -> int:
         if len(nums) == 1:
