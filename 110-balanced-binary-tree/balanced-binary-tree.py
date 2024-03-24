50fbdbe84c184bdf932ffd99efa80a826a437478
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-23 22:02:31 -0700

    Time: 1635 ms (5.01%) | Memory: 20.6 MB (17.39%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -1,16 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
+    leftscore = 0
+    rightscore = 0
+    isbalanced = True
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        def dfs(node):
+        print(root)
+        def search(node):
             if not node:
                 return 0
-            
-            left_height = dfs(node.left)
-            right_height = dfs(node.right)
-            
-            # Check balance condition at each node
-            if abs(left_height - right_height) > 1:
-                return float('-inf')  # Return a sentinel value if unbalanced
-            
-            return max(left_height, right_height) + 1
+            left = search(node.left)
+            right = search(node.right)
+            print(f'{node.val} : {left} - {right}')
+            if abs(left - right) > 1:
+                self.isbalanced = False
+            self.leftscore = left
+            self.rightscore = right
+            return 1+ max(left, right)
+
+        search(root)
+        # print(self.leftscore, self.rightscore, "=", abs(self.leftscore - self.rightscore))
+        if self.isbalanced:
+            return True
+        else:
+            return False
         
-        return dfs(root) != float('-inf')
+
+        
