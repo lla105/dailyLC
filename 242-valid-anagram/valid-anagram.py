bdc5e441b598fc5b156e2c054680d40914fb412e
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-24 23:34:00 -0700

    Time: 51 ms (49.29%) | Memory: 17.7 MB (11.48%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,26 +1,6 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        if len(s) != len(t):
-            return False
-        sdic = {}
-        for char in s:
-            if char in sdic:
-                sdic[char] += 1
-            else:
-                sdic[char] = 1
-        # print(sdic)
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s = sorted(s)
+        t = sorted(t)
 
-        for char in t:
-            if char in sdic:
-                sdic[char] -= 1
-                if sdic[char] <=0:
-                    sdic.pop(char)
-            else:
-                return False
-
-        return True
+        return s==t
