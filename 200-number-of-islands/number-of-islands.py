3a592785e8d4ee358f8dea929817e7dec072dec8
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-24 23:26:15 -0700

    Time: 227 ms (87.92%) | Memory: 18.9 MB (60.95%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,30 +1,25 @@
-def dfs(grid, i , j ):
-    # print('test')
-    if i<0 or j<0:
-        print("a)")
-        return
-    if i>=len(grid) or j>=len(grid[0]):
-        print("b)")
-        return
-    if grid[i][j]!='1':
-        print("c)")
-        return
-    # if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] != '1':
-    #     return
-    grid[i][j] = '#'
-    dfs(grid, i+1, j)
-    dfs(grid, i-1, j)
-    dfs(grid, i, j+1)
-    dfs(grid, i, j-1)
-
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        def runbfs(grid, i, j):
+            if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]):
+                return 
+            if grid[i][j] != '1':
+                return
+            grid[i][j] = '#'
+            runbfs(grid,i+1,j)
+            runbfs(grid,i,j+1)
+            runbfs(grid,i-1,j)
+            runbfs(grid,i,j-1)
+            # return
+
+
         count = 0
         for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                print(grid[i][j], end = '')
+            for j in range(len(grid[0])):
+                # print(grid[i][j],end='')
                 if grid[i][j] == '1':
-                    dfs(grid, i, j)
+                    runbfs(grid, i , j)
                     count += 1
             print("")
+
         return count
