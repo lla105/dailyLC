083d602bcfdb4f86e5e79caca15f4081370ee9cb
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-26 14:49:55 -0700

    Time: 38 ms (44.65%) | Memory: 16.5 MB (44.63%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,20 +1,15 @@
 class Solution:
-    def solveTAB(self, nums, n):
-        dp = [0] * (n + 2)
-
-        for i in range(n - 1, -1, -1):
-            dp[i] = max(nums[i] + dp[i + 2], dp[i + 1])
-
-        return dp[0]
-
     def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
 
-        return self.solveTAB(nums, len(nums))
+        cur = 0
+        prev = 0
 
-        # if len(nums)==1:
-        #     return nums[0]
-        # for i in range(2, len(nums)):
-        #     nums[i] += nums[i-2]
-        #     # print(nums)
+        for money in nums:
+            print(f'max({money}+{prev} , {cur})')
+            temp = cur
+            cur = max(money+prev,cur)
+            prev = temp
 
-        # return max(nums[-1], nums[-2])
+        return cur
