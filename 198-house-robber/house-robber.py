4410768df4a118f3dd838abfb495df06ac3da579
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-26 15:21:22 -0700

    Time: 26 ms (97.53%) | Memory: 16.6 MB (44.63%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -13,10 +13,10 @@ class Solution:
         dp[0] = nums[0]
         dp[1] = nums[1]
         dp[2] = max(nums[1] , nums[2]+dp[0])
-        print("DP: ", dp)
-        print("nums: ", nums)
+        # print("DP: ", dp)
+        # print("nums: ", nums)
         for i in range(3, len(nums)):
-            print(f'{i} : max( {dp[i-2]}+{nums[i]} , {dp[i-1]}')
+            # print(f'{i} : max( {dp[i-2]}+{nums[i]} , {dp[i-1]}')
             dp[i] = max(dp[i-2]+nums[i] , dp[i-1], dp[i-3]+nums[i])
         print(dp)
         # print(dp[-1] , dp[-2])
