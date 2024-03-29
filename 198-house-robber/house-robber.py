aae22ecf6f63b9e04e713a3c3ef4b9e925ae4f2a
lla105 <105393685+lla105@users.noreply.github.com>
2024-03-29 00:17:57 -0700

    Time: 43 ms (11.70%) | Memory: 16.6 MB (11.94%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,24 +1,21 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        if len(nums)==1:
-            return nums[-1]
-        if len(nums)==2:
-            return max(nums[0] , nums[1])
+        if len(nums)==0: return 0
+        if len(nums)==1: return nums[-1]
+        if len(nums)==2: return max(nums[-1], nums[-2])
+        # if len(nums)==0 return 0
 
-        dp = []
-        for i in range(len(nums)):
-            dp.append(0)
+        dp = [0] * len(nums)
         dp[0] = nums[0]
         dp[1] = nums[1]
-        dp[2] = max(nums[1] , nums[2]+dp[0])
-        # print("DP: ", dp)
-        # print("nums: ", nums)
+        dp[2] = max(nums[2]+dp[0] , dp[1])
+        
+        result = 0
         for i in range(3, len(nums)):
-            # print(f'{i} : max( {dp[i-2]}+{nums[i]} , {dp[i-1]}')
-            dp[i] = max(dp[i-2]+nums[i] , dp[i-1], dp[i-3]+nums[i])
+            # print(i)
+            dp[i] = max(nums[i]+dp[i-2] , nums[i]+dp[i-3] , dp[i-1])
+
+
         print(dp)
-        # print(dp[-1] , dp[-2])
-        # print(max(dp[-1], dp[-2ll]))
-        return max(dp[-1] , dp[-2])
+
+        return max(dp[-2] , dp[-1])
