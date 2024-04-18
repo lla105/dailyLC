4d69a8117fe67122dc533ea4acef373e68ebc60e
lla105 <105393685+lla105@users.noreply.github.com>
2024-04-17 22:10:56 -0700

    Time: 80 ms (92.37%) | Memory: 20.1 MB (60.14%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,12 +1,7 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # dic format : { 'ant' : ['nat', 'tan']}
-        # dic['abc'].append('acb')
-        # dic['abc'].append('abc')
-        # if 'abc' in dic:
-        #     print("yeaaaa")
-        dic = defaultdict(list)
-
+        # dic = defaultdict(list)
+        dic = dict()
         for i in range(len(strs)):
             word = strs[i]
             sortedword = ''.join(sorted(word))
@@ -14,10 +9,12 @@ class Solution:
                 dic[sortedword].append(word)
             else:
                 dic[sortedword] = [word]
+
         print("DIC TION ARY ;;:::: ", dic)
         result = []
         for i,v in dic.items():
-            # print(f"{i} , {v}")
+            # print(i,v)
             result.append(v)
-        print(dic)
+
         return result
+
