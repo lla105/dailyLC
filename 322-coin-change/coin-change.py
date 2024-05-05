c65dc3638680a47dd9c87234ca12dcd112f980e1
lla105 <105393685+lla105@users.noreply.github.com>
2024-05-04 20:47:00 -0700

    Time: 797 ms (56.30%) | Memory: 17.2 MB (38.72%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,25 +1,15 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        # def countTurns(ba, coins,i, amount):
-            
+        dp = [ float('inf') ] * (amount+1)
+        dp[0] = 0
 
-        if amount==0 and coins:
-            return 0
-        ba = [ float('inf') ] * (amount+1)
-        ba[0] = 0
-        print("BA init: ", ba)
+        print(dp)
         for i in range(1, amount+1):
             for coin in coins:
                 if coin<=i:
-                    # countTurns(ba, coin,i, amount)
-                    ba[i] = min(ba[i] , ba[i-coin]+1)
-                    # print(i,coin,ba)
-
-                    # ba[i] = 99
-                # print(ba)
-
-
-        if ba[-1]%1 == 0:
-            return ba[-1]
+                    dp[i] = min(dp[i] , dp[i-coin] +1 )
+                
+        if dp[-1]%1==0:
+            return dp[-1]
         else:
             return -1
