faab632ea370c96288fe18883021357615c3e57d
lla105 <105393685+lla105@users.noreply.github.com>
2024-05-07 16:47:14 -0700

    Time: 228 ms (83.09%) | Memory: 18.8 MB (97.79%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,25 +1,25 @@
+def runbfs(i,j,grid):
+    if i>=len(grid) or j>=len(grid[0]):
+        return
+    if i<0 or j<0:
+        return
+    if grid[i][j] != '1':
+        return
+    grid[i][j] = "#"
+    runbfs(i+1, j, grid)
+    runbfs(i, j+1, grid)
+    runbfs(i-1, j, grid)
+    runbfs(i, j-1, grid)
+
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def runbfs(grid, i, j):
-            if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]):
-                return 
-            if grid[i][j] != '1':
-                return
-            grid[i][j] = '#'
-            runbfs(grid,i+1,j)
-            runbfs(grid,i,j+1)
-            runbfs(grid,i-1,j)
-            runbfs(grid,i,j-1)
-            # return
-
-
         count = 0
+
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                # print(grid[i][j],end='')
                 if grid[i][j] == '1':
-                    runbfs(grid, i , j)
+                    runbfs(i,j,grid)
                     count += 1
-            print("")
 
         return count
