f815228dc724d6125c11df92d34b5fb6eeef2fdd
lla105 <105393685+lla105@users.noreply.github.com>
2024-05-14 16:41:07 -0700

    Time: 57 ms (44.33%) | Memory: 16.6 MB (48.52%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -7,7 +7,6 @@ class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         cur1 = l1
         cur2 = l2
-
         s1 = []
         s2 = []
 
@@ -21,23 +20,18 @@ class Solution:
         for i in range(len(s1)-1, -1, -1):
             temp1 *= 10
             temp1 += s1[i]
-        # print(temp1)
         temp2 = 0
         for i in range(len(s2)-1, -1, -1):
             temp2 *= 10
             temp2 += s2[i]
-        summ = temp1+temp2
-        print(summ)
-        ssum = str(summ)
-        # print(len(ssum))
-        resultlist = []
+        tempsum = temp1 + temp2
+        print(temp1 , "+", temp2, "=", tempsum)
 
+        resultlist = []
+        sums = str(tempsum)
         dummy = ListNode()
         curr = dummy
-
-        for i in range(len(ssum)-1, -1, -1):
-            # print(ssum[i])
-            curr.next = ListNode(int(ssum[i]))
+        for i in range(len(sums)-1, -1, -1):
+            curr.next = ListNode(int(sums[i]))
             curr = curr.next
-
         return dummy.next
