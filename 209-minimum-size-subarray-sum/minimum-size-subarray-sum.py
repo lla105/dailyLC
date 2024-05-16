e4cc4d2c411b31655b45b4f4f63a8aff95128eff
lla105 <105393685+lla105@users.noreply.github.com>
2024-05-15 21:40:21 -0700

    Time: 273 ms (5.01%) | Memory: 29.6 MB (99.20%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -1,12 +1,22 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        if sum(nums)<target:
+            return 0
+        l = 0
+        count = len(nums) + 1
+        tempsum = 0
+        
+        for i in range(len(nums)):
+            tempsum += nums[i]
+            print('1.1tempsum : ', tempsum)
 
-        res = len(nums)+1
-        curSum, l = 0,0
-        for r, n in enumerate(nums):
-            curSum += n
-            while curSum >= target:
-                res = min(res, r-l+1)
-                curSum -= nums[l]
-                l += 1
-        return res % (len(nums)+1)
+            while tempsum >=target:
+                print('  tempsum>target, count = ', count,'vs',i-l+1)
+                count = min(count, i-l+1)
+                tempsum -= nums[l]
+                l+=1
+                print('1.2 tempsum : ', tempsum)
+
+        # arr.pop(0)
+        # print(/)
+        return count
