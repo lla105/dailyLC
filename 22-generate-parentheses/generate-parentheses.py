28daa3d5313d0cfb78968889103d2422f5804207
lla105 <105393685+lla105@users.noreply.github.com>
2024-05-17 18:02:07 -0700

    Time: 54 ms (5.15%) | Memory: 16.9 MB (42.57%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,29 +1,24 @@
-
-
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        # d = { ')':'(', ']':'[' , '}':'{' }
-        # open >= closed
-        # n >= open
         stack = []
-        result = []
-        def trav(open,closed):
-            if open==n and closed==n:
-                result.append(''.join(stack))
-                print('1', stack)
-                # stack.pop()
-                return
-            if open<n and closed<n:
-                stack.append('(')
-                print('2', stack)
-                trav(open+1, closed)
-                stack.pop()
+        resultlist = []
 
-            if open<=n and closed<open:
-                stack.append(')')
-                print('3', stack)
-                trav(open,closed+1)
+        def trav(open, close):
+            if open==n and close==open:
+                print('1:', stack)
+                resultlist.append(''.join(stack))
+                return 
+            if open<n and close<n:
+                stack.append('(')
+                print('2:', stack)
+                trav(open+1, close)
                 stack.pop()
-                
+            if close<open and open<=n:
+                stack.append(')')
+                print('3:', stack)
+                trav(open,close+1)
+                stack.pop()
+            
+        
         trav(0,0)
-        return result
+        return resultlist
