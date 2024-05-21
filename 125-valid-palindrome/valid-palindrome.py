00d420353f4bdbc4ae5310a212e37cc5b9014c95
lla105 <105393685+lla105@users.noreply.github.com>
2024-05-20 23:46:27 -0700

    Time: 71 ms (5.01%) | Memory: 17.5 MB (33.66%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,19 +1,21 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        length = len(s)
-        word = []
-        for i in range(length):
-            char = s[i]
-            if char.isalpha() or char.isdigit():
-                word.append(char.lower())
-        word = ''.join(word)
-        print(f'{word}')
+        left = 0
+        # tempset = {' ', ',', '.', ':', ';', '@', '#'}
+        news = s
+        s = ''
+        for i in range(len(news)):
+            # if news[i] not in tempset:
+            if news[i].isalpha() or news[i].isdigit():
+                s += news[i].lower()
+        right = len(s)-1
 
-        for i in range(len(word)):
-
-            # print(f'{i} : {word[i]} vs {word[-i-1]}')
-
-            if word[i]!=word[-i-1]:
+        print(s)
+        while left<right:
+            print(s[left].lower(), s[right].lower())
+            if s[left].lower() != s[right].lower() :
+                # print(s[left].lower(), s[right].lower())
                 return False
-
+            left+=1
+            right -=1
         return True
