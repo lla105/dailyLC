3ecaa9382d42cc03ef6ab953e44f8b77130eaae3
lla105 <105393685+lla105@users.noreply.github.com>
2024-05-22 22:12:42 -0700

    Time: 66 ms (61.67%) | Memory: 16.8 MB (21.16%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,18 +1,19 @@
 class Solution:
-    def fill(self, image, sr, sc, color, cur):
-        if sr<0 or sc<0 or sr>=len(image) or sc>=len(image[0]):
-            return
-        if cur != image[sr][sc]:
-            return
-        image[sr][sc] = color
-        self.fill(image,sr+1,sc,color,cur)
-        self.fill(image,sr-1,sc,color,cur)
-        self.fill(image,sr,sc+1,color,cur)
-        self.fill(image,sr,sc-1,color,cur)
-
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        if color==image[sr][sc]:
+        rows, cols = len(image), len(image[0])
+        color_to_replace = image[sr][sc]
+
+        if color_to_replace == color:
             return image
-        self.fill(image,sr,sc,color, image[sr][sc])
+
+        def dfs(r, c):
+            if r < 0 or r >= rows or c < 0 or c >= cols or image[r][c] != color_to_replace:
+                return
+            image[r][c] = color
+            dfs(r + 1, c)  # down
+            dfs(r - 1, c)  # up
+            dfs(r, c + 1)  # right
+            dfs(r, c - 1)  # left
+
+        dfs(sr, sc)
         return image
-        
