e0e5b434b73388b9694e6446b0fa190731380ae5
lla105 <105393685+lla105@users.noreply.github.com>
2024-05-23 00:08:40 -0700

    Time: 77 ms (6.32%) | Memory: 16.6 MB (75.50%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,19 +1,20 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        rows, cols = len(image), len(image[0])
-        color_to_replace = image[sr][sc]
-
-        if color_to_replace == color:
+        if image[sr][sc] == color:
             return image
+        originalcolor = image[sr][sc]
 
-        def dfs(r, c):
-            if r < 0 or r >= rows or c < 0 or c >= cols or image[r][c] != color_to_replace:
+        def dfs(i,j):
+            if i<0 or j<0 or i>=len(image) or j>=len(image[0]):
+                return 
+            if image[i][j] != originalcolor:
                 return
-            image[r][c] = color
-            dfs(r + 1, c)  # down
-            dfs(r - 1, c)  # up
-            dfs(r, c + 1)  # right
-            dfs(r, c - 1)  # left
-
-        dfs(sr, sc)
+            image[i][j] = color
+            dfs(i+1,j)
+            dfs(i-1,j)
+            dfs(i,j+1)
+            dfs(i,j-1)
+            return
+        
+        dfs(sr,sc)
         return image
