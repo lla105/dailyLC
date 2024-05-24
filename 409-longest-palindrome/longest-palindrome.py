3104d39381d43e5fba29cf553a385cc360e0f8eb
lla105 <105393685+lla105@users.noreply.github.com>
2024-05-23 17:45:28 -0700

    Time: 35 ms (70.25%) | Memory: 16.7 MB (8.34%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -1,18 +1,31 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        usedsingle = False
+        d = Counter(s)
         maxlen = 0
-        d1 = Counter(s)
-        for i,v in d1.items():
-            print(i,v)
-            if v%2==0:
-                maxlen+=v
-            elif v%2!=0: #is an odd number
-                if v/2>=1:
-                    maxlen+=v
-                    maxlen-=1
-                if usedsingle==False:
-                    maxlen+=1
-                    usedsingle = True
-            print(maxlen)
+        middleUsed = False
+
+        for i,v in d.items():
+            # aab -> aba
+            # aabc --> a_a
+            # aaac --> aca
+            print(i,maxlen)
+            if v%2==0: #v is even
+                maxlen += v
+            else: # v is odd
+                # v is 1
+                if v==1:
+                    if middleUsed==False:
+                        maxlen += 1
+                else:
+                    if middleUsed==False:
+                        maxlen += v
+                    else:
+                        maxlen += v
+                        maxlen -= 1
+                print(i,v)
+                # v is >1 odds, eg: 5
+
+                middleUsed=True
+            print(i,maxlen)
+            
         return maxlen
