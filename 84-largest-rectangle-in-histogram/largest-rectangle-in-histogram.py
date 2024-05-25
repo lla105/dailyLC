1e06b12a01f275ace514a89717329f2a08b3c3ca
lla105 <105393685+lla105@users.noreply.github.com>
2024-05-24 18:50:44 -0700

    Time: 687 ms (32.56%) | Memory: 32.9 MB (47.98%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -2,25 +2,32 @@ class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         stack = []
         maxarea = 0
-
         for i in range(len(heights)):
-            h = heights[i]
+            curheight = heights[i]
             startindex = i
-
-            while len(stack)>0 and stack[-1][0]>h:
-                oldh, oldi = stack.pop()
-                temparea = oldh * (i-oldi)
+            while len(stack)>0 and stack[-1][0] > curheight:
+                # print('entering loop with: ', i,':',curheight)
+                tempwidth = i - stack[-1][1]
+                temparea = tempwidth * stack[-1][0]
                 maxarea = max(maxarea, temparea)
-                startindex = oldi
+                startindex = stack[-1][1]
+                stack.pop()
 
-            stack.append( (h,startindex) )
+            stack.append( (curheight, startindex))
+            # print('stack: ', stack)
+            # print('maxarea so far: ', maxarea)
+        maxstacklength = len(stack) 
 
-        print(">>>", stack)
+        # for i in range(len(stack)):
+        #     tempwidth = maxstacklength - i
+        #     temparea = tempwidth * stack[i][0]
+        #     maxarea = max(maxarea, temparea)
+
         for i in range(len(stack)):
-            currh = stack[i][0]
-            curri = stack[i][1]
-            # print(f'{currh} * {len(stack)} - {curri}')
-            temparea = currh * (len(heights) - curri )
+            curheight = stack[i][0]
+            curindex = stack[i][1]
+            temparea = curheight * (len(heights) - curindex)
             maxarea = max(temparea, maxarea)
-
+        print()
+        print('maxarea final : ', maxarea)
         return maxarea
