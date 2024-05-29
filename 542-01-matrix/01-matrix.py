8ce1516c4929aafb5b20250daec2df48a3976314
lla105 <105393685+lla105@users.noreply.github.com>
2024-05-29 15:36:16 -0700

    Time: 417 ms (81.29%) | Memory: 19.3 MB (85.38%) - LeetSync

---
+++ 542-01-matrix/01-matrix.py
@@ -0,0 +1,25 @@
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        m = len(mat)
+        n = len(mat[0])
+        q = deque()
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j]==0:
+                    q.append((i,j))
+                else:
+                    mat[i][j]=m*n
+        # print(mat)
+        directions = [ (1,0),(-1,0),(0,1),(0,-1)]
+        while q:
+            oldi, oldj = q.popleft()
+            for di,dj in directions:
+                newi,newj = oldi+di,oldj+dj
+                # if newi<0 or newj<0 or newi>=m or newj>=n:
+                #     continue
+                # if mat[newi][newj]==0:
+                #     continue
+                if 0<=newi<m and 0<=newj<n and mat[newi][newj]>mat[oldi][oldj]+1:
+                    mat[newi][newj]=mat[oldi][oldj]+1
+                    q.append((newi,newj))
+        return mat
