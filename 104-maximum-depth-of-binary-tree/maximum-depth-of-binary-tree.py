d4652279a2d48f61a42287289ad78a425ab9112c
lla105 <105393685+lla105@users.noreply.github.com>
2024-05-28 22:53:19 -0700

    Time: 37 ms (75.70%) | Memory: 17.6 MB (82.95%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -11,15 +11,15 @@ class Solution:
         maxD = 0
         def dfs(node, maxD):
             if node is None:
-                print('.  returning 0')
+                # print('.  returning 0')
                 return 0
-            print(f'[{node.val}] : going left')
+            # print(f'[{node.val}] : going left')
             leftH = dfs(node.left, maxD)
-            print(f'[{node.val}] : going right')
+            # print(f'[{node.val}] : going right')
             rightH = dfs(node.right, maxD)
             tempHighest = max(leftH, rightH)
             maxD = max(tempHighest, maxD)
-            print(f'[{node.val}] maxD+1 : f{maxD+1}')
+            # print(f'[{node.val}] maxD+1 : f{maxD+1}')
             return maxD + 1
         maxD2 = dfs(root, maxD)
         return maxD2
