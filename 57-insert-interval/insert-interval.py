c253dbe1589f60e762fdfd74edfa524e02620c4d
lla105 <105393685+lla105@users.noreply.github.com>
2024-05-28 22:44:29 -0700

    Time: 73 ms (68.73%) | Memory: 19.8 MB (75.74%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,21 +1,24 @@
 class Solution:
-    def insert(self, intervals: List[List[int]], newIntervals: List[int]) -> List[List[int]]:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         result = []
         i = 0
-        while i<len(intervals) and intervals[i][1]<newIntervals[0]:
+        while i<len(intervals) and intervals[i][1]<newInterval[0]:
             result.append(intervals[i])
             i+=1
-        
-        # the remaining intervals[i][1] are > newIntervals[0]
-        while i<len(intervals) and newIntervals[1]>=intervals[i][0]:
-            newIntervals[0] = min(newIntervals[0] , intervals[i][0])
-            newIntervals[1] = max(newIntervals[1] , intervals[i][1])
+        # intervals = [3,5],[12,13]
+        # newinterval = [4,10] or [2,10]   
+        tempinterval = newInterval     
+        while i<len(intervals) and intervals[i][0]<=newInterval[1]:
+            tempinterval[0] = min(tempinterval[0], intervals[i][0])
+            tempinterval[1] = max(tempinterval[1], intervals[i][1])
             i+=1
-    
-        result.append(newIntervals)
+        result.append(tempinterval)
+        print('result : ', result)
 
         while i<len(intervals):
             result.append(intervals[i])
             i+=1
+        print('result 2 : ', result)
+        
 
         return result
