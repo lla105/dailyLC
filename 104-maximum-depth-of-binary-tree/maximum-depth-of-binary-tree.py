b38e19f5bbf4b2459e48bde7b23826a8c3daaba7
lla105 <105393685+lla105@users.noreply.github.com>
2024-05-29 21:32:41 -0700

    Time: 42 ms (43.40%) | Memory: 17.6 MB (47.89%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -8,18 +8,14 @@ class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if root is None:
             return 0
-        maxD = 0
-        def dfs(node, maxD):
+        maxD = float('-inf')
+
+        def trav(node,maxD):
             if node is None:
-                # print('.  returning 0')
                 return 0
-            # print(f'[{node.val}] : going left')
-            leftH = dfs(node.left, maxD)
-            # print(f'[{node.val}] : going right')
-            rightH = dfs(node.right, maxD)
-            tempHighest = max(leftH, rightH)
-            maxD = max(tempHighest, maxD)
-            # print(f'[{node.val}] maxD+1 : f{maxD+1}')
-            return maxD + 1
-        maxD2 = dfs(root, maxD)
-        return maxD2
+            leftH = trav(node.left, maxD)
+            rightH = trav(node.right, maxD)
+            temp = max(leftH, rightH)
+            maxD = max(maxD, temp)
+            return maxD+1
+        return trav(root, maxD)
