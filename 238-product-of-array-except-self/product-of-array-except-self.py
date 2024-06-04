0f2424aec1a94ce765e24e43adc6104250cd7149
lla105 <105393685+lla105@users.noreply.github.com>
2024-06-03 17:39:39 -0700

    Time: 297 ms (9.93%) | Memory: 26.6 MB (6.35%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,23 +1,26 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        right = []
-        left = []
-        result = []
-        for i in range(len(nums)):
-            right.append(1)
-            left.append(1)
+        # nums = [1,       2,         3,       4,5]
+        # nums = [num2~5,  num1+num3*5, num1*2 * num4*5]
+        # left = product of all #s left of cur num
+        # right = product of all #s right of cur num
+
+        # nums = [1,2,3,4]
+        # right = [24,12,4,1]
+        # left = [1,1,2,6]
+        left = [1] * len(nums)
         for i in range(1, len(nums)):
-            left[i] = nums[i-1] * left[i-1]
+            left[i] = left[i-1] * nums[i-1]
         print(left)
-        for i in range(len(nums)-2, -1, -1):
-            right[i] = nums[i+1] * right[i+1]
+
+        right = [1] * len(nums)
+        for i in range(len(nums)-2, -1 , -1):
+            right[i] = right[i+1] * nums[i+1]
         print(right)
+        resultlist = []
         for i in range(len(nums)):
-            result.append(0)
-            result[i] = left[i] * right [i]
+            temp = right[i] * left[i]
+            resultlist.append(temp)
+        print(nums)
 
-        return result
-
-
-# right = [24,12,4,1]
-# left. = [1, 1, 2,6]
+        return resultlist
