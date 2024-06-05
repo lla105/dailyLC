650aa97ffe9f926422134d2d673d7be64f6f9034
lla105 <105393685+lla105@users.noreply.github.com>
2024-06-04 21:49:43 -0700

    Time: 54 ms (6.83%) | Memory: 18.3 MB (78.28%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -6,16 +6,23 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        if root is None:
+            return None
         self.isvalid = True
-        def trav(node,low, high):
+
+        def trav(node, low, high):
             if node is None:
-                return True
+                return 
+            leftval = trav(node.left, low, node.val)
+            rightval = trav(node.right, node.val , high)
+            # if leftval is None: 
+            #     leftval=float('-inf')
+            # if rightval is None:    
+            #     rightval=float('inf')
+            print(leftval , node.val , rightval)
+            # if leftval>=node.val or node.val>=rightval:
             if low>=node.val or node.val>=high:
                 self.isvalid = False
-                return False
-            lefttree = trav(node.left , low, node.val)
-            righttree = trav(node.right , node.val, high)
-
             return 
-        trav(root, float('-inf') , float('inf'))
+        trav(root, float('-inf') , float('inf') )
         return self.isvalid
