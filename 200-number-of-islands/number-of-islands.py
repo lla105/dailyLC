58d48de43a8efd55f507e226ee7843c48642b4d3
lla105 <105393685+lla105@users.noreply.github.com>
2024-06-04 17:53:48 -0700

    Time: 231 ms (75.54%) | Memory: 18.9 MB (53.52%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,25 +1,29 @@
-def runbfs(i,j,grid):
-    if i>=len(grid) or j>=len(grid[0]):
-        return
-    if i<0 or j<0:
-        return
-    if grid[i][j] != '1':
-        return
-    grid[i][j] = "#"
-    runbfs(i+1, j, grid)
-    runbfs(i, j+1, grid)
-    runbfs(i-1, j, grid)
-    runbfs(i, j-1, grid)
-
-
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        count = 0
+        if grid is None:
+            return None
 
+        def dfs(grid,i,j):
+
+            if i>=len(grid) or j>=len(grid[0]):
+                return 
+            if i<0 or j<0 :
+                return
+            # if grid[i][j]=='0':
+            if grid[i][j] != '1':
+                return
+            grid[i][j] = "99"
+            dfs(grid,i+1,j)
+            dfs(grid,i-1,j)
+            dfs(grid,i,j+1)
+            dfs(grid,i,j-1)
+            return 
+
+
+        islands = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == '1':
-                    runbfs(i,j,grid)
-                    count += 1
-
-        return count
+                if grid[i][j]=="1":
+                    dfs(grid,i,j)
+                    islands += 1
+        return islands
