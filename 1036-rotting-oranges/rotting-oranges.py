9b69b1688006a8182abb917c5dfb852635ce3359
lla105 <105393685+lla105@users.noreply.github.com>
2024-06-05 21:43:44 -0700

    Time: 50 ms (58.58%) | Memory: 16.4 MB (90.88%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,39 +1,41 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        # get all rotten cells i,j. put in a q.
-        # while q, for loop through direction
-        # append minutespassed for each while iternation
-        minutespassed = 0
-        freshcount=0
-        if grid is None: 
-            return -1
+        if grid is None:
+            return None
+        # gather initial rotten i,j
         rottenq = deque()
+        freshoranges = 0
+
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 2:
                     rottenq.append( (i,j) )
                 elif grid[i][j] == 1:
-                    freshcount +=1 
-        print(rottenq)
-        directions = [ (0,1), (1,0), (0,-1), (-1,0)]
-        while rottenq and freshcount >0:
-            minutespassed += 1
-            for each in range(len(rottenq)):
-                x,y = rottenq.popleft()
-                print(x,y )
+                    freshoranges += 1
+        minutesPassed = 0
+        directions = [ (0,1), (1,0), (0,-1), (-1,0) ]
+        while rottenq and freshoranges>0:
+            minutesPassed += 1
+            for index in range(len(rottenq)):
+                curx,cury = rottenq.popleft()
+                # curx = eachrotten[0]
+                # cury = eachrotten[1]
                 for dx,dy in directions:
-                    i = dx+x
-                    j = dy+y
+                    i = dx+curx
+                    j = dy+cury
                     if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]):
                         continue
-                    if grid[i][j]==2 or grid[i][j]==0:
+                    if grid[i][j]==0 or grid[i][j]==2:
                         continue
-                    # if grid[i][j] == 1:
-                    freshcount -=1 
-                    grid[i][j] = 2
+                    # if grid[i][j]==1:
                     rottenq.append( (i,j) )
-        # return minutespassed
-        if freshcount==0: 
-            return minutespassed
+                    grid[i][j] = 2
+                    freshoranges -= 1
+        # return minutesPassed
+        if freshoranges==0:
+            return minutesPassed
         else:
             return -1
+
+
+            
