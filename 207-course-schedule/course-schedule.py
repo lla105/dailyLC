069eedf40d625ff5d610726e90e870d17499735f
lla105 <105393685+lla105@users.noreply.github.com>
2024-06-06 17:47:03 -0700

    Time: 90 ms (54.21%) | Memory: 18.3 MB (49.33%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,26 +1,28 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        pre = defaultdict(list)
-        for course, p in prerequisites:
-            pre[course].append(p)
-        taken = set()
+        premap = defaultdict(list)
+        for i, v in prerequisites:
+            premap[i].append(v)
 
-        def dfs(course):
-            if not pre[course]:
-                return True
-            if course in taken:
-                return False
-            taken.add(course)
-
-            for p in pre[course]:
-                if not dfs(p): 
-                    return False
-            
-            pre[course] = []
-            return True
+        visited = set()  # Nodes that have been fully processed
+        visiting = set() # Nodes that are being visited in the current path
         
-        for course in range(numCourses):
-            if not dfs(course):
+        def dfs(node):
+            if node in visiting:
+                return True  # Found a cycle
+            if node in visited:
+                return False # No cycle from this node
+            
+            visiting.add(node)
+            for neighbor in premap[node]:
+                if dfs(neighbor):
+                    return True
+            visiting.remove(node)
+            visited.add(node)
+            return False
+
+        for n in range(numCourses):
+            if dfs(n):
                 return False
 
         return True
