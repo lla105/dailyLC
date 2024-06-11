f20007cef2ca7ec1455c18f7e5d0fc2ac1eec55f
lla105 <105393685+lla105@users.noreply.github.com>
2024-06-11 15:13:56 -0700

    Time: 57 ms (5.37%) | Memory: 16.7 MB (25.97%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -4,16 +4,14 @@ class Solution:
         dp = [False] * (n+1)
         dp[0] = True
 
-        for i in range(1,n+1):
-            print(i,") ", s[i-1])
+        for i in range(1, n+1):
+            print(i)
             for word in wordDict:
-                print('<<<<<', s[i-len(word):i] , ' vs ', word)
+                print(s[i-len(word) : i])
                 if i>=len(word):
-                    print('    3) length good')
-                    if s[i-len(word):i]==word:
-                        print('     4) word equal: ')
-                        print('        ', i,'-', len(word),'=',i-len(word))
-                        if dp[i-len(word)] is True:
+                    if s[i-len(word) : i] == word:
+                        print(' num is good!!!')
+                        if dp[i-len(word)] :
                             dp[i] = dp[i-len(word)]
-        print(dp)
+
         return dp[-1]
