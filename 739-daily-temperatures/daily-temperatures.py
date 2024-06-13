2d3845f44b806d378d5c5a99d6f00186d7876f9b
lla105 <105393685+lla105@users.noreply.github.com>
2024-06-13 10:12:49 -0700

    Time: 965 ms (6.73%) | Memory: 31.8 MB (28.69%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,18 +1,17 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = [] # format = [ (temp, index) , ...]
         t = temperatures
-        length = len(t)
-
-        result = [0]*length # format is : [ int, int , int ]
-        stack = [ ] # format is : [ [tempa,daya] , [tempb,dayb] , ..]
-
-        for curday, curtemp in enumerate(t):
-            while len(stack)!=0 and curtemp>stack[-1][0]  :
-                daydiff = abs(curday - stack[-1][1])
-                print(f'>>> {stack[-1][1]}')
-                result[stack[-1][1]] = daydiff
-                stack.pop(-1)
-
-            stack.append([curtemp,curday])
-            
+        result = [0] * len(t)
+        for i in range(len(t)):
+            if not stack:
+                stack.append( (t[i] , i) )
+            else:
+                # print(stack)
+                while len(stack)>0 and stack[-1][0] < t[i] :
+                    result[stack[-1][1]] = (i-stack[-1][1])
+                    stack.pop()
+                    # print(' while loop end')
+                stack.append( (t[i] , i) )
+        
         return result
