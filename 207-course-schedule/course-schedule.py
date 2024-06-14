edac11a7671696471f0e973633d682b9e4583653
lla105 <105393685+lla105@users.noreply.github.com>
2024-06-13 21:33:39 -0700

    Time: 96 ms (23.21%) | Memory: 18.3 MB (46.97%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,33 +1,29 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        d = defaultdict(list)
+        # 0=unvisited, 1=visiting, 2=visited
+        arr = [0] * numCourses
         PR = prerequisites
+        # build dictionary = { pre-req: [class1, class2, ...] }
+        d = defaultdict(list)
+
         for i in range(len(PR)):
             d[PR[i][1]].append(PR[i][0])
         # print(d)
-        visited = [0] * numCourses
         self.isCycle = False
-        def dfs(node):
-            if visited[node] == 1: #being visited, cycle detected
+
+        def bfs(classnum):
+            if arr[classnum] == 1: #is visiting
                 self.isCycle = True
-                return 
-            if visited[node] == 2: # node fully visited
-                return 
-            visited[node] = 1
+                return
+            if arr[classnum] == 2: #is visisted, cycle detected
+                return
+            arr[classnum] = 1
+            for eachClass in d[classnum]:
+                bfs(eachClass)
+            arr[classnum] = 2
+            return
 
-            for neighbor in d[node]:
-                dfs(neighbor)
-                # if not dfs(neighbor):
-                #     return False
-            visited[node] = 2
-            return 
-        for course in range(numCourses):
-            if visited[course] == 0:
-                dfs(course)
-                # if not dfs(course):
-                #     return False
-
-        if self.isCycle:
-            return False
-        else:
-            return True
+        for num in range(numCourses):
+            if arr[num] == 0:
+                bfs(num)
+        return not self.isCycle
