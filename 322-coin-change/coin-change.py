81323d3516183b8c07112b33ebb9b462cd336507
lla105 <105393685+lla105@users.noreply.github.com>
2024-06-13 23:13:11 -0700

    Time: 733 ms (81.42%) | Memory: 16.9 MB (57.05%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,26 +1,40 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-
-# eg amount = 11$
-# coins = [1,2,5]
-
-# arr = [0 , 0, 0, 0, ....,0] <- least coins req for each$
-#        0 , 1, 2, 3, 4, 5, 6, 7
-# arr = [0 , 1, 1, 2, 2, 1
-
-# coins[i] must be <= arr[i] 
-        arr = [ float('inf') ] * (amount+1)
-        arr[0] = 0
-        # arr[1] = 1
-        for i in range(len(arr)):
-            # print(i)
-            for j in range(len(coins)):
-                if coins[j] <= i:
-                    # print(' coin: ', coins[j])
-                    tempmin = arr[i-coins[j]] + 1
-                    arr[i] = min(arr[i], tempmin)
-            # print(" arr: ", arr)
-        if type(arr[-1]) is not float:
-            return arr[-1]
-        else:
+        dp = [ float('inf') ] * (amount+1)
+        dp[0] = 0
+        for i in range(1,amount+1):
+            for coin in coins:
+                if coin<=i:
+                    dp[i] = min(dp[i] , dp[i-coin]+1)
+        if dp[-1]%1 !=0 :
             return -1
+        else:
+            return dp[-1]
+
+
+        # target = amount
+        # self.answer = float('inf')
+        # self.lastusedcoin = 0
+        # self.usedcoins = set()
+        # def trav(coinindex, cursum, coincount):
+        #     print('1 New stack :', coinindex, cursum, coincount)
+        #     print('1.1 usedcoin: ', self.lastusedcoin)
+        #     if coins[coinindex]>coins[-1] or self.lastusedcoin>=len(coins):
+        #         print(' 2 Coin index over, returning')
+        #         return
+        #     if cursum>target:
+        #         print('  3 cursum > target. returning')
+        #         return
+        #     if cursum == target:
+        #         self.answer = min(self.answer, coincount)
+        #         print('   4 curusm==target! self.answer = ', self.answer)
+        #     for i in range(self.lastusedcoin, len(coins)):
+        #         trav(i, cursum+coins[i], coincount+1)
+        #     self.usedcoins.add(self.lastusedcoin)
+        #     self.lastusedcoin += 1
+        #     print('     5 end stack. returning')
+        # trav(0, 0, 0)
+        # if self.answer%1!=0:
+        #     return -1
+        # else:
+        #     return self.answer
