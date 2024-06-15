7b323e1699dbe6fff066f71574c1e823565349cc
lla105 <105393685+lla105@users.noreply.github.com>
2024-06-15 15:10:43 -0700

    Time: 783 ms (60.44%) | Memory: 16.8 MB (96.60%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -2,22 +2,22 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         if amount == 0:
             return 0
-        print('did not catch 0')
+        # print('did not catch 0')
         # amount  = 10
         # coins = [2,3,9]
         LeastCoin = [ float('inf') ] * (amount+1)
         LeastCoin[0] = 0
-        print(LeastCoin)
+        # print(LeastCoin)
 
         for i in range( 1, amount+1) :
-            print(i,") ")
+            # print(i,") ")
             for coin in coins : 
                 if coin <= i :
                     numberA = LeastCoin[i]
                     numberB = LeastCoin[i - coin] + 1
                     smallerNumber = min(numberA, numberB)
                     LeastCoin[i] = smallerNumber
-        print(LeastCoin)
+        # print(LeastCoin)
         # return LeastCoin[-1]
         if LeastCoin[-1] % 1 != 0 :
             # is a float
