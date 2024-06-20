5f00993057561c8d3300eb0a030d04d9353117ba
lla105 <105393685+lla105@users.noreply.github.com>
2024-06-20 00:00:38 -0700

    Time: 31 ms (96.75%) | Memory: 16.6 MB (91.04%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,13 +1,33 @@
+# class Solution:
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+        # result = []
+        # def trav(curpath , available):
+        #     if len(curpath) == len(nums) :
+        #         result.append(curpath)
+        #         return
+        #     for i in range(len(available)):
+        #         nextpath = curpath + [available[i]]
+        #         nextavailable = available[:i] + available[i+1:]
+        #         trav(nextpath , nextavailable)
+        # trav([] , nums)
+        # return result
+
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        result = []
-        def trav(curpath , available):
-            if len(curpath) == len(nums) :
-                result.append(curpath)
-                return
-            for i in range(len(available)):
-                nextpath = curpath + [available[i]]
-                nextavailable = available[:i] + available[i+1:]
-                trav(nextpath , nextavailable)
-        trav([] , nums)
-        return result
+        if len(nums) == 1:
+            return [nums[:]]
+        
+        res = []
+
+        for _ in range(len(nums)):
+            n = nums.pop(0)
+            perms = self.permute(nums)
+
+            for p in perms:
+                p.append(n)
+            
+            res.extend(perms)
+            nums.append(n)
+        
+        return res
+            
