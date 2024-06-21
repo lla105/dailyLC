0528eea6aacddea7116d9d4bc1a921b0638302f7
lla105 <105393685+lla105@users.noreply.github.com>
2024-06-20 17:32:04 -0700

    Time: 34 ms (66.34%) | Memory: 16.5 MB (73.66%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,29 +1,31 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if len(digits)==0:
-            return []
-        mappings = {
-            "2": "abc",
-            "3": "def",
-            "4": "ghi",
-            "5": "jkl",
-            "6": "mno",
-            "7": "pqrs",
-            "8": "tuv",
-            "9": "wxyz"
+        result = []
+        if not digits :
+            return result
+        
+        dic = {
+            '2':'abc',
+            '3':'def',
+            '4':'ghi',
+            '5':'jkl',
+            '6':'mno',
+            '7':'pqrs',
+            '8':'tuv',
+            '9':'wxyz',
         }
-
-        def tt(index, path):
-            #base case
-            if index == len(digits):
-                result.append(''.join(path))
+        def backtrack(index, curCombination):
+            if len(curCombination) == len(digits):
+                result.append(curCombination[:])
                 return
             
-            for char in mappings[digits[index]]:
-                path.append(char)
-                tt(index+1, path)
-                path.pop()
-            
-        result = []
-        tt(0,[])
+            # for i in range(len(digits[index])):
+            #     letter = dic[digits[index]]
+            # for letter in dic[digits[index]]:
+            for letter in dic[digits[index]]:
+                nextIndex = index+1
+                tempCombo = curCombination + letter
+                backtrack(nextIndex , tempCombo)
+                # backtrack(index + 1, curCombination + letter)
+        backtrack(0,"")
         return result
