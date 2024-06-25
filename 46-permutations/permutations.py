02d0496e0d842427997379dba046d1bd4413c312
lla105 <105393685+lla105@users.noreply.github.com>
2024-06-24 17:32:22 -0700

    Time: 46 ms (20.98%) | Memory: 16.6 MB (90.03%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,33 +1,16 @@
-# class Solution:
-#     def permute(self, nums: List[int]) -> List[List[int]]:
-        # result = []
-        # def trav(curpath , available):
-        #     if len(curpath) == len(nums) :
-        #         result.append(curpath)
-        #         return
-        #     for i in range(len(available)):
-        #         nextpath = curpath + [available[i]]
-        #         nextavailable = available[:i] + available[i+1:]
-        #         trav(nextpath , nextavailable)
-        # trav([] , nums)
-        # return result
-
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        if len(nums) == 1:
-            return [nums[:]]
-        
-        res = []
+        if not nums:
+            return []
+        self.result = []
+        def trav(available, curlist):
+            if len(curlist) == len(nums):
+                self.result.append(curlist)
+                return
+            for i in range(len(available)):
+                nextcurlist = curlist + [available[i]]
+                nextavailable = available[:i] + available[i+1:]
+                trav(nextavailable, nextcurlist)
 
-        for _ in range(len(nums)):
-            n = nums.pop(0)
-            perms = self.permute(nums)
-
-            for p in perms:
-                p.append(n)
-            
-            res.extend(perms)
-            nums.append(n)
-        
-        return res
-            
+        trav(nums, [])
+        return self.result
