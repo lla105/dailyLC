d1bf8e83cccf04f5bc56d54b4404b84bb9f6b2d7
lla105 <105393685+lla105@users.noreply.github.com>
2024-06-25 14:09:00 -0700

    Time: 1144 ms (5.00%) | Memory: 16.7 MB (31.23%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,20 +1,25 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        self.resultlist = []
-        s = set()
-        def trav(cursum, curlist):
-            if cursum == target:
-                # self.resultlist.append(curlist)
-                curlist.sort()
-                s.add( tuple(curlist) )
+        cand = candidates
+        t = target
+        if not cand:
+            return []
+
+        self.result = set()
+
+        def trav(curList, index):
+            if target==sum(curList):
+                curList = sorted(curList)
+                self.result.add( tuple(curList) )
                 return
-            if cursum > target:
+            if target<sum(curList):
                 return
-            for i in range(len(candidates)):
-                trav(cursum+candidates[i] , curlist+[candidates[i]])
-            return
-        trav(0,[])
-        for each in s:
-            # print(each)
-            self.resultlist.append(each)
-        return self.resultlist
+                
+            for i in range( len(cand)):
+                c = cand[i]
+                trav( curList+[c] ,0)
+
+        trav([] , 0)
+
+        return self.result
+            
