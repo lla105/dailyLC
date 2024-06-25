8a59922ea10f465bf1fc8b762af1d0b1a80b97c2
lla105 <105393685+lla105@users.noreply.github.com>
2024-06-24 22:59:43 -0700

    Time: 924 ms (34.35%) | Memory: 17 MB (43.20%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,27 +1,18 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        if amount == 0:
-            return 0
-        # print('did not catch 0')
-        # amount  = 10
-        # coins = [2,3,9]
-        LeastCoin = [ float('inf') ] * (amount+1)
-        LeastCoin[0] = 0
-        # print(LeastCoin)
+        if not coins: return 0
 
-        for i in range( 1, amount+1) :
-            # print(i,") ")
-            for coin in coins : 
-                if coin <= i :
-                    LeastCoin[i] = min(LeastCoin[i] , LeastCoin[i - coin] + 1)
-                    # numberA = LeastCoin[i]
-                    # numberB = LeastCoin[i - coin] + 1
-                    # smallerNumber = min(numberA, numberB)
-                    # LeastCoin[i] = smallerNumber
-        # print(LeastCoin)
-        # return LeastCoin[-1]
-        if LeastCoin[-1] % 1 != 0 :
-            # is a float
-            return -1
+        dp = [ float('inf') ] * (amount+1)
+        dp[0] = 0
+        print(dp)
+        for i in range(1, len(dp)):
+            # print(dp[i])
+            for j in range(len(coins)):
+                coin = coins[j]
+                if coin <= i:
+                    dp[i] = min(dp[i] , dp[i-coin]+1)
+
+        if dp[-1]%1 == 0:
+            return dp[-1]
         else:
-            return LeastCoin[-1]
+            return -1
