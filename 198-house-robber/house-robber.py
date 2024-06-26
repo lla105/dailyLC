e0f2880be4e91a2608bdd03e02ea662f901d8ba4
lla105 <105393685+lla105@users.noreply.github.com>
2024-06-26 14:44:27 -0700

    Time: 27 ms (95.80%) | Memory: 16.6 MB (8.96%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -6,17 +6,16 @@ class Solution:
             return nums[-1]
         if len(nums)==2:
             return max(nums[0] , nums[1])
-        dp = [0] * (len(nums))
+        dp = [0] * (len(nums) )
         dp[0] = nums[0]
         dp[1] = nums[1]
-        dp[2] = max( nums[2]+dp[0] , nums[1] )
+        dp[2] = nums[2] + nums[0]
 
-        for i in range(3,len(nums)):
-            prevsum1 = dp[i-1]
-            prevsum2 = dp[i-2] + nums[i]
-            prevsum3 = dp[i-3] + nums[i]
-            dp[i] = max(prevsum1 , prevsum2, prevsum3)
-
+        for i in range(3, len(nums)):
+            print(i)
+            house1 = dp[i-1]
+            house2 = nums[i] + dp[i-2]
+            house3 = nums[i] + dp[i-3]
+            dp[i] = max(house1, house2, house3)
         print(dp)
-
-        return max(dp[-1], dp[-2])
+        return max(dp[-1] , dp[-2])
