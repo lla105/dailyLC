414e567154d965a3f1f71f5cc3dd23913e65a3cc
lla105 <105393685+lla105@users.noreply.github.com>
2024-06-25 18:08:13 -0700

    Time: 29 ms (92.18%) | Memory: 16.5 MB (72.26%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,21 +1,22 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums)==0: return 0
-        if len(nums)==1: return nums[-1]
-        if len(nums)==2: return max(nums[-1], nums[-2])
-        # if len(nums)==0 return 0
-
-        dp = [0] * len(nums)
+        if not nums:
+            return 0
+        if len(nums)==1:
+            return nums[-1]
+        if len(nums)==2:
+            return max(nums[0] , nums[1])
+        dp = [0] * (len(nums))
         dp[0] = nums[0]
         dp[1] = nums[1]
-        dp[2] = max(nums[2]+dp[0] , dp[1])
-        
-        result = 0
-        for i in range(3, len(nums)):
-            # print(i)
-            dp[i] = max(nums[i]+dp[i-2] , nums[i]+dp[i-3] , dp[i-1])
+        dp[2] = max( nums[2]+dp[0] , nums[1] )
 
+        for i in range(3,len(nums)):
+            prevsum1 = dp[i-1]
+            prevsum2 = dp[i-2] + nums[i]
+            prevsum3 = dp[i-3] + nums[i]
+            dp[i] = max(prevsum1 , prevsum2, prevsum3)
 
         print(dp)
 
-        return max(dp[-2] , dp[-1])
+        return max(dp[-1], dp[-2])
