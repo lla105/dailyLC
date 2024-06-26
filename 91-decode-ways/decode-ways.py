329f6f7a3e2a56ad48f1df0a8426a865c9b1d3d1
lla105 <105393685+lla105@users.noreply.github.com>
2024-06-26 16:09:12 -0700

    Time: 34 ms (72.79%) | Memory: 16.6 MB (48.96%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,16 +1,21 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        if not s or s[0] == '0':
+        if not s or s[0]=='0':
             return 0
         dp = [0] * (len(s)+1)
         dp[0] = 1
         dp[1] = 1
+        #nums= [ ,1,2,3,0]
+        # dp = [1,1,0,0,0]
 
+        # if cur num is not 0, i-1 + i-2
         for i in range(2, len(dp)):
-            prevDigit = int(s[i-1])
+            # print(s)
+            # dp[i] = 99
+            prev1Digit = int(s[i-1])
             prev2Digits = int(s[i-2:i])
 
-            if prevDigit!=0:
+            if prev1Digit!=0:
                 dp[i] += dp[i-1]
             if 10<=prev2Digits<=26:
                 dp[i] += dp[i-2]
