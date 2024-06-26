b78963c981750b14779f0a503a2bd05a1c340147
lla105 <105393685+lla105@users.noreply.github.com>
2024-06-26 16:40:48 -0700

    Time: 5625 ms (5.98%) | Memory: 16.6 MB (68.90%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -2,16 +2,16 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         self.result = set()
 
-        def bt(availableList , curList) :
+        def bt(availableList , curList , startindex) :
             curList = sorted(curList)
             self.result.add( tuple(curList) )
 
-            for i in range(len(availableList)):
+            for i in range(startindex, len(availableList)):
                 nextavailableList = availableList[:i] + availableList[i+1:]
                 tempA = nextavailableList
                 tempB = curList + [availableList[i]]
                 # bt(nextavailableList , curList[availableList[i]])
-                bt(tempA, tempB)
+                bt(tempA, tempB, startindex)
 
-        bt(nums, [])
+        bt(nums, [], 0)
         return self.result
