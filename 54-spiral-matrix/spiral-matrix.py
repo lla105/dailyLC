5c1efa84cb3621b784872a31f09b3fd58fe9ad67
lla105 <105393685+lla105@users.noreply.github.com>
2024-06-26 17:58:34 -0700

    Time: 39 ms (29.09%) | Memory: 16.5 MB (81.82%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,35 +1,64 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         def printmatrix(matrix):
+            print()
             for i in range(len(matrix)):
-                print('[' , end='')
                 for j in range(len(matrix[0])):
-                    print(matrix[i][j], end='')
-                print(']')
-        if not matrix:
-            return []
-        rows = len(matrix)
-        cols = len(matrix[0])
-        top = 0
-        bottom = rows -1
-        left = 0
-        right = cols - 1
-        result = []
-        while len(result) < rows*cols:
-            for i in range(left, right+1):
-                result.append(matrix[top][i])
-            top += 1
-            for i in range(top, bottom+1):
-                result.append(matrix[i][right])
-            right -= 1
+                    print(matrix[i][j], end='  ')
+                print()
+        n = len(matrix)
+        m = len(matrix[0])
+        
+        if len(matrix[0])==1:
+            result = []
+            for i in range(len(matrix)):
+                result.append(matrix[i][0])
+            return result
+        if len(matrix) ==1 :
+            result = []
+            for i in range(len(matrix[0])):
+                result.append(matrix[0][i])
+            return result
+        top=0
+        left=0
+        right=m
+        bottom = n
+        starti=0
+        startj=0
+        cellcount = 0
 
-            if top <=bottom:
-                for i in range(right, left-1, -1):
-                    result.append(matrix[bottom][i])
-                bottom -= 1
-            if left <= right:
-                for i in range(bottom, top-1, -1):
+        result = []
+        while len(result) < m*n:
+        # while matrix[starti][startj] != "#" or top<bottom or left<right:
+            for i in range(right):
+                if matrix[top][i] != '#':
+                    result.append(matrix[top][i])
+                matrix[top][i] = '#'
+            top+=1
+            # down
+            for i in range(bottom):
+                if matrix[i][right-1] != '#':
+                    result.append(matrix[i][right-1])
+                matrix[i][right-1] = '#'
+            right -= 1
+            # left
+            for i in range(right-1, left-1, -1):
+                if matrix[bottom-1][i] != '#':
+                    result.append(matrix[bottom-1][i])
+                matrix[bottom-1][i] = '#'
+            bottom -= 1
+            # up
+            print(bottom,top,'...')
+            for i in range(bottom-1, top, -1):
+                print(i,left)
+                if matrix[i][left] != '#':
                     result.append(matrix[i][left])
-                left += 1
+                matrix[i][left] = '#'
+                starti = i
+                startj = left+1
+            left += 1
+            print('starti and j : ', starti, startj)
+
         printmatrix(matrix)
+        print(' RESULT : ', result)
         return result
