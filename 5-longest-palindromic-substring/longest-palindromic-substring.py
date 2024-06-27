c2ba26fa18028e1b03e32f412f56b0e656c681eb
lla105 <105393685+lla105@users.noreply.github.com>
2024-06-26 23:03:53 -0700

    Time: 543 ms (46.82%) | Memory: 16.6 MB (69.29%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,24 +1,26 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        res = ''
-        resLen = 0
-
-        for i in range(len(s)):
-            # odd length
-            l, r = i,i
-            while l>=0 and r<len(s) and s[l]==s[r]:
-                if (r-l+1) > resLen:
-                    res = s[l:r+1]
-                    resLen = r-l+1
-                l-=1
-                r+=1
-            l, r = i, i+1
-            while l>=0 and r<len(s) and s[l]==s[r]:
-                if (r-l+1) > resLen:
-                    res = s[l:r+1]
-                    resLen = r-l+1
-                l-=1
-                r+=1
-
-        return res 
         
+        maxsubstring = ''
+        for i in range(len(s)):
+            l = i
+            r = i
+            while l>=0 and r<len(s) and s[l]==s[r]:
+                cursubstring = s[l:r+1]
+                if len(cursubstring) >= len(maxsubstring):
+                    maxsubstring = cursubstring
+                
+                l-=1
+                r+=1
+        for i in range(len(s)-1):
+            l = i
+            r = i+1
+            while l>=0 and r<len(s) and s[l]==s[r]:
+                cursubstring = s[l:r+1]
+                if len(cursubstring) >= len(maxsubstring):
+                    maxsubstring = cursubstring
+                
+                l-=1
+                r+=1
+
+        return maxsubstring
