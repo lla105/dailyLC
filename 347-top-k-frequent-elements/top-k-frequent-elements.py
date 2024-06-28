8ad8807e4030ad48c926162ea613b56401748489
lla105 <105393685+lla105@users.noreply.github.com>
2024-06-28 16:53:22 -0700

    Time: 95 ms (26.56%) | Memory: 21 MB (66.03%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,38 +1,31 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-
-        # nums = [11,11,11,22,22,22,33]
-
-        length = len(nums)
-        if length == 1:
-            return [nums[0]]
-
-        # d format = { num : frequency }
-        d1 = Counter(nums)
-        print(f'd1 : {d1}')
-        # d2 format = {freq : [num, num]}
-        # lets say nums = 4
-        # d2 = { 4:[], 3:[], 2:[11,22], 1:[33] }
-        # eg nums = [ 11,11,22,22]
-        # { 2 : [11,22] }
-
-        d2 = {}
-        for i in range(length, 0, -1):
-            d2[i] = []
-        print(f'd2 : {d2}')
-
-        for i,v in d1.items():
-            if v in d2:
-                d2[v].append(i)
-        print(f'd2 now: {d2}')
+        # nums = [1,1,1,2,2,2,3]
+        if not nums:
+            return []
+        # if len(nums)==1:
+        #     return [nums[-1]]
+        d = Counter(nums) # format : { num:frequency }
+ 
+        freqDict = {} # format : { freq: [num1, num2, ...] }
+        # freqDict = {defaultdict(list)}
+        for i,v in d.items():
+            if v in freqDict:
+                freqDict[v].append(i)
+            else:
+                freqDict[v] = [i]
+            
+        print(d)
+        print(freqDict)
 
         result = []
-        kcount = 0
-        for i in range(length, 0,-1):
-            # print(i)
-            for j in d2[i]:
-                result.append(j)
-                kcount += 1
-                if kcount == k:
-                    return result
+        for i in range(len(nums), -1, -1):
+            print(i)
+            if i in freqDict and k>0:
+                j=0
+                for j in range(len(freqDict[i])):
+                    k-=1
+                    result.append(freqDict[i][j])
+
+
         return result
