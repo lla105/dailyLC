3ad771ac7ac81b91a9426102686fe870cb7b2926
lla105 <105393685+lla105@users.noreply.github.com>
2024-06-28 14:10:51 -0700

    Time: 35 ms (59.44%) | Memory: 16.5 MB (49.66%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -11,10 +11,10 @@ class Solution:
         for i in range(len(s)):
             if s[i] in d: # is closed bracket
                 if not stack:
-                    print('  2) stack is empty')
+                    # print('  2) stack is empty')
                     return False
                 if stack[-1]!= d[s[i]]:
-                    print('    3) bracket mismatch')
+                    # print('    3) bracket mismatch')
                     return False
                 stack.pop()
             else:
