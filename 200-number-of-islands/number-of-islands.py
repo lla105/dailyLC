1fad32788b8596e8ff9ac153fd8ef88df2ef5876
lla105 <105393685+lla105@users.noreply.github.com>
2024-06-28 15:21:03 -0700

    Time: 231 ms (77.58%) | Memory: 18.9 MB (70.42%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,29 +1,24 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if grid is None:
-            return None
+        if not grid:
+            return 0
+        count = 0
 
-        def dfs(grid,i,j):
-
-            if i>=len(grid) or j>=len(grid[0]):
-                return 
-            if i<0 or j<0 :
+        def bfs(grid, i,j):
+            if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]):
                 return
-            # if grid[i][j]=='0':
-            if grid[i][j] != '1':
+            if grid[i][j]!='1':
                 return
-            grid[i][j] = "99"
-            dfs(grid,i+1,j)
-            dfs(grid,i-1,j)
-            dfs(grid,i,j+1)
-            dfs(grid,i,j-1)
-            return 
+            grid[i][j] = '#'
+            bfs(grid,i+1,j)
+            bfs(grid,i,j+1)
+            bfs(grid,i-1,j)
+            bfs(grid,i,j-1)
 
-
-        islands = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j]=="1":
-                    dfs(grid,i,j)
-                    islands += 1
-        return islands
+                if grid[i][j] == '1':
+                    count += 1
+                    bfs(grid,i,j)
+
+        return count
