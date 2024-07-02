1dad014e54927a170cc2e33f2967039ed7ffe37c
lla105 <105393685+lla105@users.noreply.github.com>
2024-07-02 16:40:24 -0700

    Time: 174 ms (38.52%) | Memory: 17.9 MB (23.16%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,38 +1,33 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        # build dict for p
-
-        # loop over s 
-        # build dict for s[i-len(p): i]
-        # compare occurance for each entry  
-        # if match, record the index.(i)
-
-        #to move to next subset, remove s[i-len(p)] from dict
-        # add next s[i] to dict. repeat. 
         pdic = Counter(p)
         sdic = {}
         result = []
-        for i in range( len(s)):
-            # print(s[i])
-            sdic[s[i]] = sdic.get(s[i],0) + 1
+        sString = deque()
+        for i in range(len(s)):
+            sdic[s[i]] = sdic.get(s[i] , 0) + 1
+            sString.append(s[i])
             if i<len(p)-1:
                 continue
-            # print('1) ', s[i] , '->', sdic)
-
             isAnagram = True
-            for pi, pv in pdic.items():
+            for pi,pv in pdic.items():
                 if pi in sdic and pv==sdic.get(pi):
-                    # print('2)   match: ', pi, pv)
                     pass
                 else:
                     isAnagram = False
                     continue
             if isAnagram :
-                # print('3)    result append : ', i-len(p)+1)
                 result.append(i-len(p)+1)
-            # print('4)     sdic pop : ', s[i-len(p)+1])
-            if sdic.get(s[i-len(p)+1]) == 1:
-                sdic.pop(s[i-len(p)+1])
-            elif sdic.get(s[i-len(p)+1]) >1:
-                sdic[s[i-len(p)+1]] -= 1
+                
+            lastchar = s[i-len(p)+1]
+            # if sdic.get(lastchar):
+            #     print(lastchar, ' is good')
+            # else:
+            #     print(lastchar, ' is None')
+            # print(i, ").     ", sdic)
+
+            if sdic.get(lastchar) == 1:
+                sdic.pop(lastchar)
+            elif sdic.get(lastchar) >1:
+                sdic[lastchar] -= 1
         return result
