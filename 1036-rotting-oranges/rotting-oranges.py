ed65e4e51a7438e2cb92c19181feb99c5e7f4ad6
lla105 <105393685+lla105@users.noreply.github.com>
2024-07-03 15:04:51 -0700

    Time: 56 ms (23.98%) | Memory: 16.5 MB (42.08%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,24 +1,37 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        if not grid:
-            return -1
-        # get initial rotten cells
-        startcells = deque()
-        freshcount = 0
+        
+
+        # def flood(grid,i,j):
+        #     #bound check
+        #     if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]):
+        #         return
+        #     if grid[i][j] != 1:
+        #         return
+        #     grid[i][j] = 2
+        #     rottencells.append( (i,j) )
+        #     flood(grid,i+1,j)
+        #     flood(grid,i,j+1)
+        #     flood(grid,i-1,j)
+        #     flood(grid,i,j-1)
+
+        #get starting rotten cells
+        rottencells = deque()
+        freshoranges = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 2:
-                    startcells.append((i,j))
+                    rottencells.append( (i,j) )
                 if grid[i][j] == 1:
-                    freshcount += 1
+                    freshoranges += 1
 
         minutesPassed = 0
-        directions = [ (0,1), (0,-1), (1,0), (-1,0) ]
-
-        while startcells and freshcount > 0 :
+        directions = [ (1,0), (-1,0), (0,1), (0,-1) ]
+        while rottencells and freshoranges>0:
+            print(rottencells)
             minutesPassed += 1
-            for temp in range(len(startcells)):
-                x,y = startcells.popleft()
+            for _ in range(len(rottencells)):
+                x,y = rottencells.popleft()
                 for dx,dy in directions:
                     i = x+dx
                     j = y+dy
@@ -26,11 +39,14 @@ class Solution:
                         continue
                     if grid[i][j] != 1:
                         continue
-                    freshcount -=1 
-                    startcells.append( (i,j) )
                     grid[i][j] = 2
+                    freshoranges -= 1
+                    rottencells.append( (i,j) )
 
-        if freshcount == 0:
-            return minutesPassed
-        else:
+        
+        if freshoranges >0:
             return -1
+        else:
+            return minutesPassed
+
+                
