34fa9ac90a954e686929be83ef85733d3fdfbc25
lla105 <105393685+lla105@users.noreply.github.com>
2024-07-03 17:43:16 -0700

    Time: 38 ms (49.99%) | Memory: 16.5 MB (89.59%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -1,31 +1,14 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        d = Counter(s)
-        maxlen = 0
-        middleUsed = False
+        scount = Counter(s)
+        pairs = 0
+        hasOdd= False
+        for i,v in scount.items():
+            pairs += (v//2)*2
+            if v%2!=0:
+                hasOdd = True
 
-        for i,v in d.items():
-            # aab -> aba
-            # aabc --> a_a
-            # aaac --> aca
-            print(i,maxlen)
-            if v%2==0: #v is even
-                maxlen += v
-            else: # v is odd
-                # v is 1
-                if v==1:
-                    if middleUsed==False:
-                        maxlen += 1
-                else:
-                    if middleUsed==False:
-                        maxlen += v
-                    else:
-                        maxlen += v
-                        maxlen -= 1
-                print(i,v)
-                # v is >1 odds, eg: 5
-
-                middleUsed=True
-            print(i,maxlen)
-            
-        return maxlen
+        if hasOdd:
+            return pairs+1
+        else:
+            return pairs
