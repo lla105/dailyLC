14fbdfab6bd024ee362c9e1cdef1269afb5ae48e
lla105 <105393685+lla105@users.noreply.github.com>
2024-07-04 16:01:38 -0700

    Time: 409 ms (85.62%) | Memory: 19.2 MB (85.35%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -1,25 +1,26 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        directions = [ (0,1), (0,-1), (1,0), (-1,0)]
         m = len(mat)
         n = len(mat[0])
         q = deque()
+
         for i in range(m):
             for j in range(n):
                 if mat[i][j]==0:
-                    q.append((i,j))
-                else:
-                    mat[i][j]=float('inf')
-        # print(mat)
-        directions = [ (1,0),(-1,0),(0,1),(0,-1)]
+                    q.append( (i,j) )
+                if mat[i][j]==1:
+                    mat[i][j] = float('inf')
+
         while q:
-            oldi, oldj = q.popleft()
-            for di,dj in directions:
-                newi,newj = oldi+di,oldj+dj
-                if newi<0 or newj<0 or newi>=m or newj>=n:
+            x,y = q.popleft()
+            for dx,dy in directions:
+                i = x+dx
+                j = y+dy
+                if i<0 or j<0 or i>=m or j>=n:
                     continue
-                if mat[newi][newj]==0:
-                    continue
-                if mat[newi][newj]>mat[oldi][oldj]+1:
-                    mat[newi][newj]=mat[oldi][oldj]+1
-                    q.append((newi,newj))
+                if mat[i][j] > mat[x][y] + 1 :
+                    mat[i][j] = mat[x][y] + 1 
+                    q.append( (i,j) )
+
         return mat
