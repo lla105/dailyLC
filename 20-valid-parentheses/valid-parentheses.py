cf4b25a3885f83c32fdc6c8570576e0cc6b1e2c7
lla105 <105393685+lla105@users.noreply.github.com>
2024-07-05 16:21:12 -0700

    Time: 34 ms (68.09%) | Memory: 16.6 MB (12.71%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,26 +1,23 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        # closed = { '(':')' , '{':'}' , '[':']' }
+        closed = { ')':'(' , '}':'{' , ']':'['}
         stack = []
 
-        d = {
-            ')':'(',
-            '}':'{',
-            ']':'['
-        }
-
         for i in range(len(s)):
-            if s[i] in d: # is closed bracket
+            if s[i] in closed:
+                # is closed bracket
                 if not stack:
-                    # print('  2) stack is empty')
                     return False
-                if stack[-1]!= d[s[i]]:
-                    # print('    3) bracket mismatch')
-                    return False
-                stack.pop()
+                else:
+                    if closed[s[i]] == stack[-1]:
+                        stack.pop()
+                    else:
+                        return False
             else:
                 # is open bracket
                 stack.append(s[i])
-        if len(stack)==0:
+        if not stack:
             return True
         else:
             return False
