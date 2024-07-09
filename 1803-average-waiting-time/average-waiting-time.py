a394c63ea729983cf1c46d04c179e288087c8bab
lla105 <105393685+lla105@users.noreply.github.com>
2024-07-09 13:55:55 -0700

    Time: 1043 ms (5.16%) | Memory: 57.1 MB (72.85%) - LeetSync

---
+++ 1803-average-waiting-time/average-waiting-time.py
@@ -0,0 +1,29 @@
+class Solution:
+    def averageWaitingTime(self, customers: List[List[int]]) -> float:
+        if not customers:
+            return 0
+        # prev_finish = customers[0][1] + customers[0][0]
+        prev_finish = 0
+        total_waited = []
+
+        if len(customers) == 1:
+            return prev_finish
+        
+        for i in range(len(customers)):
+            cur_arrive = customers[i][0]
+            tempwaited = customers[i][1] 
+            print(f'{i} )')
+            if prev_finish >= cur_arrive:
+                print(f'    clash!')
+                tempwaited += prev_finish - cur_arrive
+                # cur_arrive = prev_finish
+            print(f'    cur_arrive: {cur_arrive}, waited:{tempwaited}')
+            
+            prev_finish = cur_arrive + tempwaited
+            total_waited.append(tempwaited)
+            print(f'    finished at:{prev_finish}')
+
+
+        total_sum = sum(total_waited)
+        answer = total_sum / len(customers)
+        return answer
