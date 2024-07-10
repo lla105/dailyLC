f900bf69ead256aea50a1a29e5d311d434a5dba4
lla105 <105393685+lla105@users.noreply.github.com>
2024-07-10 16:36:02 -0700

    Time: 41 ms (54.16%) | Memory: 16.9 MB (6.30%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,18 +1,16 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        if not nums:
-            return []
+        
+        result = []
 
-        self.result = []
-
-        def bt(availableNums , curList):
-            if len(curList) == len(nums):
-                self.result.append(curList)
+        def bt(curList, available):
+            if len(curList)==len(nums):
+                result.append(curList)
                 return
-            for i in range(len(availableNums)):
-                nextAvailableNums = availableNums[0:i] + availableNums[i+1:]
-                nextList = curList + [availableNums[i]]
-                bt(nextAvailableNums, nextList)
+            
+            for i in range(len(available)):
+                nextavailable = available[:i]+available[i+1:]
+                bt(curList+[available[i]] , nextavailable)
+        bt([] , nums)
 
-        bt(nums, [])
-        return self.result
+        return result
