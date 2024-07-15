45774fc800f40c26a1fa5f9df0f7ceb9bb453166
lla105 <105393685+lla105@users.noreply.github.com>
2024-07-15 10:04:25 -0700

    Time: 1552 ms (77.78%) | Memory: 25.8 MB (85.86%) - LeetSync

--- 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.py
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.py
@@ -1,18 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def createBinaryTree(self, descriptions: List[List[int]]) -> Optional[TreeNode]:
         child = set()
         nodes = {}
-        for p, c, left in  descriptions:
+        for p,c,isleft in descriptions:
             if p not in nodes:
                 nodes[p] = TreeNode(p)
             if c not in nodes:
                 nodes[c] = TreeNode(c)
-            if left:
+            if isleft==1:
                 nodes[p].left = nodes[c]
             else:
                 nodes[p].right = nodes[c]
+            # print(nodes[p])
             child.add(c)
-        for d in descriptions:
-            if d[0] not in child:
-                return nodes[d[0]]
-        return None
+            
+        # delete all node except root
+        for i in range(len(descriptions)):
+            d = descriptions[i][0]
+            if d not in child:
+                return nodes[d]
+        rootnode = None
+
+        # for i,v in nodedic.items():
+        #     if i==rootval:
+        #         rootnode = v
+        # print('root node: \n', rootnode)
+        return rootnode
+                
