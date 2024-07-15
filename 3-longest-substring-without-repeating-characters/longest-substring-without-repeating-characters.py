fcd0bf0964a50452f8ec66e2318a09848daa94c7
lla105 <105393685+lla105@users.noreply.github.com>
2024-07-15 15:52:19 -0700

    Time: 67 ms (25.99%) | Memory: 16.6 MB (32.16%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -6,19 +6,19 @@ class Solution:
         count = 0
         answer = 0
         for i in range(len(s)):
-            if s[i] not in ss and s[i] not in stack:
-                stack.append(s[i])
-                ss.add(s[i])
-                count+=1
+            # if s[i] not in ss and s[i] not in stack:
+            #     stack.append(s[i])
+            #     ss.add(s[i])
+            #     count+=1
 
-            else:
-                while s[i] in ss:
-                    removed = stack.popleft()
-                    ss.remove(removed)
-                    count -= 1
-                stack.append(s[i])
-                ss.add(s[i])
-                count+=1
+            # else:
+            while s[i] in ss:
+                removed = stack.popleft()
+                ss.remove(removed)
+                count -= 1
+            stack.append(s[i])
+            ss.add(s[i])
+            count+=1
             answer = max(answer, count)
 
         return answer
