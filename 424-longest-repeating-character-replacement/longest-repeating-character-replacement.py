872f870398374ac1b7158b43f5761cf824a78a24
lla105 <105393685+lla105@users.noreply.github.com>
2024-07-15 17:42:12 -0700

    Time: 70 ms (93.45%) | Memory: 16.8 MB (7.60%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,14 +1,26 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        longestChar = 0
-        substringLength = 0
+        highestfreq = 0
+        longest = 0
+        stack = deque()
         d = {}
 
         for i in range(len(s)):
-            d[s[i]] = d.get(s[i], 0 ) + 1
-            longestChar = max(longestChar, d[s[i]] )
-            if k > substringLength - longestChar:
-                substringLength += 1
-            else :
-                d[s[i - substringLength]] -= 1 
-        return substringLength
+            d[s[i]] = d.get(s[i],0) + 1
+            highestfreq = max(highestfreq , d[s[i]])
+            if k > len(stack) - highestfreq:
+                stack.append(s[i])
+            else:
+                d[s[i - len(stack)]] -= 1
+            # print(stack)
+        return len(stack)
+
+            # stack.append(s[i])
+            # slen = len(stack)
+            # if k < slen - highestfreq:
+            #     rm = stack.popleft()
+            #     d[rm] -= 1
+            # else:
+            #     slen += 1
+            #     highestfreq = max(highestfreq , slen-highestfreq)
+
