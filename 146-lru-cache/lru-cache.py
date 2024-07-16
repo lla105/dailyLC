14d35e95d04364337c632bbf9a5ad47875459918
lla105 <105393685+lla105@users.noreply.github.com>
2024-07-15 21:13:29 -0700

    Time: 2047 ms (5.00%) | Memory: 76.7 MB (98.67%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,24 +1,32 @@
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cache = {}
-        self.capacity = capacity
+        self.d = {}
+        self.recentq = deque()
+        self.capacity = capacity 
+        self.usage = 0
 
     def get(self, key: int) -> int:
-        if key in self.cache:
-            val = self.cache.pop(key)
-            self.cache[key] = val
-            return self.cache.get(key)
+        if key in self.d :
+            self.recentq.remove(key)
+            self.recentq.append(key)
+            return self.d[key]
         return -1
 
     def put(self, key: int, value: int) -> None:
-        if key not in self.cache:
-            if len(self.cache.keys()) == self.capacity:
-                del self.cache[next(iter(self.cache))]
+        if key in self.d:
+            self.recentq.remove(key)
+            self.recentq.append(key)
+            self.d[key] = value
         else:
-            self.cache.pop(key)
-        self.cache[key] = value
-            
+            self.usage += 1
+            if self.usage > self.capacity:
+                temp = self.recentq.popleft()
+                self.d.pop(temp)
+                self.usage -= 1
+            self.recentq.append(key)
+            self.d[key] = value
+
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
