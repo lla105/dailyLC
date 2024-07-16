bf020a743fd15de12bc276a7e15c66d711bcd80b
lla105 <105393685+lla105@users.noreply.github.com>
2024-07-15 21:30:42 -0700

    Time: 44 ms (68.29%) | Memory: 17.1 MB (50.96%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,21 +1,25 @@
-def bs(array, target):
-    left = 0
-    right = len(array) - 1
-
-    while left<=right:
-        mid = left + (right-left) // 2
-        # print(f'mid:{mid} . arr[mid]:{array[mid]}')
-        if array[mid] == target:
-            # print("??")
-            return True
-        elif array[mid] > target:
-            right = mid - 1
-        elif array[mid] < target:
-            left = mid + 1
-
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        for i in range(len(matrix)):
-            if bs(matrix[i], target):
-                return True
+        if not matrix or not matrix[0]:
+            return False
+        if len(matrix[0]) == 1 and matrix[0][-1]==target:
+            return True
+        for row in range(len(matrix)):
+            head = matrix[row][0]
+            tail = matrix[row][-1]
+            if head<=target<=tail:
+                # print(matrix[row], head,target,tail)
+                l = 0
+                r = len(matrix[0])-1
+                while l<=r:
+                    m = (l+r) // 2
+                    # print(l,m,r)
+                    if matrix[row][m] == target:
+                        return True
+                    elif matrix[row][m] < target:
+                        l = m+1
+                    else:
+                        r = m-1
+
+
         return False
