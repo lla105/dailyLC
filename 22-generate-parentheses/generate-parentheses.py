9854fd6720122ba58bac2a1f7362543f9e77fb38
lla105 <105393685+lla105@users.noreply.github.com>
2024-07-17 18:22:47 -0700

    Time: 37 ms (60.92%) | Memory: 16.9 MB (28.90%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,24 +1,17 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        stack = []
-        resultlist = []
+        result = []
+        def bt(openb, closeb, curlist, level):
+            if closeb==openb and openb==n:
+                print(level*'  ', 'APPEND')
+                result.append(''.join(curlist))
+                return
+            if n>openb and n>closeb:
+                print(level*'  ','ADDING OPEN')
+                bt(openb+1, closeb, curlist+['('], level+1)
+            if openb<=n and openb>closeb:
+                print(level*'  ','ADDING CLOSE')
+                bt(openb,closeb+1,curlist+[')'], level+1)
 
-        def trav(open, close):
-            if open==n and close==open:
-                print('1:', stack)
-                resultlist.append(''.join(stack))
-                return 
-            if open<n and close<n:
-                stack.append('(')
-                print('2:', stack)
-                trav(open+1, close)
-                stack.pop()
-            if close<open and open<=n and stack[-1]:
-                stack.append(')')
-                print('3:', stack)
-                trav(open,close+1)
-                stack.pop()
-            
-        
-        trav(0,0)
-        return resultlist
+        bt(0,0, [],0)
+        return result
