49612a158b126d936993ecc947f16e9e65680dbc
lla105 <105393685+lla105@users.noreply.github.com>
2024-07-17 18:13:01 -0700

    Time: 67 ms (46.76%) | Memory: 17.1 MB (16.71%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -1,26 +1,27 @@
 class Solution:
-    def operations(self, prefix, operation, postfix):
-        if operation=="+":
-            return prefix+postfix
-        elif operation=="-":
-            return prefix-postfix
-        elif operation =="*":
-            return prefix*postfix
-        else:
-            return int(prefix/postfix)
-
-
     def evalRPN(self, tokens: List[str]) -> int:
-        operators = ['+', '-', '*', '/']
-        # def operation(prefix, )
         stack = []
-        for t in tokens:
-            if t in operators:
-                postfix = stack.pop()
-                prefix = stack.pop()
-                result = int(self.operations(prefix, t, postfix))
-                stack.append(result)
+        operations = ['+', '-', '*', '/']
+
+        for i in range(len(tokens)):
+            t = tokens[i]
+            if t not in operations:
+                # print('is digit: ', t, stack)
+                stack.append(int(t))
             else:
-                # is a number:
-                stack.append( int(t) )
-        return stack[0]
+                # print('not digit: ', t, stack)
+                num2 = stack.pop()
+                num1 = stack.pop()
+                if t=='+':
+                    stack.append(num2+num1)
+                elif t=='-':
+                    stack.append(num1-num2)
+                elif t=='*':
+                    stack.append(num1*num2)
+                else:
+                    stack.append( int(num1/num2))
+
+            # print(stack)
+        print(stack)
+
+        return stack[-1]
