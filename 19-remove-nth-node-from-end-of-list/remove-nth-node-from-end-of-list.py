ebf67f24654bd095211f792cec5c501d94f6f000
lla105 <105393685+lla105@users.noreply.github.com>
2024-07-19 16:08:15 -0700

    Time: 38 ms (47.73%) | Memory: 16.7 MB (18.18%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -3,23 +3,40 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-def printlist(node):
-    cur = node
-    while cur:
-        print(f'{cur.val}->', end='')
-        cur = cur.next
-    print("")
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        dummy = ListNode(0)
-        dummy.next = head
-        h1 = dummy
-        h2 = dummy
-        # printlist(h1)
-        for i in range(n+1):
-            h2 = h2.next
-        while h2:
-            h2 = h2.next
-            h1 = h1.next
-        h1.next = h1.next.next
-        return dummy.next
+        def printlist(somelist):
+            cur = somelist
+            print( ' printing list')
+            while cur:
+                print(cur.val, end='->')
+                cur = cur.next 
+            print()
+        if not head.next:
+            return None
+        slow = ListNode(99)
+        returnhead = slow
+        slow.next = head
+        fast = slow.next
+        count = 0
+        while fast and count<n:
+            count+=1
+            fast = fast.next
+        printlist(slow)
+        printlist(fast)
+        while fast:
+            fast = fast.next
+            slow = slow.next
+        # print(slow)
+        # print(slow.next)
+        # print()
+        # print(fast)
+        print()
+        printlist(slow)
+        printlist(fast)
+
+        temp = slow.next
+        slow.next = temp.next
+
+
+        return returnhead.next
