49b15ff4fc21744e648ad351055809997b4e80f8
lla105 <105393685+lla105@users.noreply.github.com>
2024-07-18 22:41:38 -0700

    Time: 64 ms (97.87%) | Memory: 16.5 MB (93.84%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,26 +1,14 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        highestfreq = 0
-        longest = 0
-        stack = deque()
+        highestfreq = 0 
         d = {}
-
+        q = deque()
         for i in range(len(s)):
-            d[s[i]] = d.get(s[i],0) + 1
+            d[s[i]] = d.get(s[i] , 0 ) + 1
             highestfreq = max(highestfreq , d[s[i]])
-            if k > len(stack) - highestfreq:
-                stack.append(s[i])
+            if k > len(q) - highestfreq:
+                q.append(s[i])
             else:
-                d[s[i - len(stack)]] -= 1
-            # print(stack)
-        return len(stack)
-
-            # stack.append(s[i])
-            # slen = len(stack)
-            # if k < slen - highestfreq:
-            #     rm = stack.popleft()
-            #     d[rm] -= 1
-            # else:
-            #     slen += 1
-            #     highestfreq = max(highestfreq , slen-highestfreq)
-
+                d[s[i-len(q)]] -= 1
+            # print(q)
+        return len(q)
