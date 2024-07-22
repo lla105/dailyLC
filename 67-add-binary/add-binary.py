1d39fdcb6cb12958d44cc417914073df9b7d99fa
lla105 <105393685+lla105@users.noreply.github.com>
2024-07-22 13:54:41 -0700

    Time: 42 ms (27.92%) | Memory: 16.5 MB (82.00%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -1,4 +1,17 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        temp = int(a,2) + int(b,2)
+        # print( bin(int(a)), bin(int(b)) )
+        temp = int(int(a,2) + int(b,2))
         return bin(temp)[2:]
+
+        return '1010'
+
+        # 1 = 1
+        # 2 = 10
+        # 3 = 11
+        # 4 = 100
+        # 8 = 1000
+        # 9 = 1001
+        # 10 = 1010
+        # 11 = 1011 
+    
