5687df575577fadcadf24f3aefc58466196c6b20
lla105 <105393685+lla105@users.noreply.github.com>
2024-07-21 18:21:17 -0700

    Time: 104 ms (60.83%) | Memory: 17.2 MB (14.55%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,19 +1,12 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        d = defaultdict(list)
-        maxheight = max(heights)
+        a1 = []
         for i in range(len(names)):
-            if heights[i] in d:
-                d[heights[i]].append(names[i])
-            else:
-                d[heights[i]] = [names[i]]
-        # print(d)
-        result = []
-        sortedheight = sorted( heights,reverse=True)
-        # print(sortedheight)
-        # for h in range(maxheight, -1 ,-1):
-        for h in sortedheight:
-            for person in d[h]:
-                result.append(person)
+            a1.append([heights[i],names[i]]) 
+        a1.sort(reverse = True)
+        b1 = []
+        for i in range(len(names)):
+            b1.append(a1[i][1])
+        return b1
 
-        return result
+        
