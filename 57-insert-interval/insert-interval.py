85474d17e4a49467fb1064551b6ef9f7fc68ca51
lla105 <105393685+lla105@users.noreply.github.com>
2024-07-22 16:39:48 -0700

    Time: 78 ms (38.49%) | Memory: 20 MB (12.08%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,20 +1,24 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        # [4,5], [5,7],[8,9]
+        # newiunbteraval: [5,11]
+        # [4,11]
         result = []
         i = 0
-        while i<len(intervals) and intervals[i][1] < newInterval[0]:
+        newint = newInterval
+        while i<len(intervals) and intervals[i][0]<newint[0] and intervals[i][1]<newint[0]:
             result.append(intervals[i])
-            i += 1
-        # print(result)
-        temp = newInterval
-
-        while i<len(intervals) and intervals[i][0] <= temp[1] :
-            temp[0] = min(temp[0], intervals[i][0])
-            temp[1] = max(temp[1], intervals[i][1])
             i+=1
-        result.append(temp)
-        # print(result)
-        while i<len(intervals):
+        tempint = newint
+        while i<len(intervals) and intervals[i][0] <= newint[1]:
+            # print(intervals[i][0])
+            tempint[0] = min(intervals[i][0] , newint[0])
+            tempint[1] = max(intervals[i][1] , newint[1])
+            i+=1
+        result.append(tempint)
+        # print('2.   ', result)
+        while i<len(intervals) and intervals[i]:
             result.append(intervals[i])
             i+=1
         return result
+        
