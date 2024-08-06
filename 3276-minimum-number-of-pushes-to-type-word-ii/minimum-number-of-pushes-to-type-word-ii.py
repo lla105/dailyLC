bd992212f731abdfdfe9237a326b4e8806a91112
lla105 <105393685+lla105@users.noreply.github.com>
2024-08-05 21:03:47 -0700

    Time: 353 ms (5.01%) | Memory: 17.7 MB (36.75%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
@@ -5,22 +5,18 @@ class Solution:
         d1 = {}
         for i in range(len(word)):
             d1[word[i]] = d1.get(word[i], 0)+1
-        print(d1)
         # dicSet = defaultdict(set)
         d2 = defaultdict(list)
         for i,v in d1.items():
             # dicSet[v].add(i)
             d2[v].append(i)
-        print(d2)
         # print(' word length : ', len(word)-1)
         numstack = []
-        print( ' len ( word ) : ', len(word))
         for i in range( len(word), -1, -1):
             if i not in d2:
                 continue
             for numindex in d2[i]:
                 numstack.append([numindex])
-        print(' num stack: ', numstack)
         presscount = 0
         level = 1
         curlevelcount = 0
@@ -29,9 +25,7 @@ class Solution:
             presscount += level * d1[numstack[i][-1]]
             
             curlevelcount +=1
-            print(' >> ', level, ': ', numstack[i][-1],' -> freq : ', d1[numstack[i][-1]])
             if curlevelcount >= 8:
-                print( ' >>>>> level count over 7')
                 curlevelcount = 0
                 level += 1
         return presscount
