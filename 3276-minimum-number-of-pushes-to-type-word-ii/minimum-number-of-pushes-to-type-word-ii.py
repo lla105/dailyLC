673f311dab818144eb190d35c5bd89d70846bfb6
lla105 <105393685+lla105@users.noreply.github.com>
2024-08-05 21:03:59 -0700

    Time: 356 ms (5.01%) | Memory: 17.6 MB (36.75%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
@@ -5,12 +5,9 @@ class Solution:
         d1 = {}
         for i in range(len(word)):
             d1[word[i]] = d1.get(word[i], 0)+1
-        # dicSet = defaultdict(set)
         d2 = defaultdict(list)
         for i,v in d1.items():
-            # dicSet[v].add(i)
             d2[v].append(i)
-        # print(' word length : ', len(word)-1)
         numstack = []
         for i in range( len(word), -1, -1):
             if i not in d2:
@@ -21,7 +18,6 @@ class Solution:
         level = 1
         curlevelcount = 0
         for i in range(len(numstack)):
-            # print('. ', d1[numstack[i][-1]],level)
             presscount += level * d1[numstack[i][-1]]
             
             curlevelcount +=1
@@ -29,4 +25,3 @@ class Solution:
                 curlevelcount = 0
                 level += 1
         return presscount
-# 6+12 = 18
