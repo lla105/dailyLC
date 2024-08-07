07bbe2ac3a7db34afe5d59a3c6f1aa74e9c0b659
lla105 <105393685+lla105@users.noreply.github.com>
2024-08-07 15:35:39 -0700

    Time: 49 ms (65.58%) | Memory: 16.4 MB (98.01%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,31 +1,31 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        minute= 0
-        rottencells = deque()
         freshcount = 0
+        rottencellsQ = deque()
         for i in range(len(grid)):
             for j in range(len(grid[0])):
+                if grid[i][j]==1:
+                    freshcount +=1
                 if grid[i][j] == 2:
-                    rottencells.append( (i,j) )
-                elif grid[i][j] == 1:
-                    freshcount += 1
-        directions = [ (0,1),(0,-1),(1,0),(-1,0) ]
-        while freshcount>0 and rottencells:
-            minute+=1
-            for _ in range(len(rottencells)):
-                x,y = rottencells.popleft()
+                    rottencellsQ.append( (i,j) )
+        minutepassed = 0
+        directions = [ (0,1),(0,-1), (1,0), (-1,0) ]
+        while freshcount and rottencellsQ:
+            minutepassed += 1
+            for _ in range(len(rottencellsQ)):
+                x,y = rottencellsQ.popleft()
                 for dx,dy in directions:
                     i = x+dx
                     j = y+dy
                     if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]):
                         continue
-                    if grid[i][j] != 1:
+                    if grid[i][j]!=1:
                         continue
-                    freshcount-=1
+                    rottencellsQ.append( (i,j) )
                     grid[i][j] = 2
-                    rottencells.append( (i,j) )
-
-        if freshcount>0:
+                    freshcount -= 1
+        
+        if freshcount >0:
             return -1
         else:
-            return minute
+            return minutepassed
