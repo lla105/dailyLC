9a06c38c1334a942c64371c73fab7bf968bc352c
lla105 <105393685+lla105@users.noreply.github.com>
2024-08-09 14:32:27 -0700

    Time: 194 ms (36.40%) | Memory: 18.1 MB (27.75%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -2,13 +2,13 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         l = 0
         r = len(nums) - 1
-
         while l<=r:
             m = (l+r) // 2
-            if nums[m] > target:
-                r -= 1
-            elif nums[m] < target:
-                l += 1
-            else:
+            cur = nums[m]
+            if cur == target:
                 return m
+            elif cur> target:
+                r = m-1
+            else:
+                l = m+1
         return -1
