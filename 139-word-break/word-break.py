eadf446dae4078e6b2db7b658d75f42b6c00f4cb
lla105 <105393685+lla105@users.noreply.github.com>
2024-08-09 16:45:12 -0700

    Time: 40 ms (58.74%) | Memory: 16.6 MB (46.82%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,16 +1,57 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        dp = [ False ] * (len(s)+1)
+        dp = [False]* (len(s)+1)
         dp[0] = True
-        print(dp)
+
         
         for i in range(len(s)):
-            if dp[i]:  # Only proceed if the substring up to i can be segmented
-                for word in wordDict:
-                    end_index = i + len(word)
-                    if end_index <= len(s) and s[i:end_index] == word:
-                        dp[end_index] = True
-        
-        return dp[len(s)]
+            def printt(level, msg):
+                # print(level*' ', msg)
+                return
+
+            if not dp[i] :
+                continue
+            for word in wordDict:
+                printt(i, word)
+                tail = i+len(word)
+                if tail>len(s):
+                    printt(i, ' 1) tail>=len(s)')
+                    continue
+                if word!=s[i:i+len(word)]:
+                    printt(i, ' 2) word!=')
+                    continue
+                printt(i, ' 3)')
+                dp[tail] = True
         print(dp)
         return dp[-1]
+
+        # self.result = False
+        # self.dp = [False] * (len(s)+1)
+        # self.dp[0] = True
+        # # print(self.dp)
+
+        # def bt( sindex, level):
+        #     def printt(msg):
+        #         print(level *' ', msg)
+        #     if sindex>=len(s):
+        #         # printt(' END ')
+        #         return
+        #     # printt(f'bt stack: {sindex}, {s[sindex:]}')
+        #     for i in range(len(wordDict)):
+        #         word = wordDict[i]
+        #         # print( ' looking at word : ', word)
+        #         # printt(f' looking at word: {word}')
+        #         if len(s)< sindex+len(word):
+        #             # print('  len(s) <= s+len(word)' )
+        #             # printt(f' len(s)={len(s)} <= s+len(word) {sindex}+{len(word)}. CONTINUE')
+        #             continue
+        #         if word != s[sindex:len(word) + sindex]:
+        #             # print('  ', word, '=', s[sindex:len(word)])
+        #             # printt( f'{word} = {s[sindex:sindex+len(word)]} -> CONTINUE')
+        #             continue
+        #         # print('MATCH!')
+        #         self.dp[sindex+len(word)] = True
+        #         bt(sindex+len(word), level+1)
+        # bt(0, 0)
+        # # print(self.dp)
+        # return self.dp[-1]
