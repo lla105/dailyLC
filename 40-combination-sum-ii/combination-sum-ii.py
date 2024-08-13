70ec3ca4a9e976ec2c19e16cfbaf019930d68355
lla105 <105393685+lla105@users.noreply.github.com>
2024-08-12 21:21:58 -0700

    Time: 144 ms (5.01%) | Memory: 16.6 MB (63.92%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -1,18 +1,23 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        c = sorted(candidates)
-        self.result = set()
-        def bt(curList, startindex):
+        self.resultlist = set()
+        c = candidates
+        def bt( curList, alist , index) :
+            if sum(curList) > target:
+                return
+            if sum(curList) == target:
+                # self.resultlist.append( tuple(curList) )
+                self.resultlist.add( tuple(sorted(curList)) )
+                # self.resultlist.add( tuple(curList))
+                return
 
-            if sum(curList)>target:
-                return
-            if sum(curList)==target:
-                self.result.add( tuple(curList) )
-                return
-            else:
-                for i in range(startindex, len(c)):
-                    if i>startindex and c[i] == c[i-1]:
-                        continue
-                    bt(curList+[c[i]] , i+1)
-        bt([] , 0)
-        return self.result
+            # print(curList, alist)
+            for i in range( index, len(alist)):
+                nextcurList = curList+[alist[i]]
+                # nextalist = alist[:i] + alist[i+1:]
+                if i>index and c[i]==c[i-1]:
+                    continue
+                bt( nextcurList , alist , i+1)
+        bt( [] , c , 0)
+
+        return self.resultlist
