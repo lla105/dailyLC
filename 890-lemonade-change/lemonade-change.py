6d944fd7c326965e0d7c12956544d653c271d33a
lla105 <105393685+lla105@users.noreply.github.com>
2024-08-14 22:11:11 -0700

    Time: 653 ms (11.11%) | Memory: 21.5 MB (15.58%) - LeetSync

--- 890-lemonade-change/lemonade-change.py
+++ 890-lemonade-change/lemonade-change.py
@@ -1,29 +1,27 @@
-class Solution:
-    def lemonadeChange(self, bills: List[int]) -> bool:
-        self.result = False
-        d = { 5:0, 10:0, 20:0 }
-        for i in range(len(bills)):
-            bill = bills[i]
-            print(f'{i} bill: {bill}')
-            if bill==20:
-                if d[5] < 3 and (d[10]<1 or d[5]<1):
-                    print(' FALSE: bill=$20' , d)
+class Solution(object):
+    def lemonadeChange(self, bills):
+        if bills[0] != 5:
+            return False
+        
+        five_dollers = 0
+        ten_dollers = 0
+
+        for x in bills:
+            if x == 5:
+                five_dollers += 1
+            elif x == 10:
+                if five_dollers > 0:
+                    five_dollers -= 1
+                else:
                     return False
-                if d[10]>0:
-                    d[10] -= 1
-                    d[5] -= 1
-                else :
-                    d[5] -=3 
-                    if d[5] <0:
-                        return False
-            
-            elif bill==10:
-                if d[5]<1:
-                    print( ' FALSE; bill=$10', d)
-                    return False
-                d[5]-=1
-                d[10] += 1
+                ten_dollers += 1
             else:
-                d[5] += 1
-            print(d)
+                if five_dollers > 0 and ten_dollers > 0:
+                    five_dollers -= 1
+                    ten_dollers -= 1
+                elif five_dollers > 2 :
+                    five_dollers -= 3
+                else:
+                    return False
+            print(five_dollers, ten_dollers)
         return True
