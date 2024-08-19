242d5b543889378589fa4830d3a6d9a920adf1e4
lla105 <105393685+lla105@users.noreply.github.com>
2024-08-19 15:37:09 -0700

    Time: 33 ms (80.89%) | Memory: 16.6 MB (63.34%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,9 +1,11 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        resultset = set()
+        resultset = []
 
         def bf( curList , index ):
-            resultset.add( tuple(curList) )
+            # resultset.add( tuple(curList) )
+            resultset.append( tuple(curList) )
+
             for i in range( index, len(nums)):
                 bf( curList+[nums[i]] , i+1)
 
