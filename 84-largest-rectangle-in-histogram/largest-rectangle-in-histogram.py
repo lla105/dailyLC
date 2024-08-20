7b4beed11cbeaa3247a8a58281f6847dc0c602ef
lla105 <105393685+lla105@users.noreply.github.com>
2024-08-19 20:44:59 -0700

    Time: 682 ms (36.29%) | Memory: 30 MB (96.84%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -1,38 +1,18 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        if len(heights)==1:
-            return heights[-1]
-        stack = []
-        maxarea = 0
+        stack = [-1]
+        max_area = 0
+
         for i in range(len(heights)):
-            startindex = i
-            if stack and heights[i] < stack[-1][0]:
-                while stack and heights[i] < stack[-1][0]:
-                    w = i - stack[-1][1] 
-                    h = stack[-1][0]
-                    curarea = w*h
-                    maxarea = max(maxarea, curarea)
-                    startindex = stack[-1][1]
-                    stack.pop()
-                stack.append( (heights[i], startindex) )
-            else:
-                stack.append( (heights[i], startindex) )
-            # print('cur stack: ', stack)
-
-        print('stack after 1st round: ', stack)
-        for i in range(len(stack)-1, 0, -1):
-            # print(stack[i][0] , stack[i][1])
-            w = len(heights)-stack[i][1]
-            h = stack[i][0]
-            temparea = w*h
-            # print(i,")   ", w,"*",h,"=",temparea)
-            maxarea = max(temparea, maxarea)
-        w = len(heights)
-        h = stack[0][0]
-        temparea = w*h
-        print("x)   ", w,"*",h,"=",temparea)
-
-        maxarea = max(temparea, maxarea)
-
+            while stack[-1] != -1 and heights[i] <= heights[stack[-1]]:
+                height = heights[stack.pop()]
+                width = i - stack[-1] - 1
+                max_area = max(max_area, height * width)
+            stack.append(i)
         
-        return maxarea
+        while stack[-1] != -1:
+            height = heights[stack.pop()]
+            width = len(heights) - stack[-1] - 1
+            max_area = max(max_area, height * width)
+        
+        return max_area
