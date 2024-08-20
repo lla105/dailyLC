34c1181161cd7d1ec2d603506c51e98edf2bd66c
lla105 <105393685+lla105@users.noreply.github.com>
2024-08-19 19:20:31 -0700

    Time: 73 ms (90.57%) | Memory: 17.7 MB (79.72%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,24 +1,28 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        self.pdic = Counter(p)
-        self.sdic = Counter(s[0:len(p)])
-        # del self.sdic['c']
-        self.result = []
-        if self.pdic == self.sdic:
-            self.result.append(0)
-        for i in range(len(p), len(s)):
-            lastchar = s[i-len(p)]
-            # print(i)
-            # print(' last char : ' , lastchar)
-            if self.sdic.get(lastchar) > 1:
-                self.sdic[lastchar] -= 1
+        d = {}
+        for c in p:
+            d.setdefault(c, 0)
+            d[c] += 1
+        
+        output = []
+        head = 0
+        tail = 0
+        while tail < len(s):
+            if s[tail] not in d:
+                while head < tail:
+                    d[s[head]] += 1
+                    head += 1
+                head = tail = tail + 1
+            elif d[s[tail]] == 0:
+                d[s[head]] += 1
+                head += 1
             else:
-                del self.sdic[s[i-len(p)]]
-
-
-            self.sdic[s[i]] = self.sdic.get(s[i] , 0) + 1
-            if self.pdic == self.sdic:
-                # print( ' MATCH ')
-                self.result.append(i-len(p)+1 )
-        return self.result
-
+                d[s[tail]] -= 1
+                tail += 1
+            
+            if tail - head == len(p):
+                output.append(head)
+                d[s[head]] += 1
+                head += 1
+        return output
