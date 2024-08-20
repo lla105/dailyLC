90e14185d91986df580e1b1286b9e1393fb79db9
lla105 <105393685+lla105@users.noreply.github.com>
2024-08-19 19:45:48 -0700

    Time: 392 ms (76.40%) | Memory: 17.2 MB (35.64%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -1,21 +1,43 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        maxfreq = 0
-        maxchar = ''
-        samemax = 0
-        counter = Counter(tasks)
-        for i,v in counter.items():
-            if v > maxfreq:
-                maxfreq = v
-                maxchar = i
-        for i,v in counter.items():
-            if v == maxfreq:
-                samemax+=1
-        samemax -= 1
-        print(maxchar, maxfreq)
-        print(samemax)
+        mostfreq = 0
+        d2 = defaultdict(list) # format : { freq:[char1, char2] }
+        d = defaultdict(list)
+        for i in range(len(tasks)):
+            t = tasks[i]
+            d[t] = d.get(t,0) + 1
+            mostfreq = max(mostfreq, d[t])
+        print(d, mostfreq)
+        mostfreqchars = []
+        for i,v in d.items():
+            if mostfreq == v:
+                mostfreqchars.append(i)
+        print(' mots freq chars : ', mostfreqchars)
 
-        temp = (n+1) * (maxfreq-1) + 1 + samemax
+        temp = mostfreq - 1
+        temp2 = n+1
+        temp = temp * temp2
+        temp += len(mostfreqchars)
+        if temp < len(tasks):
+            return len(tasks)
+        else:
+            return temp
+        # (most freq -1) * (n+1) ) + rest 
 
-        result = 99
-        return max(temp, len(tasks))
+        # a a c a
+        # n = 1
+        # a c a i a
+        # n = 2
+        # a c i a i i a
+
+        # a a a c 
+        # n = 1
+        # a c a c
+        # n = 2
+        # a c a c
+        # n = 4
+        # a c i a c
+        # a c i i i a i i i i a 
+
+        # n = 3
+        # aciiaiiia
