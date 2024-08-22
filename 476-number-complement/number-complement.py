95c8d46648d42a3eac8d8fdce2d13b3c11ed4fd2
lla105 <105393685+lla105@users.noreply.github.com>
2024-08-22 14:29:27 -0700

    Time: 30 ms (84.57%) | Memory: 16.5 MB (14.53%) - LeetSync

--- 476-number-complement/number-complement.py
+++ 476-number-complement/number-complement.py
@@ -1,7 +1,28 @@
 class Solution:
     def findComplement(self, num: int) -> int:
-        bit_length = num.bit_length()
-        
-        mask = (1 << bit_length) - 1
-        
-        return num ^ mask
+        def itob(somenum):
+            binary = ''
+            while somenum>0:
+                remain = somenum%2
+                binary = str(remain) + binary
+                somenum = somenum//2
+            return binary
+
+        if num==0:
+            return 1
+        binnum = itob(num)
+        print('binnum: ', binnum)
+        flipped = []
+        for num in binnum:
+            if num=='1':
+                flipped.append('0')
+            else:
+                flipped.append('1')
+        flipped = ''.join(flipped)
+        flipped = str(int(flipped))
+        print(flipped)
+        result = 0
+        for i in range(len(flipped)):
+            bit = int(flipped[i])
+            result = result*2 + bit  
+        return result
