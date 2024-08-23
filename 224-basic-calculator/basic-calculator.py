92406e94133f54c6801174a9a3625ab64813b4f4
lla105 <105393685+lla105@users.noreply.github.com>
2024-08-23 13:46:32 -0700

    Time: 66 ms (58.99%) | Memory: 17.8 MB (99.19%) - LeetSync

--- 224-basic-calculator/basic-calculator.py
+++ 224-basic-calculator/basic-calculator.py
@@ -1,35 +1,32 @@
 class Solution:
     def calculate(self, s: str) -> int:
+        stack = []
+        curnum = 0
+        sign = 1
+        result = 0
         def getsign(char):
-            if char=="+":
+            if char=='+':
                 return 1
             else:
                 return -1
-
-        sign = 1
-        stack = []
-        result = 0
-        num = 0
         for i in range(len(s)):
             c = s[i]
-            if c.isdigit():
-                num = num*10 + int(c)
-            elif c in '+-':
-                result += (num*sign)
-                sign =  getsign(c)
-                num = 0
+            if c.isdigit() : 
+                curnum = curnum*10 + int(c)
+            elif c in '+-' : 
+                result += curnum*sign
+                curnum = 0
+                sign = getsign(c)
             elif c=='(':
                 stack.append(result)
                 stack.append(sign)
-                result=0
-                num=0
+                result = 0
+                curnum = 0
                 sign = 1
-            elif c==')':
-                # tempresult = result+ (num*sign)
-                result += (num*sign)
+            elif c == ')':
+                result += curnum*sign
                 sign = stack.pop()
-                num = stack.pop()
-                result = (result*sign) + num
-                num = 0
-
-        return result+num*sign
+                prevnum = stack.pop()
+                result = (result*sign) + prevnum
+                curnum = 0                
+        return result+ curnum*sign
