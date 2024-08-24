5969b7d49d300af00892ba4b4940ef606caa5ad4
lla105 <105393685+lla105@users.noreply.github.com>
2024-08-23 17:14:30 -0700

    Time: 64 ms (63.35%) | Memory: 17.9 MB (59.73%) - LeetSync

--- 224-basic-calculator/basic-calculator.py
+++ 224-basic-calculator/basic-calculator.py
@@ -1,32 +1,60 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        stack = []
-        curnum = 0
-        sign = 1
-        result = 0
+        # s = '12-(3+4)'
         def getsign(char):
-            if char=='+':
-                return 1
-            else:
+            if char=='-':
                 return -1
+            return 1
+        num = 0
+        cursum = 0
+        sign = 1
+        stack = []
+        operators = ("+", "-")
         for i in range(len(s)):
-            c = s[i]
-            if c.isdigit() : 
-                curnum = curnum*10 + int(c)
-            elif c in '+-' : 
-                result += curnum*sign
-                curnum = 0
-                sign = getsign(c)
-            elif c=='(':
-                stack.append(result)
-                stack.append(sign)
-                result = 0
-                curnum = 0
+            char = s[i]
+            if char.isdigit():
+                num = num*10 + int(char)
+            elif char in operators:
+                cursum += int(num*sign)
+                sign = getsign(char)
+                num = 0
+            elif char == '(':
+                stack.append( int(cursum) )
+                if sign==1: 
+                    stack.append('+')
+                if sign==-1: 
+                    stack.append('-')
+                num = 0
+                cursum = 0
                 sign = 1
-            elif c == ')':
-                result += curnum*sign
-                sign = stack.pop()
-                prevnum = stack.pop()
-                result = (result*sign) + prevnum
-                curnum = 0                
-        return result+ curnum*sign
+            elif char == ')' :
+                cursum += int(num*sign)
+                oldsign = stack.pop()
+                oldsum = stack.pop()
+                # print('>> ', oldsum, oldsign, cursum)
+                if oldsign=='+':
+                    newsign = 1
+                else:
+                    newsign = -1
+                num = cursum
+                cursum = oldsum
+                sign = newsign
+                # cursum = int(oldsum) + int(newsign*cursum)
+
+        #stack = [ 12, '-']
+
+        answer = cursum + (num*sign)
+        return answer
+        # 11 + 12 - ( 3 + 4 )
+        # num = 1
+        # num = 11 
+        # cursum = 11, num = 0, sign = 1 
+        # num = 1
+        # num = 12
+        # result = cursum + (num*sign)
+        # sign = 1, + 
+        # stack.append(result), stack.append('+'), num=0, result = 0, sign=1
+        # ... 
+        # result = 7 = result(3) + (num*sign)
+        # ). stack.pop->sign. stack.pop->(prevresult). result=(result*sign)+prevresult
+
