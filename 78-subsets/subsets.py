574beedbe5c51af49af141c83aec675d7438c333
lla105 <105393685+lla105@users.noreply.github.com>
2024-08-24 22:52:00 -0700

    Time: 26 ms (98.29%) | Memory: 16.7 MB (24.86%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,13 +1,9 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        # self.result = set()
         self.result = []
-        def bf( curList , index ):
-            # self.result.add( tuple(curList) )
-            self.result.append( tuple(curList) )
-            if len(curList)>=len(nums):
-                return
-            for i in range(index, len(nums)):
-                bf( curList+[nums[i]] , i+1)
-        bf([], 0)
+        def bruteforce( curList , index ) :
+            self.result.append(curList)
+            for i in range( index, len(nums)):
+                bruteforce( curList+[nums[i]] , i+1 )
+        bruteforce( [] , 0 )
         return self.result
