40006d0341d8ce23dc75613f1ebf9c5c86c4cd48
lla105 <105393685+lla105@users.noreply.github.com>
2024-08-24 21:58:19 -0700

    Time: 39 ms (27.55%) | Memory: 16.5 MB (41.32%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,64 +1,46 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        def printmatrix(matrix):
-            print()
-            for i in range(len(matrix)):
-                for j in range(len(matrix[0])):
-                    print(matrix[i][j], end='  ')
-                print()
-        n = len(matrix)
-        m = len(matrix[0])
-        
-        if len(matrix[0])==1:
-            result = []
-            for i in range(len(matrix)):
-                result.append(matrix[i][0])
-            return result
-        if len(matrix) ==1 :
-            result = []
-            for i in range(len(matrix[0])):
-                result.append(matrix[0][i])
-            return result
-        top=0
-        left=0
-        right=m
-        bottom = n
-        starti=0
-        startj=0
-        cellcount = 0
+        visited = set() # format : { (i,j) , (i2,j2) }
 
-        result = []
-        while len(result) < m*n:
-        # while matrix[starti][startj] != "#" or top<bottom or left<right:
-            for i in range(right):
-                if matrix[top][i] != '#':
-                    result.append(matrix[top][i])
-                matrix[top][i] = '#'
-            top+=1
-            # down
-            for i in range(bottom):
-                if matrix[i][right-1] != '#':
-                    result.append(matrix[i][right-1])
-                matrix[i][right-1] = '#'
-            right -= 1
-            # left
-            for i in range(right-1, left-1, -1):
-                if matrix[bottom-1][i] != '#':
-                    result.append(matrix[bottom-1][i])
-                matrix[bottom-1][i] = '#'
-            bottom -= 1
-            # up
-            print(bottom,top,'...')
-            for i in range(bottom-1, top, -1):
-                print(i,left)
-                if matrix[i][left] != '#':
-                    result.append(matrix[i][left])
-                matrix[i][left] = '#'
-                starti = i
-                startj = left+1
-            left += 1
-            print('starti and j : ', starti, startj)
-
-        printmatrix(matrix)
-        print(' RESULT : ', result)
-        return result
+        m = len(matrix)
+        n = len(matrix[0])
+        count = 0
+        i = 0
+        j = 0
+        mat = matrix
+        path = []
+        # while (i+1,j) not in visited or (i,j+1) not in visited or (i-1,j) not in visited or (i,j-1) not in visited:
+        while len(path) < m*n:
+            # right 
+            while j < n and (i,j) not in visited:
+                print(i,j)
+                path.append(mat[i][j])
+                visited.add( (i,j) )
+                j+=1
+            i+=1
+            j-=1
+            #down
+            while i < m and (i,j) not in visited:
+                print(i,j)
+                path.append(mat[i][j])
+                visited.add( (i,j) )
+                i+=1
+            i-=1
+            j-=1
+            #left
+            while j>=0 and (i,j) not in visited:
+                print(i,j)
+                path.append(mat[i][j])
+                visited.add( (i,j) )
+                j-=1
+            j+=1
+            i-=1
+            #up
+            while i>=0 and (i,j) not in visited:
+                print(i,j)
+                path.append(mat[i][j])
+                visited.add( (i,j) )
+                i-=1
+            i+=1
+            j+=1
+        return path
