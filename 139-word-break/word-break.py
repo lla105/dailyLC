a84bfa759533eb79bf190629ea1abca6d823e0a8
lla105 <105393685+lla105@users.noreply.github.com>
2024-08-24 22:25:43 -0700

    Time: 42 ms (44.85%) | Memory: 16.6 MB (49.60%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,16 +1,27 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        dp = [ False ] * (len(s) + 1)
+
+        # self.result = False
+        # def bf( curList ) : 
+        #     # print('   looking for : ', s[len(curList): ] , curList)
+        #     if len(curList) > len(s):
+        #         return
+        #     if len(curList) == len(s):
+        #         self.result = True
+
+        #     for i in range(len(wordDict)):
+        #         if wordDict[i] == s[len(curList) : len(curList)+len(wordDict[i])]:
+        #             bf( curList+str(wordDict[i] ))
+        # bf('')
+        # return self.result
+        dp = [False ] * (len(s)+1)
         dp[0] = True
-        # dp[1:len(s)] = s[0:-1]
-
         for i in range(len(s)):
-            if dp[i] :
-                for word in wordDict:
-                    # print(i,s[i: i+len(word)], word)
-                    if s[i : i+len(word) ] == word :
-                        dp[i+len(word)] = True
-                # print(dp)
-
+            if not dp[i]:
+                continue
+            for j in range(len(wordDict)):
+                word = wordDict[j]
+                if word==s[i:i+len(word)]:
+                    dp[i+len(word)] = True
+        print(dp)
         return dp[-1]
-
