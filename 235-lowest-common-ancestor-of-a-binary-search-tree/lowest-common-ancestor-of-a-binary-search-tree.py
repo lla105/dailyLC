0f0a27af31a94050d59b5829f6de50b4a6e4375f
lla105 <105393685+lla105@users.noreply.github.com>
2024-08-25 13:49:43 -0700

    Time: 59 ms (32.22%) | Memory: 20.3 MB (30.89%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,10 +7,8 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        # self.foundleft = None
-        # self.foundright = None
-        self.answer = None
-        def trav(node):
+        
+        def trav( node ):
             if not node:
                 return None
             if node==p:
@@ -20,11 +18,9 @@ class Solution:
             left = trav(node.left)
             right= trav(node.right)
             if left and right:
-                self.answer = node
                 return node
             if left:
                 return left
             if right:
                 return right
         return trav(root)
-        return self.answer
