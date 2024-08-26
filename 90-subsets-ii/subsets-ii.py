c2b468bff4f4d1fa0f8ff9fb7fadfa6c2ea529ed
lla105 <105393685+lla105@users.noreply.github.com>
2024-08-26 14:21:24 -0700

    Time: 48 ms (8.75%) | Memory: 16.8 MB (12.46%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -1,14 +1,13 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        result = set()
+        # self.arr = []
+        self.arr = set()
         nums = sorted(nums)
+        def bf( curList , index ) :
+            # self.arr.append( tuple(curList) ) 
+            self.arr.add ( tuple(curList) )
+            for i in range( index, len(nums)):
+                bf( curList+[nums[i]] , i+1 )
 
-        def bt(curList, startindex):
-            if len(curList) >= len(nums):
-                result.add( tuple(curList))
-                return
-            result.add( tuple(curList))
-            for i in range(startindex, len(nums)):
-                bt(curList+[nums[i]] , i+1 )
-        bt([], 0)
-        return result
+        bf( [] , 0 ) 
+        return self.arr
