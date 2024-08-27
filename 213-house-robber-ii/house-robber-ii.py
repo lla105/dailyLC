ffbe3d5946ae1242c6fcf146ee7d1badf701a624
lla105 <105393685+lla105@users.noreply.github.com>
2024-08-26 21:18:33 -0700

    Time: 43 ms (11.93%) | Memory: 16.5 MB (32.92%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -3,22 +3,22 @@ class Solution:
         def robber( arr ) :
             if len(arr) < 3:
                 return [max(arr)]
-            dp = [ float('-inf') ] * len(arr)
+            dp = [ float('inf') ] * len(arr) 
             dp[0] = arr[0]
-            dp[1] = arr[1]
+            dp[1] = arr[1] 
             dp[2] = max(arr[2]+dp[0] , dp[1])
-            for i in range(3, len(arr)):
-                dp[i] = max(arr[i]+dp[i-2], dp[i-1], arr[i]+dp[i-3])
-
-            # print(dp)
+            for i in range( 3, len(arr)):
+                dp[i] = max(dp[i-1], dp[i-2]+arr[i], dp[i-3]+arr[i])
             return dp
+
         if len(nums) == 1:
-            return nums[-1]
-        route1 = nums[1:]
-        route2 = nums[:-1]
+            return nums[0]
+
+        route1 = nums[:-1]
+        route2 = nums[1:]
         dp1 = robber(route1)
         dp2 = robber(route2)
+
         print(dp1)
         print(dp2)
-        
         return max(dp1[-1] , dp2[-1])
