ad667c97d18aca77d3b86b2484e20971fc78698e
lla105 <105393685+lla105@users.noreply.github.com>
2024-08-26 21:08:17 -0700

    Time: 35 ms (65.07%) | Memory: 16.4 MB (70.05%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,17 +1,24 @@
 class Solution:
-    def helper(self, nums):
-        # print(">>>", nums)
-        dp = [0] * (len(nums)+2)
-        # print(dp)
-        for i in range(len(nums)-1, -1, -1):
-            dp[i] = max(nums[i]+dp[i+2] , dp[i+1])
-            # print(dp)
-        return max(dp)
     def rob(self, nums: List[int]) -> int:
+        def robber( arr ) :
+            if len(arr) < 3:
+                return [max(arr)]
+            dp = [ float('-inf') ] * len(arr)
+            dp[0] = arr[0]
+            dp[1] = arr[1]
+            dp[2] = max(arr[2]+dp[0] , dp[1])
+            for i in range(3, len(arr)):
+                dp[i] = max(arr[i]+dp[i-2], dp[i-1], arr[i]+dp[i-3])
+
+            # print(dp)
+            return dp
         if len(nums) == 1:
-            return nums[0]
-        rob1 = self.helper(nums[1:])
-        rob2 = self.helper(nums[:-1])
-
-
-        return max(rob1, rob2)
+            return nums[-1]
+        route1 = nums[1:]
+        route2 = nums[:-1]
+        dp1 = robber(route1)
+        dp2 = robber(route2)
+        print(dp1)
+        print(dp2)
+        
+        return max(dp1[-1] , dp2[-1])
