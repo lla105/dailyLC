2b119a836f39448d72f0e5827356109be0019cef
lla105 <105393685+lla105@users.noreply.github.com>
2024-08-27 16:24:10 -0700

    Time: 31 ms (86.42%) | Memory: 16.5 MB (80.27%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -10,16 +10,20 @@ class Solution:
         for i in range(2, len(dp)):
             j = i-1
             twodigits = int(s[j-1:j+1])
-            if s[j]=='0' :
-                if twodigits==10 or twodigits==20:
-                    dp[i] = dp[i-2]
-                else:
-                    return 0
+            # if s[j]=='0' :
+            #     if twodigits==10 or twodigits==20:
+            #         dp[i] = dp[i-2]
+            #     else:
+            #         return 0
 
-            else:
-                if twodigits>=10 and twodigits<=26:
-                    dp[i] = dp[i-1] + dp[i-2]
-                else:
-                    dp[i] = dp[i-1]
+            # else:
+            #     if twodigits>=10 and twodigits<=26:
+            #         dp[i] = dp[i-1] + dp[i-2]
+            #     else:
+            #         dp[i] = dp[i-1]
+            if s[j]!='0' :
+                dp[i] += dp[i-1]
+            if twodigits >= 10 and twodigits <= 26:
+                dp[i] += dp[i-2]
             # print(dp)
         return dp[-1]
