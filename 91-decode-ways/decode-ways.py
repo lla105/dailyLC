6391fae7052dcad0653a5a306bf7afd64f050cfd
lla105 <105393685+lla105@users.noreply.github.com>
2024-08-27 16:16:35 -0700

    Time: 48 ms (6.55%) | Memory: 16.7 MB (28.97%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,27 +1,25 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        if not s or s[0] == '0':
+        if not s or s[0]=='0':
             return 0
 
-        n = len(s)
-        dp = [0] * (n + 1)
+        dp = [0] * (len(s)+1)
         dp[0] = 1
         dp[1] = 1
 
-        for i in range(2, n + 1):
-            j = i - 1
-
-            # Check if the current character is '0'
-            if s[j] == '0':
-                if 10 <= int(s[j-1:j+1]) <= 26:
+        for i in range(2, len(dp)):
+            j = i-1
+            twodigits = int(s[j-1:j+1])
+            if s[j]=='0' :
+                if twodigits==10 or twodigits==20:
                     dp[i] = dp[i-2]
                 else:
-                    return 0  # Invalid encoding when '0' is not part of "10" or "20"
+                    return 0
+
             else:
-                # If the two-digit number is valid, consider both dp[i-1] and dp[i-2]
-                if 10 <= int(s[j-1:j+1]) <= 26:
+                if twodigits>=10 and twodigits<=26:
                     dp[i] = dp[i-1] + dp[i-2]
                 else:
                     dp[i] = dp[i-1]
-
-        return dp[n]
+            print(dp)
+        return dp[-1]
