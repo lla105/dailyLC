eadd7814c79549962ccfc8d686b1b90465254e73
lla105 <105393685+lla105@users.noreply.github.com>
2024-08-26 19:04:00 -0700

    Time: 698 ms (5.04%) | Memory: 32.3 MB (83.56%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,16 +1,22 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        result = set()
+        self.result = []
+        def trav( curList, index ) :
+            print('. curList : ', curList)
+            # if len(curList) >= len(s):
+            if index >= len(s): 
+                self.result.append( tuple(curList) )
 
-        def bf(curList, startindex):
-            if startindex >= len(s):
-                result.add( tuple(curList) )
-                return
-            for i in range(startindex, len(s)):
-                string1 = s[startindex:i+1]
-                string2 = ''.join(reversed(s[startindex:i+1]))
-                if string1 == string2:
-                    bf(curList + [string1] , i+1)
+            for i in range( index, len(s)):
+                s1 = s[index:i+1]
+                s2 = []
+                for j in range(len(s1)-1 , -1, -1):
+                    s2.append(s1[j])
+                s2 = ''.join(s2)
+                print( ' s1: ', s1 , ' vs ', s2)
+                if s1==s2:
+                    print('  match!')
+                    trav( curList+[s1] , i+1)
 
-        bf([] , 0)
-        return result
+        trav([] , 0)
+        return self.result
