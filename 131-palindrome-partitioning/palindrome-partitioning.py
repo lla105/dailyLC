5ac37b9e7680592a90426f5957f68ceb2763476b
lla105 <105393685+lla105@users.noreply.github.com>
2024-08-26 19:04:10 -0700

    Time: 477 ms (24.73%) | Memory: 32.3 MB (83.06%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -2,7 +2,7 @@ class Solution:
     def partition(self, s: str) -> List[List[str]]:
         self.result = []
         def trav( curList, index ) :
-            print('. curList : ', curList)
+            # print('. curList : ', curList)
             # if len(curList) >= len(s):
             if index >= len(s): 
                 self.result.append( tuple(curList) )
@@ -13,9 +13,9 @@ class Solution:
                 for j in range(len(s1)-1 , -1, -1):
                     s2.append(s1[j])
                 s2 = ''.join(s2)
-                print( ' s1: ', s1 , ' vs ', s2)
+                # print( ' s1: ', s1 , ' vs ', s2)
                 if s1==s2:
-                    print('  match!')
+                    # print('  match!')
                     trav( curList+[s1] , i+1)
 
         trav([] , 0)
