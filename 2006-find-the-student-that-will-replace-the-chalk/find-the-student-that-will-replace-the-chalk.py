73f4a756786db6dc841a7c3c0369b9083c6c4f00
lla105 <105393685+lla105@users.noreply.github.com>
2024-09-01 22:22:55 -0700

    Time: 585 ms (31.93%) | Memory: 29.8 MB (84.17%) - LeetSync

--- 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.py
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.py
@@ -1,15 +1,8 @@
-from typing import List
-
 class Solution:
     def chalkReplacer(self, chalk: List[int], k: int) -> int:
-        # Calculate the remainder of k after full rounds
-        temp = k % sum(chalk)
-        
-        # Loop over the students to find the one who will run out of chalk
-        for i, c in enumerate(chalk):
-            if temp < c:
+        remainchalk = k%sum(chalk)
+
+        for i in range(len(chalk)):
+            remainchalk -= chalk[i]
+            if remainchalk<0:
                 return i
-            temp -= c
-        
-        # The loop should always return a result before completing, but just in case:
-        return -1  # This case should never be hit
