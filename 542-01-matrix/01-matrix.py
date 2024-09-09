5d99b4ef90768f37c6d276dd17b978b2a81f4277
lla105 <105393685+lla105@users.noreply.github.com>
2024-09-08 21:58:27 -0700

    Time: 493 ms (35.91%) | Memory: 19.6 MB (56.72%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -1,52 +1,27 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         q = deque()
+        visited = set()
         for i in range(len(mat)):
             for j in range(len(mat[0])):
-                if mat[i][j] != 0:
-                    mat[i][j] = float('inf')
-                else:
+                if mat[i][j] == 0 :
+                    visited.add( (i,j) )
                     q.append( (i,j) )
-        directions = [ (0,1), (0,-1), (1,0), (-1,0) ]
+                else:
+                    mat[i][j] = float('inf')
+
+        directions = [ (1,0), (0,1), (-1,0), (0,-1) ]
+
         while q:
             x,y = q.popleft()
-            for dx,dy in directions:
+            for dx, dy in directions:
                 i = x+dx
                 j = y+dy
                 if i<0 or j<0 or i>=len(mat) or j>=len(mat[0]):
                     continue
-                if mat[i][j] == 0:
+                if (i,j) in visited:
                     continue
-                if mat[i][j] > mat[x][y]+1:
-                    mat[i][j] = mat[x][y] + 1
-                    q.append( (i,j) )
-                # if mat[i][j]!=0:
-                #     mat[i][j] = min(mat[x][y]+1 , mat[i][j])
-                #     q.append( (i,j) )
+                mat[i][j] = min(mat[i][j] , mat[x][y]+1 )
+                visited.add( (i,j) )
+                q.append( (i,j) )
         return mat
-
-
-        # if not mat:
-        #     return mat
-        # visited = set()
-        # def bfs(mat,i,j):
-        #     if i<0 or j<0 or i>=len(mat) or j>=len(mat[0]):
-        #         return float('inf')
-        #     if mat[i][j] == 0 or (i,j) in visited:
-        #         return 1
-        #     visited.add( (i,j) )
-        #     right = bfs(mat,i+1,j)
-        #     left = bfs(mat,i-1,j)
-        #     up = bfs(mat,i,j+1)
-        #     down = bfs(mat,i,j-1)
-        #     minpath = min( right, left, up, down )
-        #     # print(' >> ', math[i][j], 'min path: ', minpath)
-        #     mat[i][j] = mat[i][j] + minpath 
-        #     return float('inf')
-
-        # for i in range(len(mat)):
-        #     for j in range(len(mat[0])):
-        #         if mat[i][j]==1:
-        #             bfs(mat,i,j)
-
-        # return mat
