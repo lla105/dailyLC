90fe05bf0e083023ae0139f8b33e03f4815e8c66
lla105 <105393685+lla105@users.noreply.github.com>
2024-09-10 19:28:20 -0700

    Time: 38 ms (37.69%) | Memory: 16.5 MB (26.96%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.py
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.py
@@ -1,17 +1,36 @@
-class Solution(object):
-    def minBitFlips(self, start, goal):
-        """
-        :type start: int
-        :type goal: int
-        :rtype: int
-        """
-        # XOR to find differing bits between start and goal
-        xor_result = start ^ goal
+class Solution:
+    def minBitFlips(self, start: int, goal: int) -> int:
+        def inttobin(n):
+            if n==0:
+                return '0'
+            arr = []
+            while n>0:
+                arr.append(str(n%2))
+                n = n//2
+            return ''.join(reversed(arr))
+
+        startbin = str(inttobin(start))
+        goalbin = str(inttobin(goal))
+        # print(startbin, goalbin)
+        m = len(startbin)
+        n = len(goalbin)
+        if m > n :
+            diff = m-n
+            zeros = []
+            for i in range(diff):
+                zeros.append('0')
+            zerostring = ''.join(zeros)
+            goalbin = zerostring + goalbin
+        else:
+            diff = n-m
+            zeros = []
+            for i in range(diff):
+                zeros.append('0')
+            zerostring = ''.join(zeros)
+            startbin = zerostring + startbin
+        # print(startbin, goalbin)
         count = 0
-        
-        # Count the number of 1's in the XOR result
-        while xor_result > 0:
-            count += xor_result & 1  # Check if the least significant bit is 1
-            xor_result >>= 1         # Right shift to check the next bit
-        
+        for i in range(len(goalbin)):
+            if startbin[i] != goalbin[i] :
+                count +=1 
         return count
