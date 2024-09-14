112f09969feafdf0f942df15f405cea3a9d28b0f
lla105 <105393685+lla105@users.noreply.github.com>
2024-09-13 23:23:32 -0700

    Time: 48 ms (9.81%) | Memory: 16.6 MB (91.66%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,16 +1,24 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        
-        resultlist = []
-
-        def trav( curList , alist ):
-            if len(curList)>=len(nums):
-                resultlist.append(curList)
+        outputList = []
+        def bf( curList , availableNums ):
+            if len(curList) == len(nums):
+                print(' appending : ', curList )
+                outputList.append( tuple(curList) )
                 return
             
-            for i in range(len(alist)):
-                nextalist = alist[:i] + alist[i+1:]
-                trav( curList+[alist[i]] , nextalist )
+            for i in range(len( availableNums )):
+                nextcurList = curList + [availableNums[i]]
+                nextaNums = availableNums[:i] + availableNums[i+1:]
+                bf( nextcurList , nextaNums )
 
-        trav( [] , nums )
-        return resultlist
+        bf( [] , nums )
+        return outputList
+# [] [1,2,3] -> 1 -> 2,3
+# [1] [2,3] -> 2 -> 3
+# [1,2] [3] -> 3 -> [] -> append
+
+# [] [1,2,3] -> 2 -> 1,3
+# [2] [1,3] -> 1 -> 3
+# [2,1] [3] -> 3 -> []
+# [2,1,3]
