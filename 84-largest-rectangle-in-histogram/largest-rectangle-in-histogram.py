20f2252be8e8320db556d5ad88201788f1f7fd71
lla105 <105393685+lla105@users.noreply.github.com>
2024-09-13 23:14:36 -0700

    Time: 794 ms (7.74%) | Memory: 36.2 MB (7.03%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -1,18 +1,40 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        stack = [-1]
-        max_area = 0
-
+        stack = []
+        largest = 0
         for i in range(len(heights)):
-            while stack[-1] != -1 and heights[i] <= heights[stack[-1]]:
-                height = heights[stack.pop()]
-                width = i - stack[-1] - 1
-                max_area = max(max_area, height * width)
-            stack.append(i)
-        
-        while stack[-1] != -1:
-            height = heights[stack.pop()]
-            width = len(heights) - stack[-1] - 1
-            max_area = max(max_area, height * width)
-        
-        return max_area
+            h = heights[i]
+            if not stack:
+                stack.append( (h,i) )
+                continue
+            if stack[-1][0] <= h:
+                stack.append( (h,i) )
+            else:
+                savei = i
+                while stack and stack[-1][0] > h:
+                    oldh, oldi = stack.pop()
+                    curw = i-oldi
+                    curh = oldh
+                    largest = max(largest, curh*curw)
+                    savei = oldi
+                stack.append( (h,savei) )
+        print(' stack after 1st : ', stack)
+        newstack = []
+        for i in range(len(stack)):
+            if not newstack:
+                newstack.append( stack[i] )
+            else:
+                if newstack[-1][0] == stack[i][0]:
+                    continue
+                else:
+                    newstack.append( stack[i] )
+        for i in range(len( stack )):
+            if i==0:
+                curw = len(heights)
+                curh = stack[i][0]
+            else:
+                curw = len(heights) - stack[i][1]
+                curh = stack[i][0]
+            largest = max(largest, curw*curh)
+                
+        return largest
