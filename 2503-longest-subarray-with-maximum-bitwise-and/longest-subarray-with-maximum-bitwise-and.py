a1b06b8107d9002790010fda52080facf1aff21e
lla105 <105393685+lla105@users.noreply.github.com>
2024-09-13 21:14:52 -0700

    Time: 619 ms (86.18%) | Memory: 31.3 MB (42.18%) - LeetSync

--- 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.py
+++ 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.py
@@ -1,25 +1,16 @@
-# class Solution:
-#     def longestSubarray(self, nums: List[int]) -> int:
-#         best = [ 0,0 ] # [ num, maxlength]
-#         curbest = [0,0]
-
-#         for i in range(len(nums)):
-#             if i==0:
-#                 curbest = [ nums[i] , 1 ]
-#                 continue
-#             if nums[i] == curbest[0] :
-#                 curbest[1] += 1
-#             elif nums[i] < curbest[0] :
-#                 if curbest[1] 
-class Solution(object):
-    def longestSubarray(self, nums):
-        ans, cnt = 0, 0
-        
-        max_element = max(nums)
-        for num in nums:
-            if num == max_element:
-                cnt += 1
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        best = [ 0,0 ] # [ num, maxlength]
+        curbest = [0,0]
+        maxnum = max(nums)
+        count = 0
+        maxcount = 0
+        for i in range(len(nums)):
+            if nums[i] == maxnum :
+                count += 1
             else:
-                cnt = 0
-            ans = max(ans, cnt)
-        return ans
+                maxcount = max(maxcount, count)
+                count = 0
+        maxcount = max(maxcount, count)
+        
+        return maxcount
