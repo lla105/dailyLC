020b4c61d373cb8331211a38b67ff5291b411053
lla105 <105393685+lla105@users.noreply.github.com>
2024-09-16 22:04:26 -0700

    Time: 33 ms (72.59%) | Memory: 16.5 MB (49.32%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,5 +1,7 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
         self.d = {
             '2':'abc',
             '3':'def',
@@ -10,18 +12,16 @@ class Solution:
             '8':'tuv',
             '9':'wxyz'
         }
-        result = []
-        if not digits:
-            return result
-        def bf( curList , index) :
-            if len(curList) == len(digits):
-                result.append( ''.join(curList) )
+        self.output = []
+        def bf( curList, index) :
+            if  len(curList) == len(digits):
+                self.output.append( ''.join(curList) )
+                return
             for i in range( index, len(digits)):
-                if digits[i] not in self.d:
-                    continue
-                letters = self.d[digits[i]]
-                for char in letters:
-                    bf( curList+[char] , i+1 )
-        bf( [] , 0)
-
-        return result
+                digit = digits[i]
+                if digit in self.d :
+                    chars = self.d[digit]
+                    for char in chars:
+                        bf (curList+[char] , i+1 )
+        bf( [], 0 )
+        return self.output
