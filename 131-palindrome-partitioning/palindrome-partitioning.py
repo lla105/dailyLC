2177c852da2cf942746d3a0f67e147b40b0b2b15
lla105 <105393685+lla105@users.noreply.github.com>
2024-09-18 22:36:11 -0700

    Time: 522 ms (8.11%) | Memory: 32.4 MB (83.39%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,22 +1,21 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         self.result = []
-        def trav( curList, index ) :
-            # print('. curList : ', curList)
-            # if len(curList) >= len(s):
-            if index >= len(s): 
+        def isPalindrome( somestring ) :
+            s1 = somestring
+            s2 = []
+            for j in range(len(s1)-1, -1, -1):
+                s2.append(s1[j])
+            s2 = ''.join(s2)
+            return s1==s2
+            
+        def trav( curList, index) :
+            if index >= len(s):
                 self.result.append( tuple(curList) )
-
-            for i in range( index, len(s)):
-                s1 = s[index:i+1]
-                s2 = []
-                for j in range(len(s1)-1 , -1, -1):
-                    s2.append(s1[j])
-                s2 = ''.join(s2)
-                # print( ' s1: ', s1 , ' vs ', s2)
-                if s1==s2:
-                    # print('  match!')
-                    trav( curList+[s1] , i+1)
-
-        trav([] , 0)
+            for i in range( index, len(s) ):
+                temp = isPalindrome( s[index:i+1] )
+                print(temp)
+                if temp:
+                    trav( curList+[s[index:i+1]] , i+1 )
+        trav( [] , 0 )
         return self.result
