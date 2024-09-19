56916d5b6db199127ada738a2f1f5dd1acbace48
lla105 <105393685+lla105@users.noreply.github.com>
2024-09-18 22:46:21 -0700

    Time: 460 ms (65.35%) | Memory: 32.3 MB (84.40%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,28 +1,27 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        result = []
-        
-        def isPalindrome(start, end):
-            while start < end:
-                if s[start] != s[end]:
+        if not s:
+            return []
+        self.output = []
+        def isPalindrome( somestring ):
+            l = 0
+            r = len(somestring) - 1
+            while l<r:
+                if somestring[l] != somestring[r] :
                     return False
-                start += 1
-                end -= 1
+                l+=1
+                r-=1
             return True
-        
-        def backtrack(start, path):
-            if start == len(s):
-                result.append(path[:])  # Make a copy of path
+        def bf( curList , index ) :
+            # if len(curList) == len(s):
+                # return
+            if index >= len(s):
+                self.output.append( tuple(curList) )
                 return
-            
-            for end in range(start, len(s)):
-                if isPalindrome(start, end):
-                    # Add the substring s[start:end+1] to the current path
-                    path.append(s[start:end+1])
-                    # Recurse with the next start index
-                    backtrack(end + 1, path)
-                    # Backtrack, remove the last element added
-                    path.pop()
-        
-        backtrack(0, [])
-        return result
+            for i in range( index , len(s) ):
+                tempstring = s[index : i+1]
+                temp = isPalindrome( tempstring )
+                if temp:
+                    bf( curList+[tempstring] , i+ 1 )
+        bf( [] , 0 ) 
+        return self.output
