a1cb47144a65bafb53ed48b78c775633cb8e8d3e
lla105 <105393685+lla105@users.noreply.github.com>
2024-09-18 18:35:45 -0700

    Time: 73 ms (20.92%) | Memory: 16.7 MB (34.28%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -1,18 +1,18 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        outputlist = set()
         c = candidates
         c = sorted(c)
+        output = []
         def bf( curList , index ) :
             if sum(curList) == target:
-                outputlist.add ( tuple(curList) )
+                output.append( tuple(curList) )
                 return
             if sum(curList) > target:
                 return
-            
+
             for i in range( index , len(c)):
-                if i>index and c[i] == c[i-1] :
+                if i>index and c[i]==c[i-1]:
                     continue
-                bf( curList+[c[i]] , i+1 )
+                bf( curList+[c[i]] , i+1)
         bf( [] , 0 )
-        return outputlist
+        return output
