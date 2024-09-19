5fe7f4b5c0af1a6d1093cd33cb63ed1996dd5f40
lla105 <105393685+lla105@users.noreply.github.com>
2024-09-18 21:05:37 -0700

    Time: 50 ms (12.74%) | Memory: 16.8 MB (27.58%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
@@ -1,5 +1,5 @@
 class Solution:
-    def diffWaysToCompute(self, expression: str) -> List[int]:
+    def diffWaysToCompute(self, expression: str, level = 0) -> List[int]:
         res = []
         # ans = []
         for i in range(len(expression)):
@@ -8,16 +8,20 @@ class Solution:
             if oper in '+-*':
                 sub_str1 = expression[0 : i]
                 sub_str2 = expression[i + 1 : ]
-                s1 = self.diffWaysToCompute(sub_str1)
-                s2 = self.diffWaysToCompute(sub_str2)
+                s1 = self.diffWaysToCompute(sub_str1, level+1)
+                s2 = self.diffWaysToCompute(sub_str2, level+1)
+                print(level*'  ', 's1:',s1, ' , s2:', s2)
                 for i in s1:
                     for j in s2:
+                        print(level*'  ', int(i),oper,int(j))
                         if oper == "*":
                             res.append(int(i) * int(j))
                         if oper == "+":
                             res.append(int(i) + int(j))
                         if oper == "-":
                             res.append(int(i) - int(j))
+
+        print(level*'  ', 'res : ', res )
         if len(res) == 0:
             res.append(int(expression))
         # print(res)
