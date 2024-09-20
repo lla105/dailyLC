e5cd3d26de834fde7bb010692eb36222633cb2cc
lla105 <105393685+lla105@users.noreply.github.com>
2024-09-19 22:42:21 -0700

    Time: 36 ms (69.49%) | Memory: 16.8 MB (27.95%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
@@ -1,28 +1,25 @@
 class Solution:
-    def diffWaysToCompute(self, expression: str, level = 0) -> List[int]:
-        res = []
-        # ans = []
-        for i in range(len(expression)):
-            oper = expression[i]
-            # if oper == "+" or oper == "-" or oper == "*":
-            if oper in '+-*':
-                sub_str1 = expression[0 : i]
-                sub_str2 = expression[i + 1 : ]
-                s1 = self.diffWaysToCompute(sub_str1, level+1)
-                s2 = self.diffWaysToCompute(sub_str2, level+1)
-                print(level*'  ', 's1:',s1, ' , s2:', s2)
-                for i in s1:
-                    for j in s2:
-                        print(level*'  ', int(i),oper,int(j))
-                        if oper == "*":
-                            res.append(int(i) * int(j))
-                        if oper == "+":
-                            res.append(int(i) + int(j))
-                        if oper == "-":
-                            res.append(int(i) - int(j))
-
-        print(level*'  ', 'res : ', res )
-        if len(res) == 0:
-            res.append(int(expression))
-        # print(res)
-        return res
+    def diffWaysToCompute(self, expression: str) -> List[int]:
+        def bf( arr ):
+            self.diffWaysToCompute( arr )
+        output = []
+        exp = expression
+        for i in range(len(exp)):
+            op = exp[i]
+            if op not in '+-*':
+                continue
+            # left  = bf( exp[ :i] )
+            # right = bf( exp[i+1:] )
+            left = self.diffWaysToCompute( exp[0:i] )
+            right = self.diffWaysToCompute( exp[i+1: ] )
+            for i in left:
+                for j in right:
+                    if op == '*':
+                        output.append( int(i)*int(j) )
+                    elif op=='+':
+                        output.append( int(i)+int(j) )
+                    elif op=='-':
+                        output.append( int(i)-int(j) )
+        if not output:
+            output.append(int(exp))
+        return output
