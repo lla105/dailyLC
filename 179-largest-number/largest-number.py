5edbc57cd3297ba18225af6a39effd12a77e7598
lla105 <105393685+lla105@users.noreply.github.com>
2024-09-19 22:04:41 -0700

    Time: 72 ms (5.10%) | Memory: 16.6 MB (50.31%) - LeetSync

--- 179-largest-number/largest-number.py
+++ 179-largest-number/largest-number.py
@@ -1,15 +1,17 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
+        def sortit( nums ) :
+            for i in range(len(nums)):
+                for j in range(i+1, len(nums)):
+                    # print(i,j)
+                    if int(nums[i]+nums[j]) > int(nums[j]+nums[i]) :
+                        continue
+                    else:
+                        nums[i], nums[j] = nums[j] , nums[i]
+            return nums
         for i in range(len(nums)):
             nums[i] = str(nums[i])
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                # print(i,j)
-                if int(nums[i]+nums[j]) > int(nums[j]+nums[i]) :
-                    continue
-                else:
-                    nums[i], nums[j] = nums[j] , nums[i]
-        print(nums)
+        nums = sortit(nums)
         result = ''.join(nums)
         if result[0] == '0':
             return '0'
