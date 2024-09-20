3e94c72e7f6425cfa0962088ec64f76f424dc063
lla105 <105393685+lla105@users.noreply.github.com>
2024-09-19 21:22:47 -0700

    Time: 38 ms (73.26%) | Memory: 16.6 MB (14.00%) - LeetSync

--- 179-largest-number/largest-number.py
+++ 179-largest-number/largest-number.py
@@ -2,10 +2,8 @@ class Solution:
     def largestNumber(self, nums: List[int]) -> str:
         # Convert integers to strings
         array = list(map(str, nums))
-        
         # Custom sorting with a lambda function
         array.sort(key=lambda x: x*10, reverse=True)
-        
         # Handle the case where the largest number is "0"
         if array[0] == "0":
             return "0"
