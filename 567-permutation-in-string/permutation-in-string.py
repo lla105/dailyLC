4b27adb8c55170a50670e9879b67b4651301cb9d
lla105 <105393685+lla105@users.noreply.github.com>
2024-09-22 00:32:32 -0700

    Time: 220 ms (24.04%) | Memory: 16.8 MB (17.58%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -2,35 +2,6 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s2)<len(s1):
             return False
-        s1check = [0]*26
-        # print(s1check)
-        chardict = { 'a' : 0 ,
-                    'b' : 1 ,
-                    'c' : 2 ,
-                    'd' : 3 ,
-                    'e' : 4 ,
-                    'f' : 5 ,
-                    'g' : 6 ,
-                    'h' : 7 ,
-                    'i' : 8 ,
-                    'j' : 9 ,
-                    'k' : 10 ,
-                    'l' : 11 ,
-                    'm' : 12 ,
-                    'n' : 13 ,
-                    'o' : 14 ,
-                    'p' : 15 ,
-                    'q' : 16 ,
-                    'r' : 17 ,
-                    's' : 18 ,
-                    't' : 19 ,
-                    'u' : 20 ,
-                    'v' : 21 ,
-                    'w' : 22 ,
-                    'x' : 23 ,
-                    'y' : 24 ,
-                    'z' : 25 ,
-                    }
         d1 = {}
         d2 = {}
         for i in range(len(s1)):
