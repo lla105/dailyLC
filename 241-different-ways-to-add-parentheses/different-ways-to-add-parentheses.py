aa101058d5df441e32f44a32004e3d7e9409b671
lla105 <105393685+lla105@users.noreply.github.com>
2024-09-23 01:18:56 -0700

    Time: 40 ms (41.19%) | Memory: 16.7 MB (44.49%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
@@ -2,32 +2,25 @@ class Solution:
     def diffWaysToCompute(self, expression: str) -> List[int]:
         output = []
         exp = expression
-
         def dfs( arr ) :
-            curlist = []
+            templist = []
             for i in range(len(arr)):
                 char = arr[i]
                 if char not in '+-*':
                     continue
-                left = dfs( arr[ :i] )
-                right = dfs( arr[i+1:] )
-                print(left, char, right)
+                left = dfs( arr[:i] )
+                right = dfs( arr[i+1:])
                 for num1 in left:
                     for num2 in right:
-                        if char=='+':
-                            tempnum = int(num1) + int(num2)
+                        if char == '+':
+                            tempnum = int(num1)+int(num2)
                         elif char=='-':
-                            tempnum = int(num1) - int(num2)
+                            tempnum = int(num1)-int(num2)
                         elif char=='*':
-                            tempnum = int(num1) * int(num2)
-                        curlist.append(tempnum)
-            if not curlist:
-                return [ int(arr) ]
-            return curlist            
-        return dfs(exp)
-        
-        # for i in range(len(exp)):
-        #     char = exp[i]
-        #     if char not in '+-*':
-        #         continue
-            
+                            tempnum = int(num1)*int(num2)
+                        templist.append(tempnum)
+            if not templist:
+                return [int(arr)]
+            return templist
+
+        return dfs( exp )
