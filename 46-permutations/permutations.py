27ef62729a67c4218ea2ac8cdf31533b237effd0
lla105 <105393685+lla105@users.noreply.github.com>
2024-09-24 13:18:36 -0700

    Time: 37 ms (77.77%) | Memory: 16.7 MB (33.58%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,24 +1,16 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        outputList = []
-        def bf( curList , availableNums ):
-            if len(curList) == len(nums):
-                print(' appending : ', curList )
-                outputList.append( tuple(curList) )
+        
+        output = []
+        def bf( curList , candidates ) :
+            if len(curList) == len(nums) :
+                output.append( tuple(curList ) )
                 return
-            
-            for i in range(len( availableNums )):
-                nextcurList = curList + [availableNums[i]]
-                nextaNums = availableNums[:i] + availableNums[i+1:]
-                bf( nextcurList , nextaNums )
-
+            for i in range( len (candidates) ):
+                nextList = curList+[candidates[i]]
+                firsthalf = candidates[:i]
+                secondhalf= candidates[i+1:]
+                nextcand = firsthalf + secondhalf
+                bf( nextList , nextcand)
         bf( [] , nums )
-        return outputList
-# [] [1,2,3] -> 1 -> 2,3
-# [1] [2,3] -> 2 -> 3
-# [1,2] [3] -> 3 -> [] -> append
-
-# [] [1,2,3] -> 2 -> 1,3
-# [2] [1,3] -> 1 -> 3
-# [2,1] [3] -> 3 -> []
-# [2,1,3]
+        return output
