f705fe1f25697e06733d1a48aa6fe63f4c82ee6d
lla105 <105393685+lla105@users.noreply.github.com>
2024-09-24 23:53:53 -0700

    Time: 45 ms (5.08%) | Memory: 16.4 MB (73.56%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,24 +1,19 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        def robber( arr ) :
-            if len(arr) < 3:
-                return [max(arr)]
-            dp = [ float('inf') ] * len(arr) 
-            dp[0] = arr[0]
-            dp[1] = arr[1] 
-            dp[2] = max(arr[2]+dp[0] , dp[1])
-            for i in range( 3, len(arr)):
-                dp[i] = max(dp[i-1], dp[i-2]+arr[i], dp[i-3]+arr[i])
-            return dp
-
-        if len(nums) == 1:
-            return nums[0]
-
-        route1 = nums[:-1]
-        route2 = nums[1:]
-        dp1 = robber(route1)
-        dp2 = robber(route2)
-
-        print(dp1)
-        print(dp2)
-        return max(dp1[-1] , dp2[-1])
+        if len(nums) < 4 :
+            return max(nums)
+        arr1 = nums[:len(nums)-1]
+        arr2 = nums[1:]
+        dp1 = [0] * len(arr1)
+        dp2 = [0] * len(arr2)
+        for i in range(2) :
+            dp1[i] = arr1[i]
+            dp2[i] = arr2[i]
+        for i in range( 2, len(arr1)):
+            if i==2:
+                dp1[i] = arr1[i]+dp1[i-2]
+                dp2[i] = arr2[i]+dp2[i-2]
+                continue
+            dp1[i] = max( arr1[i]+dp1[i-2] , arr1[i]+dp1[i-3])
+            dp2[i] = max( arr2[i]+dp2[i-2] , arr2[i]+dp2[i-3])
+        return max( dp1[-1] , dp2[-1], dp1[-2], dp2[-2])
