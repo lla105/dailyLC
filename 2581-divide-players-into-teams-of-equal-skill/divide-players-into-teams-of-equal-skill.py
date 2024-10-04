d1942d56429b8a5d77243aca798f80d37213d434
lla105 <105393685+lla105@users.noreply.github.com>
2024-10-03 23:39:23 -0700

    Time: 541 ms (5.13%) | Memory: 29.9 MB (22.83%) - LeetSync

---
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.py
@@ -0,0 +1,30 @@
+class Solution:
+    def dividePlayers(self, skill: List[int]) -> int:
+        totalsum = sum(skill)
+        halflen = (len(skill))/2
+        if len(skill) == 2:
+            return skill[0] * skill[1]
+
+        if halflen % 1 != 0 or (len(skill))%2!= 0 :
+            return -1
+        d = Counter(skill)
+        pairsum = totalsum / halflen
+        output = []
+        for i in range(len(skill)) :
+            num = int(skill[i])
+            if d[num] <= 0:
+                continue
+            diff = int(pairsum - num)
+            d[num] -= 1
+            if d[diff] <= 0 :
+                continue
+            d[diff] -=1 
+            output.append( (num,diff) )
+        answer = 0
+        print(f' output : {output}')
+        if not output or len(output) != halflen :
+            return -1
+        for num1,num2 in output:
+            answer += (num1*num2)
+        return answer
+        
