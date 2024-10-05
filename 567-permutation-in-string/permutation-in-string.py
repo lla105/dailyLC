894c844a9d0b643d9d2f7aee25e97df7a928bbae
lla105 <105393685+lla105@users.noreply.github.com>
2024-10-04 17:46:51 -0700

    Time: 54 ms (77.04%) | Memory: 16.5 MB (98.77%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,23 +1,22 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s2)<len(s1):
+        if len(s1) > len(s2):
             return False
         d1 = {}
         d2 = {}
         for i in range(len(s1)):
-            d1[s1[i]] = d1.get( s1[i] , 0) +1
-            d2[s2[i]] = d2.get( s2[i] , 0) + 1
+            d1[s1[i]] = d1.get(s1[i], 0) + 1
+            d2[s2[i]] = d2.get(s2[i], 0) + 1
         if d1==d2:
             return True
-        for i in range( len(s1) , len(s2) ):
-            d2[s2[i-len(s1)]] = d2.get( s2[i-len(s1)] , 0 ) - 1
-            d2[s2[i]] = d2.get( s2[i] , 0 ) + 1
-            print(d2)
-            count = 0
-            for i,v in d1.items():
-                if d2.get( i , 0 ) == v:
-                    count += v
-            if count == len(s1):
+        print(d1,d2)
+        for i in range(len(s1), len(s2)):
+            leftindex = i-len(s1)
+            rightindex = i
+            d2[ s2[leftindex] ] -= 1
+            if d2[s2[leftindex]]<=0:
+                d2.pop( s2[leftindex] )
+            d2[ s2[rightindex] ] = d2.get( s2[rightindex] , 0 ) + 1
+            if d1==d2:
                 return True
-
         return False
