625ea35d14618920705fce9bf2627a10ff2236a6
lla105 <105393685+lla105@users.noreply.github.com>
2024-10-14 14:21:52 -0700

    Time: 744 ms (36.36%) | Memory: 31.2 MB (78.68%) - LeetSync

--- 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.py
+++ 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.py
@@ -1,12 +1,10 @@
 class Solution:
     def maxKelements(self, nums: List[int], k: int) -> int:
-        # nums = [2,5,3,7,10,0,99]
         for i in range(len(nums)):
             nums[i] *= -1 
         heapq.heapify(nums)
         result = 0
         count = 0
-        print(nums)
         while count < k:
             temp = -1*(heapq.heappop(nums) )
             result += temp
@@ -14,7 +12,5 @@ class Solution:
             newnum = ceil(temp/3)
             newnum = -1* newnum
             heapq.heappush( nums, newnum )
-            # print(' sum : ', result, ' from ', temp)
-            # print(nums)
 
         return result
