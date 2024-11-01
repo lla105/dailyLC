8074dbfd9a74b9f2e7dc243693146763669c522c
lla105 <105393685+lla105@users.noreply.github.com>
2024-11-01 15:58:53 -0700

    Time: 0 ms (100.00%) | Memory: 16.7 MB (5.18%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -10,9 +10,24 @@ class Solution:
         for i in range(2,numRows):
             arr = [1]* (i+1)
             # print(arr)
-            for j in range(1,i):
-                prevval1 = output[-1][j-1] 
-                prevval2 = output[-1][j]
-                arr[j] = prevval1 + prevval2
+            roundlimit = (i//2)
+            leftj = 1
+            rightj = i-1
+            while leftj <= roundlimit:
+                prevval1 = output[-1][leftj-1] 
+                prevval2 = output[-1][leftj]
+                arr[leftj] = prevval1 + prevval2
+                arr[rightj] = arr[leftj]
+                leftj += 1
+                rightj -= 1
+            # for j in range(1, roundlimit):
+            #     prevval1 = output[-1][j-1] 
+            #     prevval2 = output[-1][j]
+            #     arr[j] = prevval1 + prevval2
             output.append(arr)
         return output
+
+3, 1
+4, 1
+5, 2
+6, 2
