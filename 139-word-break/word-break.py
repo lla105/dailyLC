f1881ba4a73a8bd2c900f3bcecac8e466b1f53a0
lla105 <105393685+lla105@users.noreply.github.com>
2024-11-02 11:31:41 -0700

    Time: 3 ms (69.51%) | Memory: 16.7 MB (34.47%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,27 +1,38 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-
-        # self.result = False
-        # def bf( curList ) : 
-        #     # print('   looking for : ', s[len(curList): ] , curList)
-        #     if len(curList) > len(s):
-        #         return
-        #     if len(curList) == len(s):
-        #         self.result = True
-
-        #     for i in range(len(wordDict)):
-        #         if wordDict[i] == s[len(curList) : len(curList)+len(wordDict[i])]:
-        #             bf( curList+str(wordDict[i] ))
-        # bf('')
-        # return self.result
-        dp = [False ] * (len(s)+1)
+        dp = [ False ] * ( len(s) + 1)
         dp[0] = True
         for i in range(len(s)):
-            if not dp[i]:
+            if not dp[i] : 
                 continue
-            for j in range(len(wordDict)):
-                word = wordDict[j]
-                if word==s[i:i+len(word)]:
+            for word in wordDict:
+                if s[i:i+len(word)] == word :
                     dp[i+len(word)] = True
-        print(dp)
         return dp[-1]
+        # dp = [ False ] * ( len(s) + 1 )
+        # dp[0] = True
+        # wordSet = set(wordDict)
+
+        # def explore( index ) :
+        #     if index == len(s):
+        #         dp[index] = True
+        #     if index >= len(s):
+        #         print(' Over length ')
+        #         return
+
+        #     print(' >>>> explore (', index,'): ', s[index])
+        #     dp[index] = True
+        #     for word in wordDict:
+        #         if s[index:index+len(word)] != word or not dp[i] :
+        #             continue
+        #         print('3)  word: ', word, ' vs ', s[index:index+len(word)])
+        #         explore (index+len(word))
+
+        # for i in range(len(s)):
+        #     for word in wordDict:
+        #         if i+len(word) >= len(s) or s[i:i+len(word)] != word or not dp[i]:
+        #             continue
+        #         print('1) match: ', s[i:i+len(word)])
+        #         explore( i+len(word) )
+
+        # return dp[-1]
