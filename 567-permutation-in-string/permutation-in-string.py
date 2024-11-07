e3d94e3246afbe6b51582d3d664df749746a2c96
lla105 <105393685+lla105@users.noreply.github.com>
2024-11-07 14:27:30 -0800

    Time: 12 ms (91.70%) | Memory: 16.8 MB (8.12%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -4,12 +4,12 @@ class Solution:
             return False
         d1 = {}
         d2 = {}
+        d3={}
         for i in range(len(s1)):
             d1[s1[i]] = d1.get(s1[i], 0) + 1
             d2[s2[i]] = d2.get(s2[i], 0) + 1
         if d1==d2:
             return True
-        print(d1,d2)
         for i in range(len(s1), len(s2)):
             leftindex = i-len(s1)
             rightindex = i
