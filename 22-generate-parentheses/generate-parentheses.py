aa1118f034b9762d8d9ec9c029eb46dea4e68b0e
lla105 <105393685+lla105@users.noreply.github.com>
2024-11-11 21:04:36 -0800

    Time: 0 ms (100.00%) | Memory: 16.9 MB (56.31%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -3,14 +3,13 @@ class Solution:
         result = []
         def bt(openb, closeb, curlist, level):
             if closeb==openb and openb==n:
-                print(level*'  ', 'APPEND')
                 result.append(''.join(curlist))
                 return
+            # if n>openb:
             if n>openb and n>closeb:
-                print(level*'  ','ADDING OPEN')
                 bt(openb+1, closeb, curlist+['('], level+1)
+            # if openb==n and openb>closeb:
             if openb<=n and openb>closeb:
-                print(level*'  ','ADDING CLOSE')
                 bt(openb,closeb+1,curlist+[')'], level+1)
 
         bt(0,0, [],0)
