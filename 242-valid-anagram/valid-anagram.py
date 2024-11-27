65ac236619ff6fc784243cb4b2b437413211842d
lla105 <105393685+lla105@users.noreply.github.com>
2024-11-26 18:07:39 -0800

    Time: 15 ms (48.99%) | Memory: 17.3 MB (25.67%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,6 +1,17 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s = Counter(s)
-        t = Counter(t)
+        sdic = {}
+        pdic = {}
+        for i in range(len(s)):
+            sdic[s[i]] = sdic.get(s[i], 0) + 1
+            pdic['a'] = 2+2
+        print(sdic)
+        tdic = {}
+        for i in range(len(t)):
+            tdic[t[i]] = tdic.get(t[i] , 0) + 1
+        print(tdic)
 
-        return s==t
+        # print(tdic == sdic)
+        return tdic==sdic
+
+        return True
