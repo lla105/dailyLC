7827ce385c5e9d284bc4dfc4a2c75e9c9fb1e482
lla105 <105393685+lla105@users.noreply.github.com>
2024-12-29 15:51:57 -0800

    Time: 4 ms (52.04%) | Memory: 17.8 MB (6.14%) - LeetSync

--- 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.py
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.py
@@ -1,13 +1,14 @@
 class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
-        maxnum = float('-inf')
+        n_gifts = []
+        for i in range(len(gifts)):
+            n_gifts.append( -1*gifts[i] )
+        heapq.heapify(n_gifts)
+        # print(n_gifts)
         for _ in range(k):
-            maxnum = float('-inf')
-            maxindex = -1
-            for i in range(len(gifts)):
-                if gifts[i] > maxnum :
-                    maxindex = i
-                    maxnum = gifts[i]
-            gifts[maxindex] = int(sqrt(gifts[maxindex]))
-            # print('>>> ', gifts)
-        return sum(gifts)
+            rm_num = heapq.heappop(n_gifts)
+            rm_num = -1 * rm_num
+            rm_num = sqrt(rm_num)
+            heapq.heappush( n_gifts, int((-1*rm_num)) )
+            # print(n_gifts)
+        return sum(n_gifts)*-1
