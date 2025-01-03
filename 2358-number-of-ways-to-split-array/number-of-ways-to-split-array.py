0c0eab6f90e5ba80eabf5f11e4935a80f6bf1e72
lla105 <105393685+lla105@users.noreply.github.com>
2025-01-02 20:28:48 -0800

    Time: 75 ms (45.66%) | Memory: 32.4 MB (51.34%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.py
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.py
@@ -1,18 +1,21 @@
 class Solution:
     def waysToSplitArray(self, nums: List[int]) -> int:
-        stack = [ nums[0] ]
-        q = deque()
+        # stack = [ nums[0] ]
+        # q = deque()
+        qsum = 0
         splits = 0
+        stacksum = nums[0]
         for i in range(1,len(nums)):
-            q.append( nums[i] )
-        stacksum = sum(stack)
-        qsum = sum(q)
+            # q.append( nums[i] )
+            qsum += nums[i]
+        # stacksum = sum(stack)
         if stacksum >= qsum:
             splits += 1
         for i in range( 1 , len(nums)-1 ):
-            rm_num = q.popleft()
+            # rm_num = q.popleft()
+            rm_num = nums[i]
             qsum -= rm_num
-            stack.append( rm_num )
+            # stack.append( rm_num )
             stacksum += rm_num
             if stacksum >= qsum:
                 splits += 1
