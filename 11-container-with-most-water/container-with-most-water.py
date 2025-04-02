bcfeafcaa0a9dc0ffa55809de342e567b91bfe2e
lla105 <105393685+lla105@users.noreply.github.com>
2025-04-02 14:49:56 -0700

    Time: 102 ms (58.94%) | Memory: 28.4 MB (90.50%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,14 +1,27 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        h = height
+        max_vol = 0
         l = 0
-        r = len(h)-1
-        most = float('-inf')
-        while l<r:
-            lowerheight = min(h[l] , h[r])
-            most = max(most, lowerheight*(abs(l-r)))
-            if h[l] > h[r] :
+        r = len(height) - 1
+        while l<r :
+            cur_vol = min(height[l],height[r]) * (r-l)
+            max_vol = max(max_vol , cur_vol )
+            if height[l] > height[r] :
                 r -= 1
             else:
                 l += 1
-        return most
+        return max_vol
+
+        #height = [1,8,6,2,5,4,8,3,7]
+        # 0 : min(1,7)*8 = 8
+        # 1 : min(8,7)*7 = 49
+        # 2 : min(8,3)*6 = 18
+        # 3 : min(8,8)*5 = 40
+
+        #height = [1,1]
+        # 0 : min(1,1)*1 = 1
+
+        #height = [1,x,2] > [1,2]=w*1 > 
+        # move the lower height pointer. 
+        #height = [ ,l,r] : if x lower: take x, if x higher, take r (higher value)
+        #height = [l,r, ] : if x lower: take x, if x higher, take l (lower value)
