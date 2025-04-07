3447632e9ded4a2ce0769a06265c30bcaf0a8678
lla105 <105393685+lla105@users.noreply.github.com>
2025-04-07 14:08:06 -0700

    Time: 3 ms (20.59%) | Memory: 18.4 MB (60.72%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -6,21 +6,29 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        output_arr = []
+        output_arr = [] # format : [ [val1], [val2,val3], ... ] 
         d = {} #format : { level:[val1,val2] }
+        
+        # 3,9,20 > lvl=1 > output_arr=[ [3], [9] ] , len()=2 , [ [3], [9,20] ]
+        # 3,9,20,25 > lvl=2 > [ [3], [9,20] ] , len()=2 , [ [3], [9,20], [15] ]
+
 
         def trav( node, level ) :
             if not node:
                 return
-            if level in d:
-                d[level].append(node.val)
-            else:
-                d[level] = [ node.val ]
+            if level >= len(output_arr) :
+                output_arr.append( [node.val] )
+            else: 
+                output_arr[level].append(node.val)
+            # if level in d:
+            #     d[level].append(node.val)
+            # else:
+            #     d[level] = [ node.val ]
             left = trav( node.left, level+1 )
             right = trav( node.right, level+1 )
             return 
         trav( root , 0)
 
-        for level, val_list in d.items():
-            output_arr.append( tuple(val_list) )
+        # for level, val_list in d.items():
+        #     output_arr.append( tuple(val_list) )
         return output_arr
