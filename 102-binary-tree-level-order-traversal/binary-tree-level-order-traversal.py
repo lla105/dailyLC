27536db73ee59bfb035804c96d4c51ce214809dd
lla105 <105393685+lla105@users.noreply.github.com>
2025-04-07 13:38:20 -0700

    Time: 0 ms (100.00%) | Memory: 18.7 MB (18.11%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -6,17 +6,21 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        leveld = defaultdict(list)
+        output_arr = []
+        d = {} #format : { level:[val1,val2] }
 
-        def dfs(node, level):
+        def trav( node, level ) :
             if not node:
-                return node
-            leveld[level].append(node.val)
-            left = dfs(node.left, level+1)
-            right = dfs(node.right, level+1)
-            return
-        dfs(root, 0)
-        result = []
-        for i,v in leveld.items():
-            result.append(v)
-        return result
+                return
+            if level in d:
+                d[level].append(node.val)
+            else:
+                d[level] = [ node.val ]
+            left = trav( node.left, level+1 )
+            right = trav( node.right, level+1 )
+            return 
+        trav( root , 0)
+
+        for level, val_list in d.items():
+            output_arr.append( tuple(val_list) )
+        return output_arr
