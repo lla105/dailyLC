ec8b46db7584102aad3416cc29ec2e53cff0d9c4
lla105 <105393685+lla105@users.noreply.github.com>
2025-04-08 23:41:28 -0700

    Time: 61 ms (86.32%) | Memory: 17.7 MB (53.07%) - LeetSync

--- 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.py
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.py
@@ -1,21 +1,16 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
         s = set()
+        min_num = float('inf')
         for num in nums:
+            min_num = min(min_num, num)
             s.add(num)
-        min_num = min(s)
         if min_num < k :
             return -1
         same_min = False
         if min_num == k :
             same_min = True
-        max_heap = []
-        for num in s:
-            heapq.heappush(max_heap, -num)
-        output = 0
-        while max_heap :
-            output += 1
-            heapq.heappop(max_heap)
+        output = len(s)
         if same_min :
             output -= 1
         return output
