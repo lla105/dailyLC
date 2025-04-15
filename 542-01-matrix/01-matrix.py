d59802c3178f22846ffbd34811a8c8a4c9aab216
lla105 <105393685+lla105@users.noreply.github.com>
2025-04-15 15:38:05 -0700

    Time: 272 ms (11.71%) | Memory: 21.1 MB (31.04%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -1,27 +1,24 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         q = deque()
-        visited = set()
         for i in range(len(mat)):
             for j in range(len(mat[0])):
-                if mat[i][j] == 0 :
-                    visited.add( (i,j) )
+                if mat[i][j] == 0:
                     q.append( (i,j) )
                 else:
-                    mat[i][j] = float('inf')
-
-        directions = [ (1,0), (0,1), (-1,0), (0,-1) ]
-
+                    mat[i][j] = -1
+        # print(q)
+        dir = [ (1,0), (0,1), (-1,0), (0,-1) ]
+        visited = set()
         while q:
             x,y = q.popleft()
-            for dx, dy in directions:
+            for dx, dy in dir:
                 i = x+dx
                 j = y+dy
-                if i<0 or j<0 or i>=len(mat) or j>=len(mat[0]):
+                if i<0 or j<0 or i>=len(mat) or j>=len(mat[0]) or (i,j) in visited:
                     continue
-                if (i,j) in visited:
-                    continue
-                mat[i][j] = min(mat[i][j] , mat[x][y]+1 )
-                visited.add( (i,j) )
+                if mat[i][j] < 0 :
+                    mat[i][j] = mat[x][y] + 1
+                visited.add( (i,j))
                 q.append( (i,j) )
         return mat
