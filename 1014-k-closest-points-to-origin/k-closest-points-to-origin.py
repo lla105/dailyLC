9ac58cd730f8ac91a7fe964bdb502f8859fa7721
lla105 <105393685+lla105@users.noreply.github.com>
2025-04-15 16:00:19 -0700

    Time: 188 ms (8.80%) | Memory: 23.9 MB (5.35%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -1,32 +1,33 @@
-import math 
-
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        def geteuc(points):
-            # points = [ (x,y) ]
-            x = points[0]
-            y = points[1]
-            xx = x*x
-            yy = y*y
-            tempsum = xx+yy
-            return math.sqrt(tempsum)
-        
-        shortest = float('inf')
-        result = []
+        d = {}
+        def get_dist(x,y):
+            xsq = abs(x*x)
+            ysq = abs(y*y)
+            temp = sqrt( xsq+ysq )
+            # d[temp] = (x,y)
+            if temp in d:
+                d[temp].append( (x,y) )
+            else:
+                d[temp] = [ (x,y) ]
+            return temp
         arr = []
         for i in range(len(points)):
             x = points[i][0]
             y = points[i][1]
-            curdist = geteuc(points[i])
-            arr.append( (curdist, (x,y)))
-        print('arr > ', arr)
-        arr = sorted(arr, key=lambda x:x[0])
-        print('arr > ', arr)
-        
-        for i in range(len(arr)):
-            if k>0:
-                result.append(arr[i][1])
-            else:
-                return result
-            k-=1
-        return result
+            temp = get_dist(x,y)
+            # print('>>>', temp)
+            arr.append(temp)
+        heapq.heapify(arr)
+        count = 0
+        output = []
+        print('heap : ', arr)
+        while count < k:
+            rm_num = heapq.heappop(arr)
+            print('rm_num: ', rm_num)
+            array = d[rm_num]
+            for item in array:
+                output.append( item )
+                count+=1
+            # print(rm_num)
+        return output
