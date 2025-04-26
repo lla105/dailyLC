aceb5c1a976d271b4ea2bd201966c8bbe7ef79cd
lla105 <105393685+lla105@users.noreply.github.com>
2025-04-26 15:33:57 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (90.67%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,9 +1,26 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        self.result = []
-        def bruteforce( curList , index ) :
-            self.result.append(curList)
-            for i in range( index, len(nums)):
-                bruteforce( curList+[nums[i]] , i+1 )
-        bruteforce( [] , 0 )
-        return self.result
+        output = []
+        def traverse( nums_index , temp_arr ) :
+            if nums_index >= len(nums) :
+                output.append( tuple(temp_arr) )
+
+                return
+            output.append( tuple(temp_arr) )
+            for i in range( nums_index , len(nums) ) :
+                traverse( i+1 , temp_arr + [nums[i]] )
+        traverse( 0 , [] )
+        return output
+        
+# somefunction( nums_index, temp_arr )
+#    check base case
+
+#      for i in range(nums_index , len(nums)):
+#           somefunction( i+1 , temp_arr+[nums[i]] ) > (0+1 , [1])
+
+#somefunction(0, [] )
+#somefunction(1, [1] )
+#somefunction(2, [1,2] )
+#somefunction(3, [1,2,3] )
+#somefunction(3, [1,3] )
+
