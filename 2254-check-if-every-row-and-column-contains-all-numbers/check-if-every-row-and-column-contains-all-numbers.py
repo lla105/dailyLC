5e03bc6f08aa5b4d9e8b5f36e2fc3f0ac50d9a40
lla105 <105393685+lla105@users.noreply.github.com>
2025-04-26 23:12:48 -0700

    Time: 51 ms (26.75%) | Memory: 18.4 MB (94.36%) - LeetSync

--- 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.py
+++ 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.py
@@ -3,19 +3,26 @@ class Solution:
         # print(boo_arr)
         for i in range(len(matrix)):
             boo_arr = [False] * len(matrix)
+            boo_arr_vert = [False] * len(matrix)
             for j in range(len(matrix[0])):
                 cell_val = matrix[i][j]
                 boo_index = cell_val - 1
                 if boo_arr[boo_index] :
                     return False
                 boo_arr[boo_index] = True
-        print(' good so far? ')
-        for i in range(len(matrix)):
-            boo_arr = [False] * len(matrix)
-            for j in range(len(matrix[0])):
-                cell_val = matrix[j][i]
-                boo_index = cell_val - 1
-                if boo_arr[boo_index] :
+
+                cell_val_vert = matrix[j][i]
+                boo_index_vert = cell_val_vert - 1
+                if boo_arr_vert[boo_index_vert] :
                     return False
-                boo_arr[boo_index] = True
+                boo_arr_vert[boo_index_vert] = True
+        print(' good so far? ')
+        # for i in range(len(matrix)):
+        #     boo_arr = [False] * len(matrix)
+        #     for j in range(len(matrix[0])):
+        #         cell_val = matrix[j][i]
+        #         boo_index = cell_val - 1
+        #         if boo_arr[boo_index] :
+        #             return False
+        #         boo_arr[boo_index] = True
         return True
