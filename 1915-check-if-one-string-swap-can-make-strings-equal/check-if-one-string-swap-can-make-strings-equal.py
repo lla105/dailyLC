e3549c057a5cc8c7850ef504559ab115986752c7
lla105 <105393685+lla105@users.noreply.github.com>
2025-04-26 23:37:06 -0700

    Time: 0 ms (100.00%) | Memory: 17.6 MB (86.87%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.py
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.py
@@ -1,25 +1,19 @@
 class Solution:
     def areAlmostEqual(self, s1: str, s2: str) -> bool:
-        print('len s1: ', len(s1))
         s1_arr = []
         s2_arr = []
         match_count = 0
         for i in range(len(s1)):
-            print('>> ', s1[i], s2[i] )
             if s1[i] != s2[i] :
-                print(' MIS MATCH')
                 s1_arr.append( s1[i] )
                 s2_arr.append( s2[i] )
             else:
                 match_count += 1
-                print(' match ocunt : ', match_count)
         # if s1_arr[0] == s2_arr[1] and s1_arr[]
         if match_count != len(s1)-2 and len(s1_arr)!=0 :
-            print( ' match ocunt bad: ', match_count)
             return False
 
         if sorted(s1_arr) == sorted(s2_arr) :
             return True
         else:
-            print(' arrs wrong ')
             return False
