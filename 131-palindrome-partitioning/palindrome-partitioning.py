63384cd420aa0463c8fbc40b014fbedac88151ed
lla105 <105393685+lla105@users.noreply.github.com>
2025-05-06 16:15:42 -0700

    Time: 54 ms (40.01%) | Memory: 34.6 MB (43.90%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,26 +1,29 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        output = []
-        def isPalindrome( arr ) :
+        output = [] 
+        def isP( word ) :
             i = 0
-            j = len(arr) - 1
-            while i<j and arr[i]==arr[j] :
+            j = len(word) - 1
+            while i<j:
+                if word[i]!=word[j]:
+                    return False
                 i+=1
                 j-=1
-            if i == j or i-1==j :
-                return True
-            else:
-                return False
-        def trav( cur_list , idx):
+            return True
+        def trav(cur_string, cur_list, idx):
             if idx == len(s):
-                output.append( tuple(cur_list) )
+                if not cur_string:
+                    output.append(cur_list)
                 return
-            for i in range( idx, len(s) ):
-                ispalin = isPalindrome(s[idx:i+1])
-                if ispalin:
-                    trav( cur_list+[ s[idx:i+1]], i+1 )
-            return
-        trav( [], 0 )
+
+            cur_word = cur_string + s[idx]
+
+            # Option 1: commit current word if it's a palindrome
+            if isP(cur_word):
+                # print(f'{cur_word} is palin.')
+                trav("", cur_list + [cur_word], idx + 1)
+
+            # Option 2: keep extending current word
+            trav(cur_word, cur_list, idx + 1)
+        trav( "", [] , 0)
         return output
-            # aba
-            # aa
