e7e93901c460788d5291921d0e7dcadb86310e0c
lla105 <105393685+lla105@users.noreply.github.com>
2025-05-06 16:17:30 -0700

    Time: 91 ms (9.15%) | Memory: 34.7 MB (19.41%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -10,20 +10,21 @@ class Solution:
                 i+=1
                 j-=1
             return True
-        def trav(cur_string, cur_list, idx):
+        def trav( cur_string, cur_list, idx ) :
             if idx == len(s):
+                # if cur_string :
+                #     output.append( cur_list + [cur_string] )
                 if not cur_string:
-                    output.append(cur_list)
+                    output.append( cur_list)
                 return
-
+            # left
             cur_word = cur_string + s[idx]
-
-            # Option 1: commit current word if it's a palindrome
             if isP(cur_word):
-                # print(f'{cur_word} is palin.')
-                trav("", cur_list + [cur_word], idx + 1)
-
-            # Option 2: keep extending current word
-            trav(cur_word, cur_list, idx + 1)
+                print(f'{cur_word} is palin.')
+                trav( "", cur_list+[cur_word] , idx+1)
+            else:
+                print(f'{cur_word} is not palin.')
+            # right
+            trav( cur_word , cur_list , idx+1)
         trav( "", [] , 0)
         return output
