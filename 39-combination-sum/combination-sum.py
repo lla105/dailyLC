871dd1a7f25dd02210390ccd24280f633ec36c3b
lla105 <105393685+lla105@users.noreply.github.com>
2025-07-08 16:27:57 -0700

    Time: 19 ms (16.32%) | Memory: 17.8 MB (87.37%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,17 +1,16 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        c = candidates
-        if not c :
-            return []
-        outputList = set()
-
-        def bf( curList , index ) :
-            if sum(curList) > target :
+        output = []
+        c = sorted(candidates)
+        print(c)
+        def traverse(index, arr ):
+            if sum(arr) > target:
+                return 
+            if sum(arr) == target:
+                output.append( tuple(arr) )
                 return
-            if sum(curList) == target:
-                outputList.add( tuple(curList) )
-                return
+            # print(arr)
             for i in range(index, len(c)):
-                bf( curList+[c[i]] , i )
-        bf( [], 0)
-        return outputList
+                traverse(i, arr+[c[i]])
+        traverse(0, [] )
+        return output
