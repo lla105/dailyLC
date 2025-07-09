fb833e536b8903a3d6e15a8dfe9355fa611f02e2
lla105 <105393685+lla105@users.noreply.github.com>
2025-07-09 15:39:33 -0700

    Time: 2 ms (39.11%) | Memory: 17.9 MB (57.40%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,16 +1,41 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        
         output = []
-        def bf( curList , candidates ) :
-            if len(curList) == len(nums) :
-                output.append( tuple(curList ) )
+        def traverse( cur_arr, av_list ):
+            # print('>> ', av_list)
+            if len(cur_arr) == len(nums):
+                output.append( cur_arr )
                 return
-            for i in range( len (candidates) ):
-                nextList = curList+[candidates[i]]
-                firsthalf = candidates[:i]
-                secondhalf= candidates[i+1:]
-                nextcand = firsthalf + secondhalf
-                bf( nextList , nextcand)
-        bf( [] , nums )
+            for i in range( len(av_list)):
+                next_av_list = av_list[:i] + av_list[i+1:]
+                traverse( cur_arr+[av_list[i]] , next_av_list )
+        traverse( [], nums )
         return output
+
+# Decisions: include num, or don't include num (go to next num)
+# 123
+# [1] 23
+#   [1,2] 3
+#       [1,2,3]
+#   [1,3] 2
+#       [1,3,2]
+# [2] 13
+#   [2,3] 1
+#       [2,3,1]
+#   [2,1] 3
+#       [2,1,3]
+# [3]
+#   [3,1]
+#       [3,1,2]
+#   [3,2]
+#       [3,2,1]
+
+# 1,2,3
+# 1,3,2
+# 2,3,1
+# 3,1,2
+# 3,2,1
+
+# [1,2,3] -> 3*2*1 = 6
+# [1,2,3,4] -> 4*3*2 = 24
+
