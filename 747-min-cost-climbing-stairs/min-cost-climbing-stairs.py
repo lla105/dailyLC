3e718bab6a197c44c23889a79b8fa0bbf5ef1124
lla105 <105393685+lla105@users.noreply.github.com>
2025-07-11 16:02:36 -0700

    Time: 3 ms (73.25%) | Memory: 17.9 MB (62.83%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,13 +1,30 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        # dp array where dp[i] represents the minimum cost to reach step i
-        dp = [float('inf')] * (len(cost) + 1)
-        dp[0] = 0
-        dp[1] = 0
-        
-        # Iterate from the 2nd step up to the length of cost
-        for i in range(2, len(dp)):
-            dp[i] = min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2])
-        print(dp)
-        # return dp[len(cost)]
-        return dp[-1]
+        output = 0
+        cache = [0] * (len(cost))
+        cache[-1] = cost[-1]
+        cache[-2] = cost[-2]
+        #cost = [0,1,2,3]
+        #cache= [0,1,2,3]
+        print(cache)
+        for i in range( len(cost)-3, -1, -1 ) :
+            cache[i] = min(cost[i]+cache[i+1] , cost[i]+cache[i+2])
+        return min(cache[0], cache[1])
+
+# Decisions: take 1 or 2 steps
+
+#10
+#   +15
+#       +20
+#           end
+#       end
+#   +20
+#       end
+#15
+#   +20
+#       end
+#   end
+
+# cost = [1,100,1000,1]
+# cost = [1,1000,100,1]
+# cache= [x,x,   100,1]
