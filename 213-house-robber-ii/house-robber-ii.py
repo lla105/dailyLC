25ca04accbfc245cc12392760e214bfde07a1223
lla105 <105393685+lla105@users.noreply.github.com>
2025-07-16 16:54:35 -0700

    Time: 7 ms (1.15%) | Memory: 18 MB (17.29%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,19 +1,36 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) < 4 :
+        if len(nums) < 4:
             return max(nums)
-        arr1 = nums[:len(nums)-1]
-        arr2 = nums[1:]
-        dp1 = [0] * len(arr1)
-        dp2 = [0] * len(arr2)
-        for i in range(2) :
-            dp1[i] = arr1[i]
-            dp2[i] = arr2[i]
-        for i in range( 2, len(arr1)):
-            if i==2:
-                dp1[i] = arr1[i]+dp1[i-2]
-                dp2[i] = arr2[i]+dp2[i-2]
-                continue
-            dp1[i] = max( arr1[i]+dp1[i-2] , arr1[i]+dp1[i-3])
-            dp2[i] = max( arr2[i]+dp2[i-2] , arr2[i]+dp2[i-3])
-        return max( dp1[-1] , dp2[-1], dp1[-2], dp2[-2])
+        cache1 = [0] * (len(nums)-1)
+        cache2 = [0] * (len(nums)-1)
+        # [200,3,140,20,10]
+        cache1[0] = nums[0]
+        cache1[1] = nums[1]
+        cache1[2] = nums[0] + nums[2]
+        # cache1 = [200,3,340,0]
+        cache2[0] = nums[1]
+        cache2[1] = nums[2]
+        cache2[2] = nums[1] + nums[3]
+        # cache2 = [3,140,23,0]
+        print(cache1)
+        print(cache2)
+        # [200,3,140,20,10]
+        for i in range(3 , len(cache1)) :
+            cache1[i] = max( (cache1[i-2]+nums[i]) , (cache1[i-3]+nums[i]) )
+            cache2[i] = max( (cache2[i-2]+nums[i+1]) , (cache2[i-3]+nums[i+1]) )
+            print(cache1)
+            print(cache2)
+            print('=====')
+        return max(cache1[-1], cache1[-2] , cache2[-1] , cache2[-2])
+
+#nums=[a,b,c,d,e,f]
+#cache1=[a,b,c,d,e]
+#cache2=[b,c,d,e,f]
+
+#[a,b,c,d,e,f,g]
+#[a,b,c,d,e,f]
+#[b,c,d,e,f,g]
+
+# d+a, e+a, f+b
+# if len(nums)>4
