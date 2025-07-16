10101bbb75cd8d6cb0c0a45616f62c3d82862ca2
lla105 <105393685+lla105@users.noreply.github.com>
2025-07-16 15:02:06 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (54.38%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -2,14 +2,50 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         if len(nums) < 3:
             return max(nums)
-        dp = [0] * len(nums)
-        dp[0] = nums[0]
-        dp[1] = nums[1]
-        for i in range(2, len(dp)):
-            if i==2:
-                dp[i] = nums[i] + dp[i-2]
-                # print('>>> ' , nums[i] , dp[i-2])
-                continue
-            dp[i] = max( nums[i]+dp[i-3] , nums[i]+dp[i-2] )
-            # print( dp, nums[i]+dp[i-3] , nums[i]+dp[i-2] )
-        return max(dp[-1], dp[-2])
+        cache = [0] * len(nums)
+        cache[0] = nums[0]
+        cache[1] = nums[1]
+        cache[2] = nums[0] + nums[2]
+        for i in range( 3, len(nums)):
+            one = nums[i] + cache[i-2]
+            two = nums[i] + cache[i-3]
+            cache[i] = max( one , two )
+        print(cache)
+        return max( cache[-1] , cache[-2])
+        # self.output = 0
+        # def traverse(prev_sum, idx):
+        #     if idx >=len(nums) :
+        #         self.output = max(self.output , prev_sum)
+        #         return prev_sum
+        #     cur_sum = prev_sum + nums[idx]
+        #     one = traverse(cur_sum, idx+2)
+        #     two = traverse(cur_sum, idx+3)
+        #     three = traverse(prev_sum, idx+1)
+        # traverse(0, 0)
+        # return self.output
+
+#t(0,0)
+#   one = t(2,1)
+#       one = t()
+#   two = t(2,2)
+#   thr = t(0,1)
+
+#nums = [a,b,c,d,e]
+#nums = [5,1,1,5,1]
+#notes:
+# at each num, either:
+# - pick current number
+#   - pick next next num (i+2)
+#   - pick (i+3) num
+# - pick next num (i+1)
+
+
+# [2,7,9,3,1]
+#[]
+#   [2]2
+#       [2,9]11
+#           [2,9,1]12
+#       [2,3]5
+#   [7]7
+#       [7,3]10
+#       [7,1]8
