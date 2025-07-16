9ae9fe4facb3f473b49b20d4c7800d6354ecec0f
lla105 <105393685+lla105@users.noreply.github.com>
2025-07-16 16:55:05 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (86.32%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -13,16 +13,12 @@ class Solution:
         cache2[1] = nums[2]
         cache2[2] = nums[1] + nums[3]
         # cache2 = [3,140,23,0]
-        print(cache1)
-        print(cache2)
         # [200,3,140,20,10]
         for i in range(3 , len(cache1)) :
             cache1[i] = max( (cache1[i-2]+nums[i]) , (cache1[i-3]+nums[i]) )
             cache2[i] = max( (cache2[i-2]+nums[i+1]) , (cache2[i-3]+nums[i+1]) )
-            print(cache1)
-            print(cache2)
-            print('=====')
-        return max(cache1[-1], cache1[-2] , cache2[-1] , cache2[-2])
+        # return max(cache1[-1], cache1[-2] , cache2[-1] , cache2[-2])
+        return max( max(cache1) , max(cache2) )
 
 #nums=[a,b,c,d,e,f]
 #cache1=[a,b,c,d,e]
