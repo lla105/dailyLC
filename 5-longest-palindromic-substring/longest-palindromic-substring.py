319088024eb7d657452f6b75457415f34f7614b6
lla105 <105393685+lla105@users.noreply.github.com>
2025-07-16 17:28:19 -0700

    Time: 267 ms (68.42%) | Memory: 17.7 MB (83.80%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,29 +1,40 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        
-        maxlen = 0
-        maxstring = ''
-        whatj= 0
+        output = ""
+        def pal_len( idx , is_even ) :
+            if is_even:
+                l = idx
+            else:
+                l = idx-1
+            r = idx+1
+            while l>=0 and r<len(s) and s[l]==s[r]:
+                l-=1
+                r+=1
+            l+=1
+            r-=1
+            return s[l:r+1]
         for i in range(len(s)):
-            #even 
-            l=i
-            r=i+1
-            while l>=0 and r<len(s) and s[l]==s[r]:
-                if maxlen < len(s[l:r+1]) :
-                    maxlen = len(s[l:r+1])
-                    maxstring = s[l:r+1]
-                l-=1
-                r+=1
-            
-            #odd
-            l=i
-            r=i
-            print()
-            while l>=0 and r<len(s) and s[l]==s[r]:
-                if maxlen < len(s[l:r+1]):
-                    maxlen = len(s[l:r+1])
-                    maxstring = s[l:r+1]
-                l-=1
-                r+=1
-        print("asdfasf")
-        return maxstring
+            even = pal_len(i,is_even=True)
+            odd = pal_len(i,is_even=False)
+            if len(even) > len(output) :
+                output = even
+            if len(odd) > len(output) :
+                output = odd
+        return output
+
+#s1=aba
+#s2=abba
+
+# s= babad
+# b
+#   b
+#   ba
+# a
+#   bab 
+#   ab
+# s = cbbd
+# c
+# b
+#   cbb
+#   bb
+#       cbbd NOPE
