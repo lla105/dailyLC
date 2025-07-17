3eba104f3f34a3b21a1a9fa709e5b78f86b79937
lla105 <105393685+lla105@users.noreply.github.com>
2025-07-17 14:24:43 -0700

    Time: 36 ms (98.60%) | Memory: 18.1 MB (26.47%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,28 +1,41 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        def count_even_pal(idx):
-            l = idx
-            r = idx+1
-            while l>=0 and r<len(s) and s[l]==s[r]:
-                l-=1
-                r+=1
-            return s[l+1 : r]
-        def count_odd_pal(idx):
-            l = idx
-            r = idx
-            while l>=0 and r<len(s) and s[l]==s[r]:
-                l-=1
-                r+=1
-            return s[l+1 : r]
-        output = ""
-        for i in range(len(s)):
-            even_string = count_even_pal(i)
-            odd_string = count_odd_pal(i)
-            if len(even_string) > len(output):
-                output = even_string
-            if len(odd_string) > len(output):
-                output = odd_string
-        return output
-# s = 12378321
-# s = abba <- even case
-# s = aba. <- odd case
+        if not s:
+            return ""
+
+        # Transform string with separators
+        t = '#' + '#'.join(s) + '#'
+        n = len(t)
+        p = [0] * n
+        center = 0
+        right = 0
+
+        max_len = 0
+        max_center = 0
+
+        for i in range(n):
+            mirror = 2 * center - i
+            if i < right:
+                p[i] = min(right - i, p[mirror])
+
+            # Expand around center i
+            a = i + p[i] + 1
+            b = i - p[i] - 1
+            while a < n and b >= 0 and t[a] == t[b]:
+                p[i] += 1
+                a += 1
+                b -= 1
+
+            # Update center and right boundary if needed
+            if i + p[i] > right:
+                center = i
+                right = i + p[i]
+
+            # Track max palindrome
+            if p[i] > max_len:
+                max_len = p[i]
+                max_center = i
+
+        # Extract the actual palindrome from the original string
+        start = (max_center - max_len) // 2
+        return s[start:start + max_len]
